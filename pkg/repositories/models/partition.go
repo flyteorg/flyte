ba3ce6258f27587637b28883db53a29374e0b45a
Andrew Chan <achan@lyft.com>
2019-10-22 19:29:42 -0700

    Add mock for parition repo

--- pkg/repositories/models/partition.go
+++ pkg/repositories/models/partition.go
@@ -5,7 +5,7 @@ package models
 // 2. Get the artifact that has the partitions (x,y,z + tag_name) = latest [x]
 type Partition struct {
 	BaseModel
-	DatasetUUID string `gorm:"type:uuid;primary_key"`
+	DatasetUUID string `gorm:"primary_key;type:uuid"`
 	KeyName     string `gorm:"primary_key"`
 	KeyValue    string `gorm:"primary_key"`
 	ArtifactID  string `gorm:"primary_key;index"` // index for JOINs with the Tag/Labels table when querying artifacts
