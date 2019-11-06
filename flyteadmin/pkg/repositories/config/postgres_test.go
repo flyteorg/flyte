ec40b1d835dae2df0118c06a6a1a18194148905d
tnsetting <38207208+tnsetting@users.noreply.github.com>
2019-11-06 16:18:34 +0100

    Support postgres extra options [gcp] (#27)
    * Support postgres extra options [gcp]
    
    Right now it is enforced to use sslmode when flyte admin is connecting to postgres and password provided.
    The PR make it possible to connect to postgres when disabling sslmode.
    
    * Add test
    
    * Add test for case with pwd but no extra option

--- flyteadmin/pkg/repositories/config/postgres_test.go
+++ flyteadmin/pkg/repositories/config/postgres_test.go
@@ -19,3 +19,28 @@ func TestConstructGormArgs(t *testing.T) {
 
 	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres sslmode=disable", postgresConfigProvider.GetArgs())
 }
+
+func TestConstructGormArgsWithPassword(t *testing.T) {
+	postgresConfigProvider := NewPostgresConfigProvider(DbConfig{
+		Host:         "localhost",
+		Port:         5432,
+		DbName:       "postgres",
+		User:         "postgres",
+		Password:     "pass",
+		ExtraOptions: "sslmode=enable",
+	}, mockScope.NewTestScope())
+
+	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres password=pass sslmode=enable", postgresConfigProvider.GetArgs())
+}
+
+func TestConstructGormArgsWithPasswordNoExtra(t *testing.T) {
+	postgresConfigProvider := NewPostgresConfigProvider(DbConfig{
+		Host:     "localhost",
+		Port:     5432,
+		DbName:   "postgres",
+		User:     "postgres",
+		Password: "pass",
+	}, mockScope.NewTestScope())
+
+	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres password=pass ", postgresConfigProvider.GetArgs())
+}
