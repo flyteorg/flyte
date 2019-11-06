882d111bf862be215b5a8671cb1b323dfc58d7da
Randy Schott <1815175+schottra@users.noreply.github.com>
2019-11-06 14:56:43 -0800

    Actually use the specialized task name list component :-)

--- src/components/Project/ProjectTasks.tsx
+++ src/components/Project/ProjectTasks.tsx
@@ -1,12 +1,6 @@
-import { Typography } from '@material-ui/core';
-import { SearchResult, WaitForData } from 'components/common';
-import {
-    SearchableNamedEntity,
-    SearchableNamedEntityList,
-    useNamedEntityListStyles
-} from 'components/common/SearchableNamedEntityList';
-import { useCommonStyles } from 'components/common/styles';
+import { WaitForData } from 'components/common';
 import { useTaskNameList } from 'components/hooks/useNamedEntity';
+import { SearchableTaskNameList } from 'components/Task/SearchableTaskNameList';
 import { limits, SortDirection, workflowSortFields } from 'models';
 import * as React from 'react';
 
@@ -20,29 +14,6 @@ export const ProjectTasks: React.FC<ProjectTasksProps> = ({
     domainId: domain,
     projectId: project
 }) => {
-    const listStyles = useNamedEntityListStyles();
-    const commonStyles = useCommonStyles();
-    const renderItem = ({
-        key,
-        value,
-        content
-    }: SearchResult<SearchableNamedEntity>) => (
-        <li key={key}>
-            <div className={listStyles.searchResult}>
-                <div className={listStyles.itemName}>
-                    <div>{content}</div>
-                    {!!value.metadata.description && (
-                        <Typography
-                            variant="body2"
-                            className={commonStyles.hintText}
-                        >
-                            {value.metadata.description}
-                        </Typography>
-                    )}
-                </div>
-            </div>
-        </li>
-    );
     const taskNames = useTaskNameList(
         { domain, project },
         {
@@ -56,10 +27,7 @@ export const ProjectTasks: React.FC<ProjectTasksProps> = ({
 
     return (
         <WaitForData {...taskNames}>
-            <SearchableNamedEntityList
-                names={taskNames.value}
-                renderItem={renderItem}
-            />
+            <SearchableTaskNameList names={taskNames.value} />
         </WaitForData>
     );
 };
