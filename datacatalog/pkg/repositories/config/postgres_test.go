77e5c5af3f08ea411e45bc1fd0b8fe120ba85d87
Honnix <honnix@users.noreply.github.com>
2019-11-13 23:01:12 +0100

    pass extra options to postgres args (#11)
    This replicates https://github.com/lyft/flyteadmin/pull/27

---
+++ datacatalog/pkg/repositories/config/postgres_test.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"testing"
+
+	mockScope "github.com/lyft/flytestdlib/promutils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConstructGormArgs(t *testing.T) {
+	postgresConfigProvider := NewPostgresConfigProvider(DbConfig{
+		Host:         "localhost",
+		Port:         5432,
+		DbName:       "postgres",
+		User:         "postgres",
+		ExtraOptions: "sslmode=disable",
+	}, mockScope.NewTestScope())
+
+	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres sslmode=disable", postgresConfigProvider.GetArgs())
+}
+
+func TestConstructGormArgsWithPassword(t *testing.T) {
+	postgresConfigProvider := NewPostgresConfigProvider(DbConfig{
+		Host:         "localhost",
+		Port:         5432,
+		DbName:       "postgres",
+		User:         "postgres",
+		Password:     "pass",
+		ExtraOptions: "sslmode=enable",
+	}, mockScope.NewTestScope())
+
+	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres password=pass sslmode=enable", postgresConfigProvider.GetArgs())
+}
+
+func TestConstructGormArgsWithPasswordNoExtra(t *testing.T) {
+	postgresConfigProvider := NewPostgresConfigProvider(DbConfig{
+		Host:     "localhost",
+		Port:     5432,
+		DbName:   "postgres",
+		User:     "postgres",
+		Password: "pass",
+	}, mockScope.NewTestScope())
+
+	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres password=pass ", postgresConfigProvider.GetArgs())
+}
