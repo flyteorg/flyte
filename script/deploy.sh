d813cb0e9d39d8e5b7a0e93a1902d85ec56df963
Yee Hing Tong <ytong@lyft.com>
2019-11-23 14:11:47 -0800

    changing the safety check to a prompt

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,11 +3,22 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 CONTEXT=$(kubectl config current-context)
+DEPLOY_FILE="${DIR}/../deployment/sandbox/flyte_generated.yaml"
 
-if [ "$CLUSTER" == "docker-for-desktop" ] || [ "$CLUSTER" == "docker-desktop" ]; then
-  DEPLOY_FILE="${DIR}/../deployment/sandbox/flyte_generated.yaml"
+function run_kubectl {
   kubectl apply -f "$DEPLOY_FILE"
+}
+
+if [ "$CONTEXT" == "docker-for-desktop" ] || [ "$CLUSTER" == "docker-desktop" ]; then
+  run_kubectl
 else
-  echo "$CLUSTER not supported - Sandbox deploys only allowed on 'docker-for-desktop' and 'docker-desktop' clusters"
-  exit 1
+  echo "$CONTEXT is not a docker-desktop cluster"
+  read -p "  Are you sure you want to continue? " answer
+    if [ "$answer" != "${answer#[Yy]}" ] ;then
+        run_kubectl
+        exit 0
+    else
+        echo Exiting without doing anything
+        exit 1
+    fi
 fi
