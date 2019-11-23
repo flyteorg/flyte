7ac4a54e17b2f123e39c1c76b8000e95700d3f6b
Yee Hing Tong <ytong@lyft.com>
2019-11-23 12:38:02 -0800

    add safety check

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,12 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-DEPLOY_FILE="${DIR}/../deployment/sandbox/flyte_generated.yaml"
-kubectl apply -f "$DEPLOY_FILE"
+CONTEXT=$(kubectl config current-context)
+
+if [ "$CLUSTER" == "docker-for-desktop" ] || [ "$CLUSTER" == "docker-desktop" ]; then
+  DEPLOY_FILE="${DIR}/../deployment/sandbox/flyte_generated.yaml"
+  kubectl apply -f "$DEPLOY_FILE"
+else
+  echo "$CLUSTER not supported - Sandbox deploys only allowed on 'docker-for-desktop' and 'docker-desktop' clusters"
+  exit 1
+fi
