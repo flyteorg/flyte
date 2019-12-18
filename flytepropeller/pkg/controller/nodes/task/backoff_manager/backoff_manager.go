b0f3f217ee3eb23aad294085ec96fc67996ab19e
Chang-Hong Hsu <changhonghsu@lyft.com>
2019-12-18 11:27:21 -0800

    add a field to BackOffManager and BackOffHandler to indicate if it is active

--- flytepropeller/pkg/controller/nodes/task/backoff_manager/backoff_manager.go
+++ flytepropeller/pkg/controller/nodes/task/backoff_manager/backoff_manager.go
@@ -17,6 +17,7 @@ var (
 type BackOffManager struct {
 	Clock             clock.Clock
 	backOffHandlerMap BackOffHandlerMap
+	defaultIsActive   bool
 }
 
 func (m *BackOffManager) GetBackOffHandler(key string) (*ComputeResourceAwareBackOffHandler, bool) {
@@ -36,6 +37,7 @@ func (m *BackOffManager) CreateBackOffHandler(key string, backOffBaseSecond int,
 		ComputeResourceCeilings: &ComputeResourceCeilings{
 			computeResourceCeilings: v1.ResourceList{},
 		},
+		IsActive: m.defaultIsActive,
 	})
 	h, _ := m.backOffHandlerMap.Get(key)
 	h.ComputeResourceCeilings.resetAll()
@@ -47,12 +49,13 @@ func ComposeResourceKey(o k8s.Resource) string {
 	return fmt.Sprintf("%v,%v", o.GroupVersionKind().String(), o.GetNamespace())
 }
 
-func NewBackOffManager(ctx context.Context, baseSecond int, maxDuration time.Duration) *BackOffManager {
+func NewBackOffManager(ctx context.Context, baseSecond int, maxDuration time.Duration, active bool) *BackOffManager {
 	backOffBaseSecond = baseSecond
 	maxBackOffDuration = maxDuration
 
 	return &BackOffManager{
 		Clock:             clock.RealClock{},
 		backOffHandlerMap: BackOffHandlerMap{},
+		defaultIsActive:   active,
 	}
 }
