d6cfc58950c990297f4bd148cb40482f46ec1d13
Katrina Rogan <krogan@lyft.com>
2019-12-19 17:05:39 -0800

    Add flyte_client config endpoint handler (#44)

---
+++ pkg/rpc/config/flyte_client.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/lyft/flyteadmin/pkg/config"
+	"github.com/lyft/flytestdlib/logger"
+)
+
+const (
+	clientID        = "client_id"
+	redirectURI     = "redirect_uri"
+	authMetadataKey = "authorization_metadata_key"
+)
+
+func HandleFlyteCliConfigFunc(ctx context.Context, cfg *config.ServerConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		configValues := map[string]string{
+			clientID:        cfg.Security.Oauth.ClientID,
+			redirectURI:     cfg.Security.Oauth.CallbackURL,
+			authMetadataKey: cfg.Security.Oauth.GrpcAuthorizationHeader,
+		}
+		configJSON, err := json.Marshal(configValues)
+		if err != nil {
+			logger.Infof(ctx, "Failed to marshal flyte_client config to JSON with err: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = w.Write(configJSON)
+		if err != nil {
+			logger.Warningf(ctx, "Failed to write config json [%+v] with err: %v", string(configJSON), err)
+		}
+	}
+}
