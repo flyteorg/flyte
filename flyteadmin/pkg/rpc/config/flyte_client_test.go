ea0ab6111246ba6fad9b9cfbb4c06a76132c46e3
Katrina Rogan <krogan@lyft.com>
2019-12-19 17:05:39 -0800

    Add flyte_client config endpoint handler (#44)

---
+++ flyteadmin/pkg/rpc/config/flyte_client_test.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	authConfig "github.com/lyft/flyteadmin/pkg/auth/config"
+	"github.com/lyft/flyteadmin/pkg/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleFlyteCliConfigFunc(t *testing.T) {
+	testClientID := "12345"
+	testRedirectURI := "localhost:12345/callback"
+	testAuthMetadataKey := "flyte-authorization"
+
+	handleFlyteCliConfigFunc := HandleFlyteCliConfigFunc(context.Background(), &config.ServerConfig{
+		Security: config.ServerSecurityOptions{
+			Oauth: authConfig.OAuthOptions{
+				ClientID:                testClientID,
+				CallbackURL:             testRedirectURI,
+				GrpcAuthorizationHeader: testAuthMetadataKey,
+			},
+		},
+	})
+
+	responseRecorder := httptest.NewRecorder()
+	handleFlyteCliConfigFunc(responseRecorder, nil)
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	responseBody := responseRecorder.Body
+	var responseBodyMap map[string]string
+	err := json.Unmarshal(responseBody.Bytes(), &responseBodyMap)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal response body with err: %v", err)
+	}
+	assert.EqualValues(t, map[string]string{
+		clientID:        testClientID,
+		redirectURI:     testRedirectURI,
+		authMetadataKey: testAuthMetadataKey,
+	}, responseBodyMap)
+}
