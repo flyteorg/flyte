ce2f851923cfc99c93f5df372fba605961c761f9
Honnix <honnix@users.noreply.github.com>
2020-01-24 12:38:50 +0100

    migrate to go module (#27)
    * migrate to go module
    
    * install mockery
    
    * do not always download pflags
    
    * latest version of flytestdlib
    
    * refomat
    
    * Revert "refomat"
    
    This reverts commit b30109297ce71b8f40e9cfab60b2db9d1dd8ad06.
    
    * mockery
    
    * mockery
    
    * sum
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Update .travis.yml
    
    * Update Makefile
    
    * Update Makefile
    
    * move it to the right place
    
    * install mockery in a temp folder
    
    * add comments
    
    * upgrade flytestdlib
    
    * go mod tidy
    
    * add h1 sum
    
    * test
    
    * rebase from master
    
    * latest flytestdlib
    
    * install pflags in tmp folder
    
    * change go version back
    
    * add comment
    
    * incorporate latest boilerplate changes
    
    * rely on Makefile to install mockery
    
    * updated boilerplate
    
    * use install instead
    
    * actually adding the new boilerplate entry and updating again
    
    * removing unnecessary files
    
    * no need for replacement in root go.mod
    
    Co-authored-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flyteidl/generate_mocks.sh
+++ flyteidl/generate_mocks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-which mockery || (go install github.com/lyft/flyteidl/vendor/github.com/vektra/mockery/cmd/mockery)
 
 mockery -dir=gen/pb-go/flyteidl/service/ -name=AdminServiceClient -output=clients/go/admin/mocks
 mockery -dir=gen/pb-go/flyteidl/datacatalog/ -name=ArtifactsClient -output=clients/go/datacatalog/mocks
