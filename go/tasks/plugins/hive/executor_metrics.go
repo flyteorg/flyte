2fc716c5303fd242d087fd89908384e73dbdc1de
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-01-27 15:58:09 -0800

    dep ensure and add a mtric

--- go/tasks/plugins/hive/executor_metrics.go
+++ go/tasks/plugins/hive/executor_metrics.go
@@ -3,6 +3,7 @@ package hive
 import (
 	"github.com/lyft/flytestdlib/promutils"
 	"github.com/lyft/flytestdlib/promutils/labeled"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type QuboleHiveExecutorMetrics struct {
@@ -10,7 +11,13 @@ type QuboleHiveExecutorMetrics struct {
 	ReleaseResourceFailed labeled.Counter
 	AllocationGranted     labeled.Counter
 	AllocationNotGranted  labeled.Counter
+	TokenAge              prometheus.Summary
 }
+
+var (
+	tokenAgeObjectives = map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001, 1.0: 0.0}
+)
+
 
 func getQuboleHiveExecutorMetrics(scope promutils.Scope) QuboleHiveExecutorMetrics {
 	return QuboleHiveExecutorMetrics{
@@ -21,5 +28,7 @@ func getQuboleHiveExecutorMetrics(scope promutils.Scope) QuboleHiveExecutorMetri
 			"Allocation request granted", scope),
 		AllocationNotGranted: labeled.NewCounter("allocation_not_granted",
 			"Allocation request did not fail but not granted", scope),
+		TokenAge:
+		// scope.MustNewSummary("token_age", "The age of the resource manager tokens"),
 	}
 }
