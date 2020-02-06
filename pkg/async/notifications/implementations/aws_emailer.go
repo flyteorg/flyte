001347ad82531362e341153b26ac50f468feb6ee
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2020-02-05 21:31:43 -0800

    Email recipient pointers (#67)

--- pkg/async/notifications/implementations/aws_emailer.go
+++ pkg/async/notifications/implementations/aws_emailer.go
@@ -37,13 +37,16 @@ type AwsEmailer struct {
 	awsEmail      sesiface.SESAPI
 }
 
-func (e *AwsEmailer) SendEmail(ctx context.Context, email admin.EmailMessage) error {
+func FlyteEmailToSesEmailInput(email admin.EmailMessage) ses.SendEmailInput {
 	var toAddress []*string
 	for _, toEmail := range email.RecipientsEmail {
-		toAddress = append(toAddress, &toEmail)
+		// SES email input takes an array of pointers to strings so we have to create a new one for each email
+		//nolint:unconvert
+		e := string(toEmail)
+		toAddress = append(toAddress, &e)
 	}
 
-	emailInput := ses.SendEmailInput{
+	return ses.SendEmailInput{
 		Destination: &ses.Destination{
 			ToAddresses: toAddress,
 		},
@@ -62,17 +65,19 @@ func (e *AwsEmailer) SendEmail(ctx context.Context, email admin.EmailMessage) er
 			},
 		},
 	}
+}
 
+func (e *AwsEmailer) SendEmail(ctx context.Context, email admin.EmailMessage) error {
+	emailInput := FlyteEmailToSesEmailInput(email)
 	_, err := e.awsEmail.SendEmail(&emailInput)
 	e.systemMetrics.SendTotal.Inc()
-
 	if err != nil {
 		// TODO: If we see a certain set of AWS errors consistently, we can break the errors down based on type.
 		logger.Errorf(ctx, "error in sending email [%s] via ses mailer with err: %s", email.String(), err)
 		e.systemMetrics.SendError.Inc()
 		return errors.NewFlyteAdminErrorf(codes.Internal, "errors were seen while sending emails")
 	}
-
+	logger.Debugf(ctx, "Sent email to %s sub: %s", email.RecipientsEmail, email.SubjectLine)
 	e.systemMetrics.SendSuccess.Inc()
 	return nil
 }
