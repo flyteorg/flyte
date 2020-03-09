9dc23bb132f388622af2a2223ad9e5e34c65d7a3
Hongxin Liang <honnix@spotify.com>
2020-03-09 22:04:42 +0100

    check generated file as part of GitHub Workflow

--- script/kustomize.sh
+++ script/kustomize.sh
@@ -12,3 +12,19 @@ for deployment in ${DEPLOYMENT}; do
            "overlays/${deployment}/flyte" \
            > "${DIR}/../deployment/${deployment}/flyte_generated.yaml"
 done
+
+# This section is used by GitHub workflow to ensure that the generation step was run
+if [ -n "$DELTA_CHECK" ]; then
+  DIRTY=$(git status --porcelain)
+  if [ -n "$DIRTY" ]; then
+    echo "FAILED: kustomize code updated without commiting generated code."
+    echo "Ensure make kustomize has run and all changes are committed."
+    DIFF=$(git diff)
+    echo "diff detected: $DIFF"
+    DIFF=$(git diff --name-only)
+    echo "files different: $DIFF"
+    exit 1
+  else
+    echo "SUCCESS: Generated code is up to date."
+  fi
+fi
