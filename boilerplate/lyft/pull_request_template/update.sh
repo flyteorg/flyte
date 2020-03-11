b6806d6f3fc5b39a5922b6cb93543b93c59bbe54
Ketan Umare <kumare@lyft.com>
2020-03-11 11:08:14 -0700

    Add Standardized pull request template (#74)

---
+++ boilerplate/lyft/pull_request_template/update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# WARNING: THIS FILE IS MANAGED IN THE 'BOILERPLATE' REPO AND COPIED TO OTHER REPOSITORIES.
+# ONLY EDIT THIS FILE FROM WITHIN THE 'LYFT/BOILERPLATE' REPOSITORY:
+# 
+# TO OPT OUT OF UPDATES, SEE https://github.com/lyft/boilerplate/blob/master/Readme.rst
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+cp ${DIR}/pull_request_template.md ${DIR}/../../../pull_request_template.md
