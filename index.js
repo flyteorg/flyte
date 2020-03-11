3bd33291b2525b245652496831e01cf24a7f14f2
Randy Schott <1815175+schottra@users.noreply.github.com>
2020-03-11 15:33:35 -0700

    Adding env variables and loading support for middleware plugins

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 const morgan = require('morgan');
 const express = require('express');
 const env = require('./env');
+const { applyMiddleware } = require('./plugins');
 
 const corsProxy = require('./corsProxy.js');
 
@@ -18,6 +19,11 @@ app.use(morgan('combined'));
 
 app.get(`${env.BASE_URL}/healthz`, (_req, res) => res.status(200).send());
 app.use(corsProxy(`${env.BASE_URL}${env.CORS_PROXY_PREFIX}`));
+
+if (typeof applyMiddleware === 'function') {
+    console.log('Found middleware plugins, applying...');
+    applyMiddleware(app);
+}
 
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
---
+++ plugins.js
@@ -0,0 +1,11 @@
+const env = require('./env');
+
+const { middleware } = env.PLUGINS_MODULE ? require(env.PLUGINS_MODULE) : {};
+
+if (Array.isArray(middleware)) {
+    module.exports.applyMiddleware = app => middleware.forEach(m => m(app));
+} else if (middleware !== undefined) {
+    console.log(
+        `Expected middleware to be of type Array, but got ${middleware} instead`
+    );
+}
