1f23878330d7586623f8e8288aa5866fc783df91
Honnix <honnix@users.noreply.github.com>
2020-03-16 18:29:36 +0100

    fix failing test case (#91)

--- flytepropeller/pkg/controller/nodes/subworkflow/subworkflow_test.go
+++ flytepropeller/pkg/controller/nodes/subworkflow/subworkflow_test.go
@@ -53,7 +53,10 @@ func Test_subworkflowHandler_HandleAbort(t *testing.T) {
 		nCtx.OnNodeID().Return("n1")
 		n := &coreMocks.ExecutableNode{}
 		swf.OnStartNode().Return(n)
-		nodeExec.OnAbortHandler(ctx, wf, n, "reason").Return(fmt.Errorf("err"))
+		swf.OnGetID().Return("swf")
+		nodeExec.OnAbortHandlerMatch(mock.Anything, mock.MatchedBy(func(wf v1alpha1.ExecutableWorkflow) bool {
+			return wf.GetID() == swf.GetID()
+		}), n, mock.Anything).Return(fmt.Errorf("err"))
 		assert.Error(t, s.HandleAbort(ctx, nCtx, wf, "x", "reason"))
 	})
 
