b705166be53efe67003d81b45c777a6825b90bff
Honnix <honnix@users.noreply.github.com>
2020-04-15 15:23:29 +0200

    wait longer for k8s to start during e2e test

--- end2end/dockernetes_run.sh
+++ end2end/dockernetes_run.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo ""
-echo "waiting up to 5 minutes for kubernetes to start..."
+echo "waiting up to 10 minutes for kubernetes to start..."
 
-K8S_TIMEOUT="300"
+K8S_TIMEOUT="600"
 
 SECONDS=0
 while ! systemctl is-active --quiet multi-user.target; do
