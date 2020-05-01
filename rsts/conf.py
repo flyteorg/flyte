209c2bcf2e97804f4ee1f37547b6516091dd85ee
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2020-05-01 10:11:13 -0700

    Milestone 0.3.0 release (#302)

--- rsts/conf.py
+++ rsts/conf.py
@@ -26,7 +26,7 @@ author = u'Flyte Authors'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
