51d5b7715a1ee1fe2af95c9cb724419ac9df9a0e
Ketan Umare <kumare@lyft.com>
2020-06-03 11:29:35 -0700

    Importing pytorch into propeller (#144)

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/array/k8s"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/hive"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/container"
+	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/kfoperators/pytorch"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/sidecar"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/spark"
 	"github.com/lyft/flytestdlib/contextutils"
