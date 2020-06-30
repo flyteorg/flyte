026b8e246d8a75cceaf9c530f53646413a11d36c
Ketan Umare <kumare@lyft.com>
2020-06-29 22:19:19 -0700

    version updated

--- rsts/conf.py
+++ rsts/conf.py
@@ -26,7 +26,7 @@ author = u'Flyte Authors'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.4.0'
+release = u'0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
