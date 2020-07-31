f11ca5f0ecb047b51e84082820abc0c060074e3f
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-07-30 17:04:38 -0700

    SageMaker on Flyte: installing SageMaker plugins [Alpha]  (#163)
    * install the sagemaker plugin
    
    * upgrade flyteplugin
    
    * upgrade dependency on flyteplugins
    
    * upgrade flyteplugins
    
    * upgrade flyteplugins
    
    * go mod
    
    * go mod
    
    * go mod
    
    * add to sandbox config and pull latest flyteidl and flyteplugins
    
    * flyteplugins v0.4.0

--- flytepropeller/cmd/controller/main.go
+++ flytepropeller/cmd/controller/main.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/hive"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/container"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/kfoperators/pytorch"
+	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/sagemaker"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/sidecar"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/spark"
 	"github.com/lyft/flytestdlib/contextutils"
