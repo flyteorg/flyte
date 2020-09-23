0a0467c9cd9ffba2fa73398ef2543b72361bc2e7
Ketan Umare <kumare@lyft.com>
2020-09-23 10:59:39 -0700

    wip

--- script/kustomize.sh
+++ script/kustomize.sh
@@ -9,7 +9,7 @@ KUSTOMIZE_IMAGE="lyft/kustomizer:v3.1.0"
 
 for deployment in ${DEPLOYMENT}; do
     docker run -v "${DIR}/../kustomize":/kustomize "$KUSTOMIZE_IMAGE" kustomize build \
-           "overlays/${deployment}/flyte" \
+           "overlays/${deployment}" \
            > "${DIR}/../deployment/${deployment}/flyte_generated.yaml"
 done
 
