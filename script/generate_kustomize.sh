f469a4373edbd26b65a9581b0720f2543d33f2ab
Ketan Umare <kumare@lyft.com>
2020-09-23 16:04:11 -0700

    Update kustomize

--- script/generate_kustomize.sh
+++ script/generate_kustomize.sh
@@ -12,3 +12,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 for deployment in ${DEPLOYMENT}; do
     kustomize build kustomize/overlays/${deployment} > ${DIR}/../deployment/${deployment}/flyte_generated.yaml
 done
+
+# This section is used by GitHub workflow to ensure that the generation step was run
+if [ -n "$DELTA_CHECK" ]; then
+  DIRTY=$(git status --porcelain)
+  if [ -n "$DIRTY" ]; then
+    echo "FAILED: kustomize code updated without commiting generated code."
+    echo "Ensure make kustomize has run and all changes are committed."
+    DIFF=$(git diff)
+    echo "diff detected: $DIFF"
+    DIFF=$(git diff --name-only)
+    echo "files different: $DIFF"
+    exit 1
+  else
+    echo "SUCCESS: Generated code is up to date."
+  fi
+fi
