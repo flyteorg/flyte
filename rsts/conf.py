6d36da11acbd0d1f3d4e86e413924b8732164ff3
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2020-10-15 15:36:01 -0700

    Adding logo to sphinx docs (#556)

--- rsts/conf.py
+++ rsts/conf.py
@@ -93,6 +93,12 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "flyte_lockup_on_dark.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
