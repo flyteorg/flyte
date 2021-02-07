a0561024f6edc82699f004410508c91208fbc0f5
pmahindrakar-oss <77798312+pmahindrakar-oss@users.noreply.github.com>
2021-02-07 20:43:43 +0530

    Archive and Update project command (#24)

---
+++ flytectl/cmd/update/project_test.go
@@ -0,0 +1,130 @@
+package update
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"github.com/lyft/flytectl/cmd/config"
+	cmdCore "github.com/lyft/flytectl/cmd/core"
+	"github.com/lyft/flyteidl/clients/go/admin/mocks"
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/admin"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"log"
+	"os"
+	"testing"
+)
+
+const projectValue = "dummyProject"
+
+var (
+	reader               *os.File
+	writer               *os.File
+	err                  error
+	ctx                  context.Context
+	mockClient           *mocks.AdminServiceClient
+	mockOutStream        io.Writer
+	args                 []string
+	cmdCtx               cmdCore.CommandContext
+	projectUpdateRequest *admin.Project
+	stdOut               *os.File
+	stderr               *os.File
+)
+
+func setup() {
+	reader, writer, err = os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	stdOut = os.Stdout
+	stderr = os.Stderr
+	os.Stdout = writer
+	os.Stderr = writer
+	log.SetOutput(writer)
+	config.GetConfig().Project = projectValue
+	mockClient = new(mocks.AdminServiceClient)
+	mockOutStream = writer
+	cmdCtx = cmdCore.NewCommandContext(mockClient, mockOutStream)
+	projectUpdateRequest = &admin.Project{
+		Id:    projectValue,
+		State: admin.Project_ACTIVE,
+	}
+}
+
+func teardownAndVerify(t *testing.T, expectedLog string) {
+	writer.Close()
+	os.Stdout = stdOut
+	os.Stderr = stderr
+	var buf bytes.Buffer
+	io.Copy(&buf, reader)
+	assert.Equal(t, expectedLog, buf.String())
+}
+
+func modifyProjectFlags(archiveProject *bool, newArchiveVal bool, activateProject *bool, newActivateVal bool) {
+	*archiveProject = newArchiveVal
+	*activateProject = newActivateVal
+}
+
+func TestActivateProjectFunc(t *testing.T) {
+	setup()
+	defer teardownAndVerify(t, "Project dummyProject updated to ACTIVE state\n")
+	modifyProjectFlags(&(projectConfig.ArchiveProject), false, &(projectConfig.ActivateProject), true)
+	mockClient.OnUpdateProjectMatch(ctx, projectUpdateRequest).Return(nil, nil)
+	updateProjectsFunc(ctx, args, cmdCtx)
+	mockClient.AssertCalled(t, "UpdateProject", ctx, projectUpdateRequest)
+}
+
+func TestActivateProjectFuncWithError(t *testing.T) {
+	setup()
+	defer teardownAndVerify(t, "Project dummyProject failed to get updated to ACTIVE state due to Error Updating Project\n")
+	modifyProjectFlags(&(projectConfig.ArchiveProject), false, &(projectConfig.ActivateProject), true)
+	mockClient.OnUpdateProjectMatch(ctx, projectUpdateRequest).Return(nil, errors.New("Error Updating Project"))
+	updateProjectsFunc(ctx, args, cmdCtx)
+	mockClient.AssertCalled(t, "UpdateProject", ctx, projectUpdateRequest)
+}
+
+func TestArchiveProjectFunc(t *testing.T) {
+	setup()
+	defer teardownAndVerify(t, "Project dummyProject updated to ARCHIVED state\n")
+	modifyProjectFlags(&(projectConfig.ArchiveProject), true, &(projectConfig.ActivateProject), false)
+	projectUpdateRequest := &admin.Project{
+		Id:    projectValue,
+		State: admin.Project_ARCHIVED,
+	}
+	mockClient.OnUpdateProjectMatch(ctx, projectUpdateRequest).Return(nil, nil)
+	err := updateProjectsFunc(ctx, args, cmdCtx)
+	assert.Nil(t, err)
+	mockClient.AssertCalled(t, "UpdateProject", ctx, projectUpdateRequest)
+}
+
+func TestArchiveProjectFuncWithError(t *testing.T) {
+	setup()
+	defer teardownAndVerify(t, "Project dummyProject failed to get updated to ARCHIVED state due to Error Updating Project\n")
+	modifyProjectFlags(&(projectConfig.ArchiveProject), true, &(projectConfig.ActivateProject), false)
+	projectUpdateRequest := &admin.Project{
+		Id:    projectValue,
+		State: admin.Project_ARCHIVED,
+	}
+	mockClient.OnUpdateProjectMatch(ctx, projectUpdateRequest).Return(nil, errors.New("Error Updating Project"))
+	updateProjectsFunc(ctx, args, cmdCtx)
+	mockClient.AssertCalled(t, "UpdateProject", ctx, projectUpdateRequest)
+}
+
+func TestEmptyProjectInput(t *testing.T) {
+	setup()
+	defer teardownAndVerify(t, "Project  not found\n")
+	config.GetConfig().Project = ""
+	modifyProjectFlags(&(projectConfig.ArchiveProject), false, &(projectConfig.ActivateProject), true)
+	mockClient.OnUpdateProjectMatch(ctx, projectUpdateRequest).Return(nil, nil)
+	updateProjectsFunc(ctx, args, cmdCtx)
+	mockClient.AssertNotCalled(t, "UpdateProject", ctx, projectUpdateRequest)
+}
+
+func TestInvalidInput(t *testing.T) {
+	setup()
+	defer teardownAndVerify(t, "Invalid state passed. Specify either activate or archive\n")
+	modifyProjectFlags(&(projectConfig.ArchiveProject), false, &(projectConfig.ActivateProject), false)
+	mockClient.OnUpdateProjectMatch(ctx, projectUpdateRequest).Return(nil, nil)
+	updateProjectsFunc(ctx, args, cmdCtx)
+	mockClient.AssertNotCalled(t, "UpdateProject", ctx, projectUpdateRequest)
+}
--- flytectl/docs/CONTRIBUTING.md
+++ flytectl/docs/CONTRIBUTING.md
@@ -4,12 +4,13 @@ A local cluster can be setup via --> https://lyft.github.io/flyte/administrator/
 
 Then, if having trouble connecting to local cluster see the following:
 
+
 #1) Find/Set/Verify gRPC port for your local Flyte service:
 
 FLYTECTL_GRPC_PORT=`kubectl get service -n flyte flyteadmin -o json | jq '.spec.ports[] | select(.name=="grpc").port'`
+
 #2) Setup Port forwarding: kubectl port-forward -n flyte service/flyteadmin 8081:$FLYTECTL_GRPC_PORT
 
-and #3) Update config line in https://github.com/lyft/flytectl/blob/master/config.yaml to dns:///localhost:8081
+#3) Update config line in https://github.com/lyft/flytectl/blob/master/config.yaml to dns:///localhost:8081
 
-
-
+#4) All new flags introduced for flytectl commands and subcommands should be camelcased. eg: bin/flytectl update project -p flytesnacks --activateProject
