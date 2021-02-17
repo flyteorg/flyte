a2c4dd3149379dd77212764d90d04e07a8b6581d
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2021-02-17 13:53:12 -0800

    Pygments style should default to material (#35)

--- flytectl/docs/source/conf.py
+++ flytectl/docs/source/conf.py
@@ -75,10 +75,6 @@ language = None
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = []
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
