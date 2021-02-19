1668019b9f38f12820e42328f55bbf8f4c85f775
Katrina Rogan <katrina@nuclyde.io>
2021-02-19 13:43:25 -0800

    Update customizing task resources with latest syntax (#114)

--- cookbook/core/remote_flyte/customizing_resources.py
+++ cookbook/core/remote_flyte/customizing_resources.py
@@ -2,38 +2,41 @@
 Customizing task resources like mem/cpu
 --------------------------------------------
 
-One of the reasons why you would want to use a hosted Flyte environment is due to the potential of leveraging CPU, memory and storage resources, far greater than whats available locally.
+One of the reasons why you would want to use a hosted Flyte environment is due to the potential of leveraging CPU,
+memory and storage resources, far greater than what's available locally.
 Flytekit makes it possible to specify these requirements declaratively and close to where the task itself is declared.
 
 """
-import typing
-
-from flytekit import task, workflow
-
 
 # %%
-# In this example the memory required by the function increases as the dataset size increases. For large datasets we may not be able to run locally. Thus we want to provide hints to flyte backend that we want to request for more memory.
-# This is done by simply decorating the task with the hints as shown in the following code sample. The following parameters are available to be changed for a task
+# In this example the memory required by the function increases as the dataset size increases.
+# For large datasets we may not be able to run locally. Thus we want to provide hints to flyte backend that we want to request for more memory.
+# This is done by simply decorating the task with the hints as shown in the following code sample.
 #
-# #. ``cpu_request`` minimum cpu requested
-# #. ``cpu_limit`` maximum cpu possible
-# #. ``memory_request`` min memory requested
-# #. ``memory_limit`` max memory possible
-# #. ``gpu_request`` min gpus requested
-# #. ``gpu_limit`` max gpus requested, they should be the same?,kub
+# Tasks can have ``requests`` and ``limits`` which mirror the native `equivalents in kubernetes <https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#requests-and-limits>`__
+# A task can possibly be allocated more resources than it requests, but never more than its limit.
+# Requests are treated as hints which are used to schedule your tasks on nodes with available resources, whereas limits
+# are hard constraints.
 #
-# The following resources are not yet supported, but coming soon
+# For either a request or limit, refer to the :py:class:`flytekit:flytekit.Resource` documentation.
 #
-# #. ``storage_request`` min storage requested
-# #. ``storage_limit`` max storage possible
+# The following attributes can be specified for a ``Resource``.
 #
-# The acutal values follow the `kubernetes convention <https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/>`_.
+# #. ``cpu``
+# #. ``mem``
+# #. ``gpu``
 #
-# .. warning::
+# The ``storage`` resources option is not yet supported, but coming soon
 #
-#    We are working on changing how the cpu-request, memory is configured. This API is likely to change in alpha-3. The changes will be minimal
-#
-@task(cpu_request="1", memory_request="2048")
+# The acutal values follow the `kubernetes convention <https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#resource-units-in-kubernetes>`_.
+
+
+import typing
+
+from flytekit import Resources, task, workflow
+
+
+@task(requests=Resources(cpu="1", mem="2048"), limits=Resources(cpu="2", mem="4096"))
 def count_unique_numbers(x: typing.List[int]) -> int:
     s = set()
     for i in x:
