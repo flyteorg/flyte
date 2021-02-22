9ea87b40c9d33c114acee98b2e1a053087f44945
Haytham Abuelfutuh <haytham@afutuh.com>
2021-02-22 13:58:35 -0800

    Build and Push Sandbox Docker Image (#735)
    * sandbox image
    
    * wip
    
    * wip
    
    * wip
    
    * first run experience
    
    * flyte-sandbox Dockerfile (#754)
    
    * flyte-sandbox Dockerfile
    
    * cleanup
    
    * cleanup
    
    * fix tags
    
    * test changes to github action workflow
    
    * wip
    
    * wip
    
    * test
    
    * test
    
    * fix
    
    * test
    
    * fix
    
    * Move dockerfile
    
    * update github workflows
    
    * increase timeout for flyte deployment
    
    * tweak timeouts and print error messages
    
    * fix
    
    * wait for flyte namespce before watching flyte deployments
    
    * remove timeout for kubernetes cluster in wait-for-flyte.sh
    
    * Update docker/sandbox/flyte-entrypoint-default.sh
    
    * Update docker/sandbox/flyte-entrypoint-dind.sh
    
    * Update docker/sandbox/wait-for-flyte.sh
    
    * Update docker/sandbox/wait-for-flyte.sh
    
    * Update docker/sandbox/wait-for-flyte.sh
    
    Co-authored-by: Jeev B <jeevb@users.noreply.github.com>

---
+++ docker/sandbox/flyte-entrypoint-default.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+trap 'pkill -P $$' EXIT
+
+# Start k3s
+echo "Starting k3s cluster..."
+k3s server --no-deploy=traefik --no-deploy=servicelb --no-deploy=local-storage --no-deploy=metrics-server &> /var/log/k3s.log &
+K3S_PID=$!
+timeout 600 sh -c "until k3s kubectl explain deployment &> /dev/null; do sleep 1; done" || ( echo >&2 "Timed out while waiting for the Kubernetes cluster to start"; exit 1 )
+echo "Done."
+
+# Deploy flyte
+echo "Deploying Flyte..."
+k3s kubectl apply -f /flyteorg/share/flyte_generated.yaml
+wait-for-flyte.sh
+
+wait ${K3S_PID}
