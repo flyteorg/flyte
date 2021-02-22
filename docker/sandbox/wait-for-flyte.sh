73e353af4b36be1d00cca6f8e3849c6e28c89d09
Jeev B <jeev.balakrishnan@freenome.com>
2021-02-22 10:54:56 -0800

    increase timeout for flyte deployment

--- docker/sandbox/wait-for-flyte.sh
+++ docker/sandbox/wait-for-flyte.sh
@@ -6,6 +6,6 @@ set -e
 timeout 180 sh -c "until k3s kubectl explain deployment &> /dev/null; do sleep 1; done"
 
 # Wait for flyte deployment
-k3s kubectl wait --for=condition=available deployment/datacatalog deployment/flyteadmin deployment/flyteconsole deployment/flytepropeller -n flyte --timeout=180s
+k3s kubectl wait --for=condition=available deployment/datacatalog deployment/flyteadmin deployment/flyteconsole deployment/flytepropeller -n flyte --timeout=10m
 
 timeout 180 sh -c 'until [[ $(k3s kubectl get daemonset envoy -n flyte -o jsonpath="{.status.numberReady}") -eq 1 ]]; do sleep 1; done'
