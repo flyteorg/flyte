45bcae166e3ebbbdaa31633ab91dcc1ff81bbeb3
Jeev B <jeev.balakrishnan@freenome.com>
2021-02-22 11:41:58 -0800

    wait for flyte namespce before watching flyte deployments

--- docker/sandbox/wait-for-flyte.sh
+++ docker/sandbox/wait-for-flyte.sh
@@ -5,7 +5,11 @@ set -e
 # Ensure cluster is up and running
 timeout 600 sh -c "until k3s kubectl explain deployment &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the Kubernetes cluster to start"; exit 1 )
 
+# Wait for Flyte namespace to be created. This is necessary for the next step.
+timeout 600 sh -c "until k3s kubectl get namespace flyte &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the Flyte namespace to be created"; exit 1 )
+
 # Wait for flyte deployment
 k3s kubectl wait --for=condition=available deployment/datacatalog deployment/flyteadmin deployment/flyteconsole deployment/flytepropeller -n flyte --timeout=10m || ( echo >&2 "Timed out while waiting for the Flyte deployment to start"; exit 1 )
 
+# Wait for envoy proxy to become ready
 timeout 600 sh -c 'until [[ $(k3s kubectl get daemonset envoy -n flyte -o jsonpath="{.status.numberReady}") -eq 1 ]]; do sleep 1; done' || ( echo >&2 "Timed out while waiting for the Flyte envoy proxy to start"; exit 1 )
