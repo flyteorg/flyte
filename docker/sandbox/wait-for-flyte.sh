3b89e04351ebc6f7b9858b016fce6adf402cf553
Jeev B <jeevb@users.noreply.github.com>
2021-02-22 12:00:01 -0800

    flyte-sandbox Dockerfile (#754)
    * flyte-sandbox Dockerfile
    
    * cleanup
    
    * cleanup
    
    * fix tags
    
    * test changes to github action workflow
    
    * wip
    
    * wip
    
    * test
    
    * test
    
    * fix
    
    * test
    
    * fix
    
    * Move dockerfile
    
    * update github workflows
    
    * increase timeout for flyte deployment
    
    * tweak timeouts and print error messages
    
    * fix
    
    * wait for flyte namespce before watching flyte deployments
    
    * remove timeout for kubernetes cluster in wait-for-flyte.sh

---
+++ docker/sandbox/wait-for-flyte.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# Ensure cluster is up and running. We don't need a timeout here, since the container
+# itself will exit with the appropriate error message if the kubernetes cluster is not
+# up within the specified timeout.
+until k3s kubectl explain deployment &> /dev/null; do sleep 1; done
+
+# Wait for Flyte namespace to be created. This is necessary for the next step.
+timeout 600 sh -c "until k3s kubectl get namespace flyte &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the Flyte namespace to be created"; exit 1 )
+
+# Wait for flyte deployment
+k3s kubectl wait --for=condition=available deployment/datacatalog deployment/flyteadmin deployment/flyteconsole deployment/flytepropeller -n flyte --timeout=10m || ( echo >&2 "Timed out while waiting for the Flyte deployment to start"; exit 1 )
+
+# Wait for envoy proxy to become ready
+timeout 600 sh -c 'until [[ $(k3s kubectl get daemonset envoy -n flyte -o jsonpath="{.status.numberReady}") -eq 1 ]]; do sleep 1; done' || ( echo >&2 "Timed out while waiting for the Flyte envoy proxy to start"; exit 1 )
