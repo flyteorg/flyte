993a907dcae74b3312b44eb8c44eba08197b3f6e
Haytham Abuelfutuh <haytham@afutuh.com>
2021-03-10 09:26:10 -0800

    Allow K3s K8s Api Port to be overriden (#812)
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- docker/sandbox/flyte-entrypoint-default.sh
+++ docker/sandbox/flyte-entrypoint-default.sh
@@ -6,7 +6,8 @@ trap 'pkill -P $$' EXIT
 
 # Start k3s
 echo "Starting k3s cluster..."
-k3s server --no-deploy=traefik --no-deploy=servicelb --no-deploy=local-storage --no-deploy=metrics-server &> /var/log/k3s.log &
+KUBERNETES_API_PORT=${KUBERNETES_API_PORT:-6443}
+k3s server --no-deploy=traefik --no-deploy=servicelb --no-deploy=local-storage --no-deploy=metrics-server --https-listen-port=${KUBERNETES_API_PORT} &> /var/log/k3s.log &
 K3S_PID=$!
 timeout 600 sh -c "until k3s kubectl explain deployment &> /dev/null; do sleep 1; done" || ( echo >&2 "Timed out while waiting for the Kubernetes cluster to start"; exit 1 )
 echo "Done."
