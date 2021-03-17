bff8a83a278ced228e52c503e50ad800c1ccd7ee
Yuvraj <yuvraj.yad001@gmail.com>
2021-03-17 16:39:26 +0530

    wip: added manual github workflow
    Signed-off-by: Yuvraj <yuvraj.yad001@gmail.com>

--- script/release.sh
+++ script/release.sh
@@ -7,7 +7,7 @@ DATACATALOG_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/datacatal
 FLYTECONSOLE_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flyteconsole/releases/latest" | jq -r .tag_name)
 FLYTEPROPELLER_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flytepropeller/releases/latest" | jq -r .tag_name)
 
-grep -rlZ FLYTEADMIN_TAG ../kustomize/overlays | xargs -0 sed -i 's/FLYTEADMIN_TAG/$FLYTEADMIN_TAG/g'
-grep -rlZ DATACATALOG_TAG ../kustomize/overlays | xargs -0 sed -i 's/DATACATALOG_TAG/$DATACATALOG_TAG/g'
-grep -rlZ FLYTECONSOLE_TAG ../kustomize/overlays | xargs -0 sed -i 's/FLYTECONSOLE_TAG/$FLYTECONSOLE_TAG/g'
-grep -rlZ FLYTEPROPELLER_TAG ../kustomize/overlays | xargs -0 sed -i 's/FLYTEPROPELLER_TAG/$FLYTEPROPELLER_TAG/g'
+grep -rlZ FLYTEADMIN_TAG ./kustomize/overlays | xargs -0 sed -i 's/FLYTEADMIN_TAG/${FLYTEADMIN_TAG}/g'
+grep -rlZ DATACATALOG_TAG ./kustomize/overlays | xargs -0 sed -i 's/DATACATALOG_TAG/${DATACATALOG_TAG}/g'
+grep -rlZ FLYTECONSOLE_TAG ./kustomize/overlays | xargs -0 sed -i 's/FLYTECONSOLE_TAG/${FLYTECONSOLE_TAG}/g'
+grep -rlZ FLYTEPROPELLER_TAG ./kustomize/overlays | xargs -0 sed -i 's/FLYTEPROPELLER_TAG/${FLYTEPROPELLER_TAG}/g'
