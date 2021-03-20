5f5a641adb5646369159b50f9e542578ed9c8e51
Yuvraj <yuvraj.yad001@gmail.com>
2021-03-20 14:42:09 +0530

    wip: revert version in kustomize
    Signed-off-by: Yuvraj <yuvraj.yad001@gmail.com>

--- script/release.sh
+++ script/release.sh
@@ -7,7 +7,7 @@ DATACATALOG_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/datacatal
 FLYTECONSOLE_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flyteconsole/releases/latest" | jq -r .tag_name)
 FLYTEPROPELLER_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flytepropeller/releases/latest" | jq -r .tag_name)
 
-grep -rlZ FLYTEADMIN_TAG ./kustomize/overlays | xargs -0 sed -i "s/FLYTEADMIN_TAG/${FLYTEADMIN_TAG}/g"
-grep -rlZ DATACATALOG_TAG ./kustomize/overlays | xargs -0 sed -i "s/DATACATALOG_TAG/${DATACATALOG_TAG}/g"
-grep -rlZ FLYTECONSOLE_TAG ./kustomize/overlays | xargs -0 sed -i "s/FLYTECONSOLE_TAG/${FLYTECONSOLE_TAG}/g"
-grep -rlZ FLYTEPROPELLER_TAG ./kustomize/overlays | xargs -0 sed -i "s/FLYTEPROPELLER_TAG/${FLYTEPROPELLER_TAG}/g"
+grep -rlZ "newTag:[^P]*# FLYTEADMIN_TAG" ./kustomize/overlays | xargs -0 sed -i "s/newTag:[^P]*# FLYTEADMIN_TAG/newTag: ${FLYTEADMIN_TAG} # FLYTEADMIN_TAG/g"
+grep -rlZ "newTag:[^P]*# DATACATALOG_TAG" ./kustomize/overlays | xargs -0 sed -i "s/newTag:[^P]*# DATACATALOG_TAG/newTag: ${DATACATALOG_TAG} # DATACATALOG_TAG/g"
+grep -rlZ "newTag:[^P]*# FLYTECONSOLE_TAG" ./kustomize/overlays | xargs -0 sed -i "s/newTag:[^P]*# FLYTECONSOLE_TAG/newTag: ${FLYTECONSOLE_TAG} # FLYTECONSOLE_TAG/g"
+grep -rlZ "newTag:[^P]*# FLYTEPROPELLER_TAG" ./kustomize/overlays | xargs -0 sed -i "s/newTag:[^P]*# FLYTEPROPELLER_TAG/newTag: ${FLYTEPROPELLER_TAG} # FLYTEPROPELLER_TAG/g"
