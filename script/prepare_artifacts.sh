12a7d24fd87d3a96e18f33aa5dabb5e6f1dc6852
yuvraj <evalsocket@gmail.com>
2021-04-28 21:13:56 +0530

    fix typo
    Signed-off-by: yuvraj <evalsocket@gmail.com>

--- script/prepare_artifacts.sh
+++ script/prepare_artifacts.sh
@@ -6,6 +6,6 @@ for file in ./deployment/**/flyte_generated.yaml; do
     if [ -f "$file" ]; then
         result=${file/#"./deployment/"}
         result=${result/%"/flyte_generated.yaml"}
-        cp $file "./release/flyte_${result}_menifest.yaml"
+        cp $file "./release/flyte_${result}_manifest.yaml"
     fi
 done
