60d107a0f324a7ec02d6cb6b4e0294bb2ece24ce
SandraGH5 <80421934+SandraGH5@users.noreply.github.com>
2021-04-29 13:01:09 -0700

    Update customizing_resources.py (#171)
    * Update customizing_resources.py
    
    * Update cookbook/core/remote_flyte/customizing_resources.py
    
    Co-authored-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Update customizing_resources.py
    
    * Update cookbook/core/remote_flyte/customizing_resources.py
    
    Co-authored-by: Klaus Azesberger <klaus.azesberger@gmail.com>
    
    * Update cookbook/core/remote_flyte/customizing_resources.py
    
    Co-authored-by: Klaus Azesberger <klaus.azesberger@gmail.com>
    
    * Update customizing_resources.py
    
    Co-authored-by: Haytham Abuelfutuh <haytham@afutuh.com>
    Co-authored-by: Klaus Azesberger <klaus.azesberger@gmail.com>

--- cookbook/core/remote_flyte/customizing_resources.py
+++ cookbook/core/remote_flyte/customizing_resources.py
@@ -1,21 +1,20 @@
 """
-Customizing task resources like mem/cpu
+Customizing Task Resources like mem/cpu
 --------------------------------------------
 
-One of the reasons why you would want to use a hosted Flyte environment is due to the potential of leveraging CPU,
-memory and storage resources, far greater than what's available locally.
+One of the reasons to use a hosted Flyte environment is the potential of leveraging CPU, memory and storage resources, far greater than what's available locally.
 Flytekit makes it possible to specify these requirements declaratively and close to where the task itself is declared.
 
 """
 
 # %%
-# In this example the memory required by the function increases as the dataset size increases.
-# For large datasets we may not be able to run locally. Thus we want to provide hints to flyte backend that we want to request for more memory.
+# In this example, the memory required by the function increases as the dataset size increases.
+# Large datasets may not be able to run locally, so we would want to provide hints to flyte backend to request for more memory.
 # This is done by simply decorating the task with the hints as shown in the following code sample.
 #
 # Tasks can have ``requests`` and ``limits`` which mirror the native `equivalents in kubernetes <https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#requests-and-limits>`__
 # A task can possibly be allocated more resources than it requests, but never more than its limit.
-# Requests are treated as hints which are used to schedule your tasks on nodes with available resources, whereas limits
+# Requests are treated as hints which are used to schedule tasks on nodes with available resources, whereas limits
 # are hard constraints.
 #
 # For either a request or limit, refer to the :py:class:`flytekit:flytekit.Resource` documentation.
@@ -26,7 +25,11 @@ Flytekit makes it possible to specify these requirements declaratively and close
 # #. ``mem``
 # #. ``gpu``
 #
-# The ``storage`` resources option is not yet supported, but coming soon
+# To ensure regular tasks that do not require GPUs are not scheduled on GPU nodes, a separate node group for GPU nodes can be configured with `taints <https://kubernetes.io/docs/concepts/scheduling-eviction/taint-and-toleration/>`_.
+#
+# To ensure tasks that do require GPUs get the needed tolerations on their pods, set up flytepropeller using the following `configuration <https://github.com/flyteorg/flytepropeller/blob/v0.10.5/config.yaml#L51,L56>`_. Make sure that this toleration config matches the taint config that you have configured to protect your gpu providing nodes from having to deal with regular non-gpu workloads (pods).
+#
+# The ``storage`` resources option is not yet supported, but coming soon.
 #
 # The acutal values follow the `kubernetes convention <https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#resource-units-in-kubernetes>`_.
 
