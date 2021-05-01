5a7ce84335c154d6b44f9149a87dc3c4992510ef
Samhita Alla <aallasamhita@gmail.com>
2021-05-01 12:37:23 +0530

    update boilerplate code (#6)
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

---
+++ flytecopilot/boilerplate/flyte/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# WARNING: THIS FILE IS MANAGED IN THE 'BOILERPLATE' REPO AND COPIED TO OTHER REPOSITORIES.
+# ONLY EDIT THIS FILE FROM WITHIN THE 'LYFT/BOILERPLATE' REPOSITORY:
+# 
+# TO OPT OUT OF UPDATES, SEE https://github.com/lyft/boilerplate/blob/master/Readme.rst
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# Clone the config.yml file
+echo "     - copying ${DIR}/config.yml to the root directory."
+cp ${DIR}/config.yml ${DIR}/../../.github/config.yml
