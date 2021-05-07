463230010cf672862c337bb7fed9b200e0ee668e
pmahindrakar-oss <77798312+pmahindrakar-oss@users.noreply.github.com>
2021-05-07 10:02:35 +0530

    Wait for deployments to be created using rollout before checking for availability (#991)
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- docker/sandbox/wait-for-flyte.sh
+++ docker/sandbox/wait-for-flyte.sh
@@ -12,6 +12,12 @@ until k3s kubectl explain deployment &> /dev/null; do sleep 1; done
 # Wait for Flyte namespace to be created. This is necessary for the next step.
 timeout 600 sh -c "until k3s kubectl get namespace flyte &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the Flyte namespace to be created"; exit 1 )
 
+# Wait for Flyte deployment to be created. This is necessary for the next step.
+timeout 600 sh -c "until k3s kubectl rollout status deployment datacatalog -n flyte  &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the datacatalog rollout to be created"; exit 1 )
+timeout 600 sh -c "until k3s kubectl rollout status deployment flyteadmin -n flyte  &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the flyteadmin rollout to be created"; exit 1 )
+timeout 600 sh -c "until k3s kubectl rollout status deployment flyteconsole -n flyte  &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the flyteconsole rollout to be created"; exit 1 )
+timeout 600 sh -c "until k3s kubectl rollout status deployment flytepropeller -n flyte  &> /dev/null; do sleep 1; done"  || ( echo >&2 "Timed out while waiting for the flytepropeller rollout to be created"; exit 1 )
+
 # Wait for flyte deployment
 k3s kubectl wait --for=condition=available deployment/datacatalog deployment/flyteadmin deployment/flyteconsole deployment/flytepropeller -n flyte --timeout=10m || ( echo >&2 "Timed out while waiting for the Flyte deployment to start"; exit 1 )
 
