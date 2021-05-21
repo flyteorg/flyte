092e5ec3d8289cd735e153b7e9600e3008df2849
SandraGH5 <80421934+SandraGH5@users.noreply.github.com>
2021-05-21 14:38:13 -0700

    Update access_cloud_resources.py (#228)
    * Update access_cloud_resources.py
    
    * Update cookbook/deployment/cluster/access_cloud_resources.py
    
    Co-authored-by: Niels Bantilan <niels.bantilan@gmail.com>

--- cookbook/deployment/cluster/access_cloud_resources.py
+++ cookbook/deployment/cluster/access_cloud_resources.py
@@ -1,8 +1,25 @@
 """
 Accessing Cloud Resources
---------------------------------------------
+-------------------------
 
-.. NOTE::
+.. TODO: add intro paragraph here
 
-    Coming soon 🛠
+**********************************
+Kubernetes ServiceAccount Examples
+**********************************
+
+Configure project-wide Kubernetes ServiceAccounts by adding the following to your config:
+
+.. code:: python
+
+   [auth]
+   kubernetes_service_account=my-kube-service-acct
+
+
+Alternatively, pass the role as an argument to ``create_launch_plan``:
+
+.. code:: python
+
+   my_lp = MyWorkflow.create_launch_plan(kubernetes_service_account='my-kube-service-acct')
+
 """
