2436be568a16bdde0248a2514277a9b7d2a2ff48
Yuvraj <10830562+evalsocket@users.noreply.github.com>
2021-05-28 00:37:02 +0530

    Added boilerplate automation (#7)
    Signed-off-by: Yuvraj <evalsocket@gmail.com>

---
+++ boilerplate/flyte/golang_support_tools/tools.go
@@ -0,0 +1,10 @@
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/alvaroloes/enumer"
+	_ "github.com/flyteorg/flytestdlib/cli/pflags"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/vektra/mockery/cmd/mockery"
+)
--- boilerplate/flyte/golangci_file/update.sh
+++ boilerplate/flyte/golangci_file/update.sh
@@ -9,6 +9,6 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# Clone the config.yml file
-echo "     - copying ${DIR}/config.yml to the root directory."
-cp "${DIR}"/config.yml "${DIR}"/../../.github/config.yml
+# Clone the .golangci file
+echo "     - copying ${DIR}/.golangci to the root directory."
+cp ${DIR}/.golangci.yml ${DIR}/../../../.golangci.yml
