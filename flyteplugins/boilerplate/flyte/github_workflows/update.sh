93f5e7414391f25de7224b473a8ed5346e4e936c
Yuvraj <10830562+evalsocket@users.noreply.github.com>
2021-05-28 00:37:45 +0530

    Added boilerplate automation (#174)
    Signed-off-by: Yuvraj <evalsocket@gmail.com>

--- flyteplugins/boilerplate/flyte/github_workflows/update.sh
+++ flyteplugins/boilerplate/flyte/github_workflows/update.sh
@@ -2,13 +2,15 @@
 
 # WARNING: THIS FILE IS MANAGED IN THE 'BOILERPLATE' REPO AND COPIED TO OTHER REPOSITORIES.
 # ONLY EDIT THIS FILE FROM WITHIN THE 'FLYTEORG/BOILERPLATE' REPOSITORY:
-# 
+#
 # TO OPT OUT OF UPDATES, SEE https://github.com/flyteorg/boilerplate/blob/master/Readme.rst
 
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# Clone the config.yml file
-echo "     - copying ${DIR}/config.yml to the root directory."
-cp "${DIR}"/config.yml "${DIR}"/../../.github/config.yml
+mkdir -p ${DIR}/../../../.github/workflows
+
+echo "     - generating github action workflows in root directory."
+sed -e "s/{{REPOSITORY}}/${REPOSITORY}/g" ${DIR}/master.yml > ${DIR}/../../../.github/workflows/master.yml
+sed -e "s/{{REPOSITORY}}/${REPOSITORY}/g" ${DIR}/pull_request.yml > ${DIR}/../../../.github/workflows/pull_request.yml
