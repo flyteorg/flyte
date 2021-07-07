d2aa29e1a567462059026cca3b30e250eb286589
Haytham Abuelfutuh <haytham@afutuh.com>
2021-07-07 13:31:24 -0700

    Milestone 0.15.0 release (#1218)
    * Milestone 0.15.0 release
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Update docs version
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Update flytetools image sha
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- rsts/conf.py
+++ rsts/conf.py
@@ -29,7 +29,7 @@ author = u'Flyte'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.14.0'
+release = u'0.15.0'
 
 # -- General configuration ---------------------------------------------------
 
