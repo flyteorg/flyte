960cbcc71aacb1ce990011e69556913b24ec3231
Katrina Rogan <katrina@nuclyde.io>
2021-07-07 16:02:11 -0700

    Add UnmarshalStructToObj to marshal utils (#90)
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- flytestdlib/utils/marshal_utils_test.go
+++ flytestdlib/utils/marshal_utils_test.go
@@ -3,12 +3,15 @@ package utils
 import (
 	"testing"
 
+	"k8s.io/apimachinery/pkg/util/json"
+
 	"github.com/go-test/deep"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/flyteorg/flytestdlib/utils/prototest"
 	"github.com/golang/protobuf/proto"
 	structpb "github.com/golang/protobuf/ptypes/struct"
+	v1 "k8s.io/api/core/v1"
 )
 
 type SimpleType struct {
@@ -137,4 +140,47 @@ func TestMarshalPbToStruct(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestUnmarshalStructToObj(t *testing.T) {
+	t.Run("no nil structs allowed", func(t *testing.T) {
+		var podSpec v1.PodSpec
+		err := UnmarshalStructToObj(nil, &podSpec)
+		assert.EqualError(t, err, "nil Struct Object passed")
+	})
+	podSpec := v1.PodSpec{
+		Containers: []v1.Container{
+			{
+				Name: "a container",
+			},
+			{
+				Name: "another container",
+			},
+		},
+	}
+
+	b, err := json.Marshal(podSpec)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	structObj := &structpb.Struct{}
+	if err := json.Unmarshal(b, structObj); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("no nil pointers as obj allowed", func(t *testing.T) {
+		var nilPodspec *v1.PodSpec
+		err := UnmarshalStructToObj(structObj, nilPodspec)
+		assert.EqualError(t, err, "json: Unmarshal(nil *v1.PodSpec)")
+	})
+
+	t.Run("happy case", func(t *testing.T) {
+		var podSpecObj v1.PodSpec
+		err := UnmarshalStructToObj(structObj, &podSpecObj)
+		assert.NoError(t, err)
+		if diff := deep.Equal(podSpecObj, podSpec); diff != nil {
+			t.Errorf("UnmarshalStructToObj() got = %v, want %v, diff: %v", podSpecObj, podSpec, diff)
+		}
+	})
 }
