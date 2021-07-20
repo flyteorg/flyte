c435291f00e6f4840e57b747b8f3303c8b8d88b5
Samhita Alla <aallasamhita@gmail.com>
2021-07-20 09:40:58 +0530

    Update code of conduct (#221)
    * Update code of conduct
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * boilerplate
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- boilerplate/flyte/code_of_conduct/update.sh
+++ boilerplate/flyte/code_of_conduct/update.sh
@@ -5,4 +5,8 @@
 # 
 # TO OPT OUT OF UPDATES, SEE https://github.com/flyteorg/boilerplate/blob/master/Readme.rst
 
-goimports -w $(find . -type f -name '*.go' -not -path "./vendor/*" -not -path "./pkg/client/*" -not -path "./boilerplate/*")
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+cp ${DIR}/CODE_OF_CONDUCT.md ${DIR}/../../../CODE_OF_CONDUCT.md
