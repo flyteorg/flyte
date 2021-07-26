478f363b26c2af59bbd75d2197234945909d8661
Yuvraj <code@evalsocket.dev>
2021-07-26 21:05:11 +0530

    Added pflag support in cli (#11)
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- cmd/root.go
+++ cmd/root.go
@@ -162,17 +162,19 @@ func NewDataCommand() *cobra.Command {
 	return command
 }
 
-func (r *RootOptions) initConfig(_ *cobra.Command, _ []string) error {
+func (r *RootOptions) initConfig(cmd *cobra.Command, _ []string) error {
 	r.configAccessor = viper.NewAccessor(config.Options{
 		StrictMode:  true,
 		SearchPaths: []string{r.cfgFile},
 	})
 
+	// persistent flags were initially bound to the root command so we must bind to the same command to avoid
+	r.configAccessor.InitializePflags(cmd.PersistentFlags())
+
 	err := r.configAccessor.UpdateConfig(context.TODO())
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
