d5c8822850eab1f359fc023cbb67cac99eb1b1ef
Samhita Alla <aallasamhita@gmail.com>
2021-07-29 09:46:21 +0530

    Update code of conduct (#175)
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- boilerplate/flyte/code_of_conduct/update.sh
+++ boilerplate/flyte/code_of_conduct/update.sh
@@ -9,7 +9,4 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# Clone the config.yml file
-echo "     - copying ${DIR}/config.yml to the root directory."
-cp "${DIR}"/config.yml "${DIR}"/../../../.github/config.yml
-
+cp ${DIR}/CODE_OF_CONDUCT.md ${DIR}/../../../CODE_OF_CONDUCT.md
