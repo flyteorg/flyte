aba015bd3f945ac3b9763039a6a09c49606fb82f
Samhita Alla <aallasamhita@gmail.com>
2021-07-29 09:46:56 +0530

    Update code of conduct (#46)
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- datacatalog/boilerplate/flyte/golang_dockerfile/update.sh
+++ datacatalog/boilerplate/flyte/golang_dockerfile/update.sh
@@ -5,4 +5,9 @@
 # 
 # TO OPT OUT OF UPDATES, SEE https://github.com/flyteorg/boilerplate/blob/master/Readme.rst
 
-goimports -w $(find . -type f -name '*.go' -not -path "./vendor/*" -not -path "./pkg/client/*" -not -path "./boilerplate/*")
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo "     - generating Dockerfile in root directory."
+sed -e "s/{{REPOSITORY}}/${REPOSITORY}/g" ${DIR}/Dockerfile.GoTemplate > ${DIR}/../../../Dockerfile
--- datacatalog/boilerplate/flyte/golang_support_tools/go.mod
+++ datacatalog/boilerplate/flyte/golang_support_tools/go.mod
@@ -6,7 +6,10 @@ require (
 	github.com/alvaroloes/enumer v1.1.2
 	github.com/flyteorg/flytestdlib v0.3.22
 	github.com/golangci/golangci-lint v1.38.0
+	github.com/pseudomuto/protoc-gen-doc v1.4.1 // indirect
 	github.com/vektra/mockery v0.0.0-20181123154057-e78b021dcbb5
 )
 
 replace github.com/vektra/mockery => github.com/enghabu/mockery v0.0.0-20191009061720-9d0c8670c2f0
+
+replace github.com/pseudomuto/protoc-gen-doc => github.com/flyteorg/protoc-gen-doc v1.4.2
