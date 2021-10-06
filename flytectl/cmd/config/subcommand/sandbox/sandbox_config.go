6f9a2aa918b39988c76b6de6e92f6c7c7b0fc602
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2021-10-06 14:39:46 -0700

    flytectl sandbox start --image xyz (#195)
    * flytectl sandbox start --image xyz
    
    Sandbox can now use a custom image
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * fixed unit test
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * updated comments
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- flytectl/cmd/config/subcommand/sandbox/sandbox_config.go
+++ flytectl/cmd/config/subcommand/sandbox/sandbox_config.go
@@ -9,8 +9,13 @@ var (
 type Config struct {
 	Source string `json:"source" pflag:",Path of your source code"`
 
-	// Flytectl sandbox only support flyte version available in Github release https://github.com/flyteorg/flyte/tags
+	// Flytectl sandbox only supports flyte version available in Github release https://github.com/flyteorg/flyte/tags
 	// Flytectl sandbox will only work for v0.10.0+
-	// Default value dind represent the latest release
-	Version string `json:"version" pflag:",Version of flyte. Only support v0.10.0+ flyte release"`
+	// Default value dind represents the latest release
+	Version string `json:"version" pflag:",Version of flyte. Only supports flyte releases greater than v0.10.0"`
+
+	// Optionally it is possible to specify a specific fqn for the docker image with the tag. This should be
+	// Flyte compliant sandbox image. Usually useful, if you want to push the image to your own registry and relaunch
+	// from there.
+	Image string `json:"image" pflag:",Optional. Provide a fully qualified path to a Flyte compliant docker image."`
 }
