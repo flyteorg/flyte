7d00aea185b55357e60cfcdc523e4cf15fdf804a
Yuvraj <code@evalsocket.dev>
2021-10-19 03:46:36 +0530

    Added mpi backend plugin (#196)
    * Added mpi plugin
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * Remove launcher log
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * More changes
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * Fix test and lint
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * fix gomod issue
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * More changes
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * make lint
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * Fix unit test
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * Added more unit test
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * Fix unit test
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    * Added documentation
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>

---
+++ go/tasks/plugins/k8s/kfoperators/mpi/mpi.go
@@ -0,0 +1,166 @@
+package mpi
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/plugins"
+	flyteerr "github.com/flyteorg/flyteplugins/go/tasks/errors"
+	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery"
+	pluginsCore "github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/core"
+	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/flytek8s"
+	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/k8s"
+	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/utils"
+	"github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/kfoperators/common"
+	commonKf "github.com/kubeflow/common/pkg/apis/common/v1"
+	mpi "github.com/kubeflow/mpi-operator/v2/pkg/apis/kubeflow/v2beta1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type mpiOperatorResourceHandler struct {
+}
+
+// Sanity test that the plugin implements method of k8s.Plugin
+var _ k8s.Plugin = mpiOperatorResourceHandler{}
+
+func (mpiOperatorResourceHandler) GetProperties() k8s.PluginProperties {
+	return k8s.PluginProperties{}
+}
+
+// Defines a func to create a query object (typically just object and type meta portions) that's used to query k8s
+// resources.
+func (mpiOperatorResourceHandler) BuildIdentityResource(ctx context.Context, taskCtx pluginsCore.TaskExecutionMetadata) (client.Object, error) {
+	return &mpi.MPIJob{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       mpi.Kind,
+			APIVersion: mpi.SchemeGroupVersion.String(),
+		},
+	}, nil
+}
+
+// Defines a func to create the full resource object that will be posted to k8s.
+func (mpiOperatorResourceHandler) BuildResource(ctx context.Context, taskCtx pluginsCore.TaskExecutionContext) (client.Object, error) {
+	taskTemplate, err := taskCtx.TaskReader().Read(ctx)
+
+	if err != nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "unable to fetch task specification [%v]", err.Error())
+	} else if taskTemplate == nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "nil task specification")
+	}
+
+	mpiTaskExtraArgs := plugins.DistributedMPITrainingTask{}
+	err = utils.UnmarshalStruct(taskTemplate.GetCustom(), &mpiTaskExtraArgs)
+	if err != nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "invalid TaskSpecification [%v], Err: [%v]", taskTemplate.GetCustom(), err.Error())
+	}
+
+	workers := mpiTaskExtraArgs.GetNumWorkers()
+	launcherReplicas := mpiTaskExtraArgs.GetNumLauncherReplicas()
+	slots := mpiTaskExtraArgs.GetSlots()
+
+	podSpec, err := flytek8s.ToK8sPodSpec(ctx, taskCtx)
+	if err != nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "Unable to create pod spec: [%v]", err.Error())
+	}
+
+	// workersPodSpec is deepCopy of podSpec submitted by flyte
+	// WorkerPodSpec doesn't need any Argument & command. It will be trigger from launcher pod
+	workersPodSpec := podSpec.DeepCopy()
+
+	for k := range workersPodSpec.Containers {
+		workersPodSpec.Containers[k].Args = []string{}
+		workersPodSpec.Containers[k].Command = []string{}
+	}
+
+	if workers == 0 {
+		return nil, fmt.Errorf("number of worker should be more then 1 ")
+	}
+	if launcherReplicas == 0 {
+		return nil, fmt.Errorf("number of launch worker should be more then 1")
+	}
+
+	jobSpec := mpi.MPIJobSpec{
+		SlotsPerWorker: &slots,
+		MPIReplicaSpecs: map[mpi.MPIReplicaType]*commonKf.ReplicaSpec{
+			mpi.MPIReplicaTypeLauncher: &commonKf.ReplicaSpec{
+				Replicas: &launcherReplicas,
+				Template: v1.PodTemplateSpec{
+					Spec: *podSpec,
+				},
+				RestartPolicy: commonKf.RestartPolicyNever,
+			},
+			mpi.MPIReplicaTypeWorker: &commonKf.ReplicaSpec{
+				Replicas: &workers,
+				Template: v1.PodTemplateSpec{
+					Spec: *workersPodSpec,
+				},
+				RestartPolicy: commonKf.RestartPolicyNever,
+			},
+		},
+	}
+
+	job := &mpi.MPIJob{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       mpi.Kind,
+			APIVersion: mpi.SchemeGroupVersion.String(),
+		},
+		Spec: jobSpec,
+	}
+
+	return job, nil
+}
+
+// Analyzes the k8s resource and reports the status as TaskPhase. This call is expected to be relatively fast,
+// any operations that might take a long time (limits are configured system-wide) should be offloaded to the
+// background.
+func (mpiOperatorResourceHandler) GetTaskPhase(_ context.Context, pluginContext k8s.PluginContext, resource client.Object) (pluginsCore.PhaseInfo, error) {
+	var numWorkers, numLauncherReplicas *int32
+	app, ok := resource.(*mpi.MPIJob)
+	if !ok {
+		return pluginsCore.PhaseInfoUndefined, fmt.Errorf("failed to convert resource data type")
+	}
+
+	numWorkers = app.Spec.MPIReplicaSpecs[mpi.MPIReplicaTypeWorker].Replicas
+	numLauncherReplicas = app.Spec.MPIReplicaSpecs[mpi.MPIReplicaTypeLauncher].Replicas
+
+	taskLogs, err := common.GetLogs(common.MPITaskType, app.Name, app.Namespace,
+		*numWorkers, *numLauncherReplicas, 0)
+	if err != nil {
+		return pluginsCore.PhaseInfoUndefined, err
+	}
+	currentCondition, err := common.ExtractMPICurrentCondition(app.Status.Conditions)
+	if err != nil {
+		return pluginsCore.PhaseInfoUndefined, err
+	}
+
+	occurredAt := time.Now()
+	statusDetails, _ := utils.MarshalObjToStruct(app.Status)
+	taskPhaseInfo := pluginsCore.TaskInfo{
+		Logs:       taskLogs,
+		OccurredAt: &occurredAt,
+		CustomInfo: statusDetails,
+	}
+
+	return common.GetMPIPhaseInfo(currentCondition, occurredAt, taskPhaseInfo)
+
+}
+
+func init() {
+	if err := mpi.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+
+	pluginmachinery.PluginRegistry().RegisterK8sPlugin(
+		k8s.PluginEntry{
+			ID:                  common.MPITaskType,
+			RegisteredTaskTypes: []pluginsCore.TaskType{common.MPITaskType},
+			ResourceToWatch:     &mpi.MPIJob{},
+			Plugin:              mpiOperatorResourceHandler{},
+			IsDefault:           false,
+			DefaultForTaskTypes: []pluginsCore.TaskType{common.MPITaskType},
+		})
+}
---
+++ go/tasks/plugins/k8s/kfoperators/mpi/mpi_test.go
@@ -0,0 +1,387 @@
+package mpi
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/kfoperators/common"
+
+	"github.com/flyteorg/flyteplugins/go/tasks/logs"
+	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/k8s"
+	mpiOp "github.com/kubeflow/common/pkg/apis/common/v1"
+	mpi "github.com/kubeflow/mpi-operator/v2/pkg/apis/kubeflow/v2beta1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	"github.com/stretchr/testify/mock"
+
+	"github.com/flyteorg/flytestdlib/storage"
+
+	pluginsCore "github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/core"
+	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/utils"
+
+	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/core/mocks"
+
+	pluginIOMocks "github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/io/mocks"
+
+	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/core"
+	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/plugins"
+	"github.com/golang/protobuf/jsonpb"
+	structpb "github.com/golang/protobuf/ptypes/struct"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const testImage = "image://"
+const serviceAccount = "mpi_sa"
+const mpiID = "the job 1"
+const mpiID2 = "the job 2"
+
+var (
+	dummyEnvVars = []*core.KeyValuePair{
+		{Key: "Env_Var", Value: "Env_Val"},
+	}
+
+	testArgs = []string{
+		"test-args",
+	}
+
+	resourceRequirements = &corev1.ResourceRequirements{
+		Limits: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse("1000m"),
+			corev1.ResourceMemory: resource.MustParse("1Gi"),
+		},
+		Requests: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse("100m"),
+			corev1.ResourceMemory: resource.MustParse("512Mi"),
+		},
+	}
+
+	jobName      = "the-job"
+	jobNamespace = "mpi-namespace"
+)
+
+func dummyMPICustomObj(workers int32, launcher int32, slots int32) *plugins.DistributedMPITrainingTask {
+	return &plugins.DistributedMPITrainingTask{
+		NumWorkers:          workers,
+		NumLauncherReplicas: launcher,
+		Slots:               slots,
+	}
+}
+
+func dummyMPITaskTemplate(id string, mpiCustomObj *plugins.DistributedMPITrainingTask) *core.TaskTemplate {
+
+	mpiObjJSON, err := utils.MarshalToString(mpiCustomObj)
+	if err != nil {
+		panic(err)
+	}
+
+	structObj := structpb.Struct{}
+
+	err = jsonpb.UnmarshalString(mpiObjJSON, &structObj)
+	if err != nil {
+		panic(err)
+	}
+
+	return &core.TaskTemplate{
+		Id:   &core.Identifier{Name: id},
+		Type: "container",
+		Target: &core.TaskTemplate_Container{
+			Container: &core.Container{
+				Image: testImage,
+				Args:  testArgs,
+				Env:   dummyEnvVars,
+			},
+		},
+		Custom: &structObj,
+	}
+}
+
+func dummyMPITaskContext(taskTemplate *core.TaskTemplate) pluginsCore.TaskExecutionContext {
+	taskCtx := &mocks.TaskExecutionContext{}
+	inputReader := &pluginIOMocks.InputReader{}
+	inputReader.OnGetInputPrefixPath().Return(storage.DataReference("/input/prefix"))
+	inputReader.OnGetInputPath().Return(storage.DataReference("/input"))
+	inputReader.OnGetMatch(mock.Anything).Return(&core.LiteralMap{}, nil)
+	taskCtx.OnInputReader().Return(inputReader)
+
+	outputReader := &pluginIOMocks.OutputWriter{}
+	outputReader.OnGetOutputPath().Return(storage.DataReference("/data/outputs.pb"))
+	outputReader.OnGetOutputPrefixPath().Return(storage.DataReference("/data/"))
+	outputReader.OnGetRawOutputPrefix().Return(storage.DataReference(""))
+	taskCtx.OnOutputWriter().Return(outputReader)
+
+	taskReader := &mocks.TaskReader{}
+	taskReader.OnReadMatch(mock.Anything).Return(taskTemplate, nil)
+	taskCtx.OnTaskReader().Return(taskReader)
+
+	tID := &mocks.TaskExecutionID{}
+	tID.OnGetID().Return(core.TaskExecutionIdentifier{
+		NodeExecutionId: &core.NodeExecutionIdentifier{
+			ExecutionId: &core.WorkflowExecutionIdentifier{
+				Name:    "my_name",
+				Project: "my_project",
+				Domain:  "my_domain",
+			},
+		},
+	})
+	tID.OnGetGeneratedName().Return("some-acceptable-name")
+
+	resources := &mocks.TaskOverrides{}
+	resources.OnGetResources().Return(resourceRequirements)
+
+	taskExecutionMetadata := &mocks.TaskExecutionMetadata{}
+	taskExecutionMetadata.OnGetTaskExecutionID().Return(tID)
+	taskExecutionMetadata.OnGetNamespace().Return("test-namespace")
+	taskExecutionMetadata.OnGetAnnotations().Return(map[string]string{"annotation-1": "val1"})
+	taskExecutionMetadata.OnGetLabels().Return(map[string]string{"label-1": "val1"})
+	taskExecutionMetadata.OnGetOwnerReference().Return(v1.OwnerReference{
+		Kind: "node",
+		Name: "blah",
+	})
+	taskExecutionMetadata.OnIsInterruptible().Return(true)
+	taskExecutionMetadata.OnGetOverrides().Return(resources)
+	taskExecutionMetadata.OnGetK8sServiceAccount().Return(serviceAccount)
+	taskExecutionMetadata.OnGetPlatformResources().Return(&corev1.ResourceRequirements{})
+	taskCtx.OnTaskExecutionMetadata().Return(taskExecutionMetadata)
+	return taskCtx
+}
+
+func dummyMPIJobResource(mpiResourceHandler mpiOperatorResourceHandler,
+	workers int32, launcher int32, slots int32, conditionType mpiOp.JobConditionType) *mpi.MPIJob {
+	var jobConditions []mpiOp.JobCondition
+
+	now := time.Now()
+
+	jobCreated := mpiOp.JobCondition{
+		Type:    mpiOp.JobCreated,
+		Status:  corev1.ConditionTrue,
+		Reason:  "MPICreated",
+		Message: "MPIJob the-job is created.",
+		LastUpdateTime: v1.Time{
+			Time: now,
+		},
+		LastTransitionTime: v1.Time{
+			Time: now,
+		},
+	}
+	jobRunningActive := mpiOp.JobCondition{
+		Type:    mpiOp.JobRunning,
+		Status:  corev1.ConditionTrue,
+		Reason:  "MPIJobRunning",
+		Message: "MPIJob the-job is running.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(time.Minute),
+		},
+	}
+	jobRunningInactive := *jobRunningActive.DeepCopy()
+	jobRunningInactive.Status = corev1.ConditionFalse
+	jobSucceeded := mpiOp.JobCondition{
+		Type:    mpiOp.JobSucceeded,
+		Status:  corev1.ConditionTrue,
+		Reason:  "MPIJobSucceeded",
+		Message: "MPIJob the-job is successfully completed.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+	}
+	jobFailed := mpiOp.JobCondition{
+		Type:    mpiOp.JobFailed,
+		Status:  corev1.ConditionTrue,
+		Reason:  "MPIJobFailed",
+		Message: "MPIJob the-job is failed.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+	}
+	jobRestarting := mpiOp.JobCondition{
+		Type:    mpiOp.JobRestarting,
+		Status:  corev1.ConditionTrue,
+		Reason:  "MPIJobRestarting",
+		Message: "MPIJob the-job is restarting because some replica(s) failed.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(3 * time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(3 * time.Minute),
+		},
+	}
+
+	switch conditionType {
+	case mpiOp.JobCreated:
+		jobConditions = []mpiOp.JobCondition{
+			jobCreated,
+		}
+	case mpiOp.JobRunning:
+		jobConditions = []mpiOp.JobCondition{
+			jobCreated,
+			jobRunningActive,
+		}
+	case mpiOp.JobSucceeded:
+		jobConditions = []mpiOp.JobCondition{
+			jobCreated,
+			jobRunningInactive,
+			jobSucceeded,
+		}
+	case mpiOp.JobFailed:
+		jobConditions = []mpiOp.JobCondition{
+			jobCreated,
+			jobRunningInactive,
+			jobFailed,
+		}
+	case mpiOp.JobRestarting:
+		jobConditions = []mpiOp.JobCondition{
+			jobCreated,
+			jobRunningInactive,
+			jobFailed,
+			jobRestarting,
+		}
+	}
+
+	mpiObj := dummyMPICustomObj(workers, launcher, slots)
+	taskTemplate := dummyMPITaskTemplate(mpiID, mpiObj)
+	resource, err := mpiResourceHandler.BuildResource(context.TODO(), dummyMPITaskContext(taskTemplate))
+	if err != nil {
+		panic(err)
+	}
+
+	return &mpi.MPIJob{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      jobName,
+			Namespace: jobNamespace,
+		},
+		Spec: resource.(*mpi.MPIJob).Spec,
+		Status: mpiOp.JobStatus{
+			Conditions:        jobConditions,
+			ReplicaStatuses:   nil,
+			StartTime:         nil,
+			CompletionTime:    nil,
+			LastReconcileTime: nil,
+		},
+	}
+}
+
+func TestBuildResourceMPI(t *testing.T) {
+	mpiResourceHandler := mpiOperatorResourceHandler{}
+
+	mpiObj := dummyMPICustomObj(100, 50, 1)
+	taskTemplate := dummyMPITaskTemplate(mpiID2, mpiObj)
+
+	resource, err := mpiResourceHandler.BuildResource(context.TODO(), dummyMPITaskContext(taskTemplate))
+	assert.NoError(t, err)
+	assert.NotNil(t, resource)
+
+	mpiJob, ok := resource.(*mpi.MPIJob)
+	assert.True(t, ok)
+	assert.Equal(t, int32(50), *mpiJob.Spec.MPIReplicaSpecs[mpi.MPIReplicaTypeLauncher].Replicas)
+	assert.Equal(t, int32(100), *mpiJob.Spec.MPIReplicaSpecs[mpi.MPIReplicaTypeWorker].Replicas)
+	assert.Equal(t, int32(1), *mpiJob.Spec.SlotsPerWorker)
+
+	for _, replicaSpec := range mpiJob.Spec.MPIReplicaSpecs {
+		for _, container := range replicaSpec.Template.Spec.Containers {
+			assert.Equal(t, resourceRequirements.Requests, container.Resources.Requests)
+			assert.Equal(t, resourceRequirements.Limits, container.Resources.Limits)
+		}
+	}
+}
+
+func TestBuildResourceMPIForWrongInput(t *testing.T) {
+	mpiResourceHandler := mpiOperatorResourceHandler{}
+
+	mpiObj := dummyMPICustomObj(0, 0, 1)
+	taskTemplate := dummyMPITaskTemplate(mpiID, mpiObj)
+
+	_, err := mpiResourceHandler.BuildResource(context.TODO(), dummyMPITaskContext(taskTemplate))
+	assert.Error(t, err)
+
+	mpiObj = dummyMPICustomObj(1, 0, 1)
+	taskTemplate = dummyMPITaskTemplate(mpiID2, mpiObj)
+
+	_, err = mpiResourceHandler.BuildResource(context.TODO(), dummyMPITaskContext(taskTemplate))
+	assert.Error(t, err)
+
+	mpiObj = dummyMPICustomObj(1, 1, 1)
+	taskTemplate = dummyMPITaskTemplate(mpiID2, mpiObj)
+
+	resource, err := mpiResourceHandler.BuildResource(context.TODO(), dummyMPITaskContext(taskTemplate))
+	app, ok := resource.(*mpi.MPIJob)
+	assert.Nil(t, err)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, []string{}, app.Spec.MPIReplicaSpecs[mpi.MPIReplicaTypeWorker].Template.Spec.Containers[0].Command)
+	assert.Equal(t, []string{}, app.Spec.MPIReplicaSpecs[mpi.MPIReplicaTypeWorker].Template.Spec.Containers[0].Args)
+}
+
+func TestGetTaskPhase(t *testing.T) {
+	mpiResourceHandler := mpiOperatorResourceHandler{}
+	ctx := context.TODO()
+
+	dummyMPIJobResourceCreator := func(conditionType mpiOp.JobConditionType) *mpi.MPIJob {
+		return dummyMPIJobResource(mpiResourceHandler, 2, 1, 1, conditionType)
+	}
+
+	taskPhase, err := mpiResourceHandler.GetTaskPhase(ctx, nil, dummyMPIJobResourceCreator(mpiOp.JobCreated))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseQueued, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = mpiResourceHandler.GetTaskPhase(ctx, nil, dummyMPIJobResourceCreator(mpiOp.JobRunning))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseRunning, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = mpiResourceHandler.GetTaskPhase(ctx, nil, dummyMPIJobResourceCreator(mpiOp.JobSucceeded))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseSuccess, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = mpiResourceHandler.GetTaskPhase(ctx, nil, dummyMPIJobResourceCreator(mpiOp.JobFailed))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseRetryableFailure, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = mpiResourceHandler.GetTaskPhase(ctx, nil, dummyMPIJobResourceCreator(mpiOp.JobRestarting))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseRunning, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+}
+
+func TestGetLogs(t *testing.T) {
+	assert.NoError(t, logs.SetLogConfig(&logs.LogConfig{
+		IsKubernetesEnabled: true,
+		KubernetesURL:       "k8s.com",
+	}))
+
+	workers := int32(2)
+	launcher := int32(1)
+	slots := int32(1)
+
+	mpiResourceHandler := mpiOperatorResourceHandler{}
+	mpiJob := dummyMPIJobResource(mpiResourceHandler, workers, launcher, slots, mpiOp.JobRunning)
+	jobLogs, err := common.GetLogs(common.MPITaskType, mpiJob.Name, mpiJob.Namespace, workers, launcher, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(jobLogs))
+	assert.Equal(t, fmt.Sprintf("k8s.com/#!/log/%s/%s-worker-0/pod?namespace=mpi-namespace", jobNamespace, jobName), jobLogs[0].Uri)
+	assert.Equal(t, fmt.Sprintf("k8s.com/#!/log/%s/%s-worker-1/pod?namespace=mpi-namespace", jobNamespace, jobName), jobLogs[1].Uri)
+}
+
+func TestGetProperties(t *testing.T) {
+	mpiResourceHandler := mpiOperatorResourceHandler{}
+	expected := k8s.PluginProperties{}
+	assert.Equal(t, expected, mpiResourceHandler.GetProperties())
+}
