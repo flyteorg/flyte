bc05d5ab812f15bc96a088e8c9c9b6ed50b17cf5
Niels Bantilan <niels.bantilan@gmail.com>
2021-10-19 16:17:42 -0400

    update task cache docs (#459)
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- cookbook/core/flyte_basics/task_cache.py
+++ cookbook/core/flyte_basics/task_cache.py
@@ -49,10 +49,11 @@ def square(n: int) -> int:
 # %%
 # If in a subsequent code update, we update the signature of the task to return the original number along with the result, it'll automatically invalidate the cache (even though the cache version remains the same).
 #
-#   :py:func:`flytekit.task`
-#   @task(cache=True, cache_version="1.0")
-#   def square(n: int) -> (int, int):
-#       ...
+# .. code-block:: python
+#
+#    @task(cache=True, cache_version="1.0")
+#    def square(n: int) -> (int, int):
+#        ...
 
 # %%
 # .. note::
