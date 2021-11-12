6c82f6dfcdf3c418a43673db094e34f1e9f351c2
Kevin Su <pingsutw@apache.org>
2021-11-12 15:57:30 +0800

    Updated docs
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- script/generate_config_docs.sh
+++ script/generate_config_docs.sh
@@ -8,6 +8,7 @@ ROOT_DIR=${CUR_DIR}/..
 OUTPUT_DIR="${ROOT_DIR}"/rsts/deployment/cluster_config
 GOBIN=${GOPATH:-~/go}/bin
 
+# The version should same as https://github.com/flyteorg/flyte/blob/6b92b72f508d0603fa44153a4e30cf81be76adfd/script/release.sh#L5-L8
 FLYTEADMIN_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flyteadmin/releases/latest" | jq -r .tag_name)
 FLYTEPROPELLER_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flytepropeller/releases/latest" | jq -r .tag_name)
 
@@ -17,16 +18,28 @@ mv $GOBIN/cmd $GOBIN/flyteadmin
 mv $GOBIN/controller $GOBIN/flytepropeller
 
 output_config () {
-OUTPUT_PATH="${OUTPUT_DIR}"/$2_config.rst
+CONFIG_NAME=$1
+COMPONENT=$2
+OUTPUT_PATH=${OUTPUT_DIR}/${COMPONENT}_config.rst
 
-echo ".. _$2-config-specification:
+if [ -z "$CONFIG_NAME" ]; then
+  log_err "output_config CONFIG_NAME value not specified in arg1"
+  return 1
+fi
+
+if [ -z "$COMPONENT" ]; then
+  log_err "output_config COMPONENT value not specified in arg2"
+  return 1
+fi
+
+echo ".. _$COMPONENT-config-specification:
 
 #########################################
-Flyte $1 Configuration
+Flyte $CONFIG_NAME Configuration
 #########################################
 " > "${OUTPUT_PATH}"
 
-$GOBIN/$2 config docs >> "${OUTPUT_PATH}"
+$GOBIN/$COMPONENT config docs >> "${OUTPUT_PATH}"
 }
 
 output_config "Admin" flyteadmin
