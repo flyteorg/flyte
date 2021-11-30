190348cb15854e1d8d205a622254d7928e2cd517
Yuvraj <code@evalsocket.dev>
2021-11-30 07:56:00 +0530

    Replace image name in helm release
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- script/prepare_artifacts.sh
+++ script/prepare_artifacts.sh
@@ -25,11 +25,26 @@ helm dep update ./charts/flyte
 # bump latest release of flyte component in helm
 sed -i "s,tag:[^P]*# FLYTEADMIN_TAG,tag: ${VERSION} # FLYTEADMIN_TAG," ./charts/flyte/values.yaml
 sed -i "s,tag:[^P]*# FLYTEADMIN_TAG,tag: ${VERSION} # FLYTEADMIN_TAG," ./charts/flyte-core/values.yaml
+sed -i "s,repository:[^P]*# FLYTEADMIN_IMAGE,repository: cr.flyte.org/flyteorg/flyteadmin-release:${VERSION} # FLYTEADMIN_IMAGE," ./charts/flyte/values.yaml
+sed -i "s,repository:[^P]*# FLYTEADMIN_IMAGE,repository: cr.flyte.org/flyteorg/flyteadmin-release:${VERSION} # FLYTEADMIN_IMAGE," ./charts/flyte-core/values.yaml
+
 sed -i "s,tag:[^P]*# FLYTESCHEDULER_TAG,tag: ${VERSION} # FLYTESCHEDULER_TAG," ./charts/flyte/values.yaml
 sed -i "s,tag:[^P]*# FLYTESCHEDULER_TAG,tag: ${VERSION} # FLYTESCHEDULER_TAG," ./charts/flyte-core/values.yaml
+sed -i "s,repository:[^P]*# FLYTESCHEDULER_IMAGE,repository: cr.flyte.org/flyteorg/flytescheduler-release:${VERSION} # FLYTESCHEDULER_IMAGE," ./charts/flyte/values.yaml
+sed -i "s,repository:[^P]*# FLYTESCHEDULER_IMAGE,repository: cr.flyte.org/flyteorg/flytescheduler-release:${VERSION} # FLYTESCHEDULER_IMAGE," ./charts/flyte-core/values.yaml
+
 sed -i "s,tag:[^P]*# DATACATALOG_TAG,tag: ${VERSION} # DATACATALOG_TAG," ./charts/flyte/values.yaml
 sed -i "s,tag:[^P]*# DATACATALOG_TAG,tag: ${VERSION} # DATACATALOG_TAG," ./charts/flyte-core/values.yaml
+sed -i "s,repository:[^P]*# DATACATALOG_IMAGE,repository: cr.flyte.org/flyteorg/datacatalog-release:${VERSION} # DATACATALOG_IMAGE," ./charts/flyte/values.yaml
+sed -i "s,repository:[^P]*# DATACATALOG_IMAGE,repository: cr.flyte.org/flyteorg/datacatalog-release:${VERSION} # DATACATALOG_IMAGE," ./charts/flyte-core/values.yaml
+
 sed -i "s,tag:[^P]*# FLYTECONSOLE_TAG,tag: ${VERSION} # FLYTECONSOLE_TAG," ./charts/flyte/values.yaml
 sed -i "s,tag:[^P]*# FLYTECONSOLE_TAG,tag: ${VERSION} # FLYTECONSOLE_TAG," ./charts/flyte-core/values.yaml
+sed -i "s,repository:[^P]*# FLYTECONSOLE_IMAGE,repository: cr.flyte.org/flyteorg/flyteconsole-release:${VERSION} # FLYTECONSOLE_IMAGE," ./charts/flyte/values.yaml
+sed -i "s,repository:[^P]*# FLYTECONSOLE_IMAGE,repository: cr.flyte.org/flyteorg/flyteconsole-release:${VERSION} # FLYTECONSOLE_IMAGE," ./charts/flyte-core/values.yaml
+
+
 sed -i "s,tag:[^P]*# FLYTEPROPELLER_TAG,tag: ${VERSION} # FLYTEPROPELLER_TAG," ./charts/flyte/values.yaml
 sed -i "s,tag:[^P]*# FLYTEPROPELLER_TAG,tag: ${VERSION} # FLYTEPROPELLER_TAG," ./charts/flyte-core/values.yaml
+sed -i "s,repository:[^P]*# FLYTEPROPELLER_IMAGE,repository: cr.flyte.org/flyteorg/flytepropeller-release:${VERSION} # FLYTEPROPELLER_IMAGE," ./charts/flyte/values.yaml
+sed -i "s,repository:[^P]*# FLYTEPROPELLER_IMAGE,repository: cr.flyte.org/flyteorg/flytepropeller-release:${VERSION} # FLYTEPROPELLER_IMAGE," ./charts/flyte-core/values.yaml
