cd520a69080cf21adb94562b90753e72cf083983
Yuvraj <code@evalsocket.dev>
2021-12-07 16:59:55 +0530

    Added override values for aws native scheduler
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- script/generate_helm.sh
+++ script/generate_helm.sh
@@ -17,6 +17,9 @@ for deployment in ${DEPLOYMENT_CORE}; do
     helm template flyte -n flyte ${DIR}/../charts/flyte-core/ -f ${DIR}/../charts/flyte-core/values.yaml -f ${DIR}/../charts/flyte-core/values-${deployment}.yaml > ${DIR}/../deployment/${deployment}/flyte_helm_generated.yaml
 done
 
+# Generate manifest AWS native scheduler
+helm template flyte -n flyte ${DIR}/../charts/flyte-core/ -f ${DIR}/../charts/flyte-core/values.yaml -f ${DIR}/../charts/flyte-core/values-eks.yaml -f ${DIR}/../charts/flyte-core/values-eks-override.yaml > ${DIR}/../deployment/eks/flyte_aws_scheduler_helm_generated.yaml
+
 echo "Generating helm docs"
 if ! command -v helm-docs &> /dev/null
 then
