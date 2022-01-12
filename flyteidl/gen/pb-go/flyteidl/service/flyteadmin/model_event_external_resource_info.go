32feec76f5175bc585516c2cbebc19bf60f884b7
Dan Rammer <daniel@union.ai>
2022-01-12 11:57:55 -0600

    Add RetryAttempt and Phase to ExternalResourceInfo proto (#244)
    * added retry attempt and phase to ExternalResourceInfo proto
    
    Signed-off-by: Daniel Rammer <daniel@union.ai>
    
    * captialized ExternalResourceInfo retry_attempt comment
    
    Signed-off-by: Daniel Rammer <daniel@union.ai>
    
    * added index to ExternalResourceInfo proto
    
    Signed-off-by: Daniel Rammer <daniel@union.ai>
    
    * regenerated protos to fix rebase issues
    
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- flyteidl/gen/pb-go/flyteidl/service/flyteadmin/model_event_external_resource_info.go
+++ flyteidl/gen/pb-go/flyteidl/service/flyteadmin/model_event_external_resource_info.go
@@ -13,4 +13,8 @@ package flyteadmin
 type EventExternalResourceInfo struct {
 	// Identifier for an external resource created by this task execution, for example Qubole query ID or presto query ids.
 	ExternalId string `json:"external_id,omitempty"`
+	// A unique index for the external resource with respect to all external resources for this task. Although the identifier may change between task reporting events or retries, this will remain the same to enable aggregating information from multiple reports.
+	Index int64 `json:"index,omitempty"`
+	RetryAttempt int64 `json:"retry_attempt,omitempty"`
+	Phase *CoreTaskExecutionPhase `json:"phase,omitempty"`
 }
