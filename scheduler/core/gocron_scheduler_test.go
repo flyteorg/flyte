311b1357500803b89b071f2d4aed437b3504a0af
pmahindrakar-oss <prafulla.mahindrakar@gmail.com>
2022-01-28 21:49:24 +0530

    Fixed corner case with scheduled time during catchup (#331)

---
+++ scheduler/core/gocron_scheduler_test.go
@@ -0,0 +1,278 @@
+package core
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	adminModels "github.com/flyteorg/flyteadmin/pkg/repositories/models"
+	"github.com/flyteorg/flyteadmin/pkg/runtime"
+	"github.com/flyteorg/flyteadmin/scheduler/executor/mocks"
+	"github.com/flyteorg/flyteadmin/scheduler/repositories/models"
+	"github.com/flyteorg/flyteadmin/scheduler/snapshoter"
+	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/admin"
+	"github.com/flyteorg/flytestdlib/promutils"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/time/rate"
+)
+
+var scheduleCron models.SchedulableEntity
+var scheduleFixed models.SchedulableEntity
+var scheduleCronDeactivated models.SchedulableEntity
+var scheduleFixedDeactivated models.SchedulableEntity
+var scheduleNonExistentDeActivated models.SchedulableEntity
+
+func setup(t *testing.T, subscope string) *GoCronScheduler {
+	configuration := runtime.NewConfigurationProvider()
+	applicationConfiguration := configuration.ApplicationConfiguration().GetTopLevelConfig()
+	schedulerScope := promutils.NewScope(applicationConfiguration.MetricsScope).NewSubScope(subscope)
+	var schedules []models.SchedulableEntity
+	True := true
+	False := false
+	scheduleCron = models.SchedulableEntity{
+		BaseModel: adminModels.BaseModel{
+			UpdatedAt: time.Now(),
+		},
+		SchedulableEntityKey: models.SchedulableEntityKey{
+			Project: "project",
+			Domain:  "domain",
+			Name:    "cron1",
+			Version: "version1",
+		},
+		CronExpression: "0 19 * * *",
+		Active:         &True,
+	}
+	scheduleFixed = models.SchedulableEntity{
+		BaseModel: adminModels.BaseModel{
+			UpdatedAt: time.Now(),
+		},
+		SchedulableEntityKey: models.SchedulableEntityKey{
+			Project: "project",
+			Domain:  "domain",
+			Name:    "fixed1",
+			Version: "version1",
+		},
+		FixedRateValue: 1,
+		Unit:           admin.FixedRateUnit_HOUR,
+		Active:         &True,
+	}
+	scheduleCronDeactivated = models.SchedulableEntity{
+		BaseModel: adminModels.BaseModel{
+			UpdatedAt: time.Now(),
+		},
+		SchedulableEntityKey: models.SchedulableEntityKey{
+			Project: "project",
+			Domain:  "domain",
+			Name:    "cron1",
+			Version: "version1",
+		},
+		CronExpression: "0 19 * * *",
+		Active:         &False,
+	}
+	scheduleFixedDeactivated = models.SchedulableEntity{
+		BaseModel: adminModels.BaseModel{
+			UpdatedAt: time.Now(),
+		},
+		SchedulableEntityKey: models.SchedulableEntityKey{
+			Project: "project",
+			Domain:  "domain",
+			Name:    "fixed1",
+			Version: "version1",
+		},
+		FixedRateValue: 1,
+		Unit:           admin.FixedRateUnit_HOUR,
+		Active:         &False,
+	}
+	scheduleNonExistentDeActivated = models.SchedulableEntity{
+		BaseModel: adminModels.BaseModel{
+			UpdatedAt: time.Now(),
+		},
+		SchedulableEntityKey: models.SchedulableEntityKey{
+			Project: "project",
+			Domain:  "domain",
+			Name:    "cron3",
+			Version: "version3",
+		},
+		CronExpression: "0 11 * * *",
+		Active:         &False,
+	}
+	schedules = append(schedules, scheduleCron)
+	schedules = append(schedules, scheduleFixed)
+	schedules = append(schedules, scheduleCronDeactivated)
+	schedules = append(schedules, scheduleFixedDeactivated)
+	schedules = append(schedules, scheduleNonExistentDeActivated)
+	rateLimiter := rate.NewLimiter(1, 10)
+	executor := new(mocks.Executor)
+	executor.OnExecuteMatch(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+	snapshot := &snapshoter.SnapshotV1{}
+	g := NewGoCronScheduler(context.Background(), schedules, schedulerScope, snapshot, rateLimiter, executor)
+	goCronScheduler, ok := g.(*GoCronScheduler)
+	assert.True(t, ok)
+	goCronScheduler.UpdateSchedules(context.Background(), schedules)
+	goCronScheduler.BootStrapSchedulesFromSnapShot(context.Background(), schedules, snapshot)
+	goCronScheduler.CatchupAll(context.Background(), time.Now())
+	return goCronScheduler
+}
+
+func TestCalculateSnapshot(t *testing.T) {
+	t.Run("empty snapshot", func(t *testing.T) {
+		ctx := context.Background()
+		g := setup(t, "empty_snapshot")
+		snapshot := g.CalculateSnapshot(ctx)
+		assert.NotNil(t, snapshot)
+		assert.True(t, snapshot.IsEmpty())
+	})
+	t.Run("non empty snapshot", func(t *testing.T) {
+		ctx := context.Background()
+		g := setup(t, "non_empty_snapshot")
+		g.jobStore.Range(func(key, value interface{}) bool {
+			currTime := time.Now()
+			job := value.(*GoCronJob)
+			job.lastTime = &currTime
+			return true
+		})
+		snapshot := g.CalculateSnapshot(ctx)
+		assert.NotNil(t, snapshot)
+		assert.False(t, snapshot.IsEmpty())
+	})
+}
+
+func TestGetTimedFuncWithSchedule(t *testing.T) {
+	type test struct {
+		input models.SchedulableEntity
+		scope string
+		want  error
+	}
+
+	t.Run("happy case", func(t *testing.T) {
+		tests := []test{
+			{input: scheduleCron, scope: "happy_case_cron", want: nil},
+			{input: scheduleFixed, scope: "happy_case_fixed", want: nil},
+		}
+		for _, tc := range tests {
+			ctx := context.Background()
+			g := setup(t, tc.scope)
+			timeFunc := g.GetTimedFuncWithSchedule()
+			assert.NotNil(t, timeFunc)
+			err := timeFunc(ctx, tc.input, time.Now())
+			assert.Equal(t, tc.want, err)
+		}
+	})
+	t.Run("failure case", func(t *testing.T) {
+		ctx := context.Background()
+		g := setup(t, "failure_case")
+		executor := new(mocks.Executor)
+		executor.OnExecuteMatch(mock.Anything, mock.Anything, mock.Anything).Return(fmt.Errorf("failure case"))
+		g.executor = executor
+		timeFunc := g.GetTimedFuncWithSchedule()
+		assert.NotNil(t, timeFunc)
+		err := timeFunc(ctx, scheduleCron, time.Now())
+		assert.NotNil(t, err)
+		assert.Equal(t, "failure case", err.Error())
+	})
+}
+
+func TestGetCronScheduledTime(t *testing.T) {
+	fromTime := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
+	nextTime, err := getCronScheduledTime("0 19 * * *", fromTime)
+	assert.Nil(t, err)
+	expectedNextTime := time.Date(2022, time.January, 28, 19, 0, 0, 0, time.UTC)
+	assert.Equal(t, expectedNextTime, nextTime)
+}
+
+func TestGetCatchUpTimes(t *testing.T) {
+	t.Run("to time before scheduled time", func(t *testing.T) {
+		s := models.SchedulableEntity{
+			CronExpression: "0 19 * * *",
+		}
+		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
+		to := time.Date(2022, time.January, 28, 00, 51, 6, 0, time.UTC)
+		catchupTimes, err := GetCatchUpTimes(s, from, to)
+		assert.Nil(t, err)
+		assert.Nil(t, catchupTimes)
+	})
+	t.Run("fixed rate catch up times", func(t *testing.T) {
+		s := models.SchedulableEntity{
+			FixedRateValue: 1,
+			Unit:           admin.FixedRateUnit_HOUR,
+		}
+		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
+		to := time.Date(2022, time.January, 28, 19, 0, 0, 0, time.UTC)
+		catchupTimes, err := GetCatchUpTimes(s, from, to)
+		assert.Nil(t, err)
+		assert.NotNil(t, catchupTimes)
+		assert.Equal(t, 24, len(catchupTimes))
+	})
+	t.Run("to time equal scheduled time", func(t *testing.T) {
+		s := models.SchedulableEntity{
+			CronExpression: "0 19 * * *",
+		}
+		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
+		to := time.Date(2022, time.January, 28, 19, 0, 0, 0, time.UTC)
+		catchupTimes, err := GetCatchUpTimes(s, from, to)
+		assert.Nil(t, err)
+		assert.NotNil(t, catchupTimes)
+		assert.Equal(t, 1, len(catchupTimes))
+		expectedNextTime := time.Date(2022, time.January, 28, 19, 0, 0, 0, time.UTC)
+		assert.Equal(t, expectedNextTime, catchupTimes[0])
+	})
+	t.Run("to time after scheduled time", func(t *testing.T) {
+		s := models.SchedulableEntity{
+			CronExpression: "0 19 * * *",
+		}
+		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
+		to := time.Date(2022, time.January, 28, 19, 50, 0, 0, time.UTC)
+		catchupTimes, err := GetCatchUpTimes(s, from, to)
+		assert.Nil(t, err)
+		assert.NotNil(t, catchupTimes)
+		assert.Equal(t, 1, len(catchupTimes))
+		expectedNextTime := time.Date(2022, time.January, 28, 19, 0, 0, 0, time.UTC)
+		assert.Equal(t, expectedNextTime, catchupTimes[0])
+	})
+	t.Run("invalid cron", func(t *testing.T) {
+		s := models.SchedulableEntity{
+			CronExpression: "0 19 * *",
+		}
+		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
+		to := time.Date(2022, time.January, 28, 00, 51, 6, 0, time.UTC)
+		_, err := GetCatchUpTimes(s, from, to)
+		assert.NotNil(t, err)
+	})
+}
+
+func TestGetFixedRateDurationFromSchedule(t *testing.T) {
+	t.Run("minute duration", func(t *testing.T) {
+		d := time.Duration(1)
+		duration, err := getFixedRateDurationFromSchedule(admin.FixedRateUnit_MINUTE, 1)
+		assert.Nil(t, err)
+		assert.Equal(t, d*time.Minute, duration)
+	})
+	t.Run("hour duration", func(t *testing.T) {
+		d := time.Duration(1)
+		duration, err := getFixedRateDurationFromSchedule(admin.FixedRateUnit_HOUR, 1)
+		assert.Nil(t, err)
+		assert.Equal(t, d*time.Hour, duration)
+	})
+	t.Run("day duration", func(t *testing.T) {
+		d := time.Duration(1)
+		duration, err := getFixedRateDurationFromSchedule(admin.FixedRateUnit_DAY, 1)
+		assert.Nil(t, err)
+		assert.Equal(t, d*time.Hour*24, duration)
+	})
+	t.Run("invalid", func(t *testing.T) {
+		_, err := getFixedRateDurationFromSchedule(100, 1)
+		assert.NotNil(t, err)
+	})
+}
+
+func TestCatchUpAllSchedule(t *testing.T) {
+	ctx := context.Background()
+	g := setup(t, "catch_up_all_schedules")
+	toTime := time.Date(2022, time.January, 29, 0, 0, 0, 0, time.UTC)
+	catchupSuccess := g.CatchupAll(ctx, toTime)
+	assert.True(t, catchupSuccess)
+}
--- scheduler/executor/executor.go
+++ scheduler/executor/executor.go
@@ -7,6 +7,8 @@ import (
 	"github.com/flyteorg/flyteadmin/scheduler/repositories/models"
 )
 
+//go:generate mockery -name Executor -output=mocks -case=underscore
+
 // Executor allows the ability to create scheduled executions on admin
 type Executor interface {
 	// Execute sends a scheduled execution request to admin
