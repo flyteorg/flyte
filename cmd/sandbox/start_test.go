eabfd2f035f68ffa6d302ed33e4517f6e8356561
Yuvraj <code@evalsocket.dev>
2022-02-10 09:02:30 +0530

    Added reuseable workflow (#276)
    * Added reuseable workflow
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- cmd/sandbox/start_test.go
+++ cmd/sandbox/start_test.go
@@ -89,7 +89,7 @@ func TestStartSandboxFunc(t *testing.T) {
 		errCh := make(chan error)
 		sandboxConfig.DefaultConfig.Version = "v0.19.1"
 		bodyStatus := make(chan container.ContainerWaitOKBody)
-		image, _, err := githubutil.GetSandboxImage("", imageName)
+		image, _, err := githubutil.GetSandboxImage(sandboxConfig.DefaultConfig.Version, imageName)
 		assert.Nil(t, err)
 		mockDocker.OnContainerCreate(ctx, &container.Config{
 			Env:          docker.Environment,
