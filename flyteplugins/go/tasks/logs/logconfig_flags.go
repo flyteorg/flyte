ec275d211d5279bafe4a5b277540334b84087704
Haytham Abuelfutuh <haytham@afutuh.com>
2022-02-15 10:10:26 -0800

    Ensure Athena Plugin ClientRequestToken is within allowed limits (#243)
    * Ensure Athena Plugin ClientRequestToken is within allowed limits
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Update interface
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Unit tests
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- flyteplugins/go/tasks/logs/logconfig_flags.go
+++ flyteplugins/go/tasks/logs/logconfig_flags.go
@@ -50,16 +50,16 @@ func (LogConfig) mustMarshalJSON(v json.Marshaler) string {
 // flags is json-name.json-sub-name... etc.
 func (cfg LogConfig) GetPFlagSet(prefix string) *pflag.FlagSet {
 	cmdFlags := pflag.NewFlagSet("LogConfig", pflag.ExitOnError)
-	cmdFlags.Bool(fmt.Sprintf("%v%v", prefix, "cloudwatch-enabled"), *new(bool), "Enable Cloudwatch Logging")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "cloudwatch-region"), *new(string), "AWS region in which Cloudwatch logs are stored.")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "cloudwatch-log-group"), *new(string), "Log group to which streams are associated.")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "cloudwatch-template-uri"), *new(string), "Template Uri to use when building cloudwatch log links")
-	cmdFlags.Bool(fmt.Sprintf("%v%v", prefix, "kubernetes-enabled"), *new(bool), "Enable Kubernetes Logging")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "kubernetes-url"), *new(string), "Console URL for Kubernetes logs")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "kubernetes-template-uri"), *new(string), "Template Uri to use when building kubernetes log links")
-	cmdFlags.Bool(fmt.Sprintf("%v%v", prefix, "stackdriver-enabled"), *new(bool), "Enable Log-links to stackdriver")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "gcp-project"), *new(string), "Name of the project in GCP")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "stackdriver-logresourcename"), *new(string), "Name of the logresource in stackdriver")
-	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "stackdriver-template-uri"), *new(string), "Template Uri to use when building stackdriver log links")
+	cmdFlags.Bool(fmt.Sprintf("%v%v", prefix, "cloudwatch-enabled"), DefaultConfig.IsCloudwatchEnabled, "Enable Cloudwatch Logging")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "cloudwatch-region"), DefaultConfig.CloudwatchRegion, "AWS region in which Cloudwatch logs are stored.")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "cloudwatch-log-group"), DefaultConfig.CloudwatchLogGroup, "Log group to which streams are associated.")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "cloudwatch-template-uri"), DefaultConfig.CloudwatchTemplateURI, "Template Uri to use when building cloudwatch log links")
+	cmdFlags.Bool(fmt.Sprintf("%v%v", prefix, "kubernetes-enabled"), DefaultConfig.IsKubernetesEnabled, "Enable Kubernetes Logging")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "kubernetes-url"), DefaultConfig.KubernetesURL, "Console URL for Kubernetes logs")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "kubernetes-template-uri"), DefaultConfig.KubernetesTemplateURI, "Template Uri to use when building kubernetes log links")
+	cmdFlags.Bool(fmt.Sprintf("%v%v", prefix, "stackdriver-enabled"), DefaultConfig.IsStackDriverEnabled, "Enable Log-links to stackdriver")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "gcp-project"), DefaultConfig.GCPProjectName, "Name of the project in GCP")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "stackdriver-logresourcename"), DefaultConfig.StackdriverLogResourceName, "Name of the logresource in stackdriver")
+	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "stackdriver-template-uri"), DefaultConfig.StackDriverTemplateURI, "Template Uri to use when building stackdriver log links")
 	return cmdFlags
 }
