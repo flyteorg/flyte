59fe9a0ed8d362f227c04b3750459d35a92261bd
Yuvraj <code@evalsocket.dev>
2022-02-19 14:00:19 +0530

    make helm
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- script/generate_helm.sh
+++ script/generate_helm.sh
@@ -27,13 +27,12 @@ done
 helm template flyte -n flyte ${DIR}/../charts/flyte-core/ -f ${DIR}/../charts/flyte-core/values.yaml -f ${DIR}/../charts/flyte-core/values-eks.yaml -f ${DIR}/../charts/flyte-core/values-eks-override.yaml ${HELM_CAPABILITIES} > ${DIR}/../deployment/eks/flyte_aws_scheduler_helm_generated.yaml
 
 echo "Generating helm docs"
-if ! command -v helm-docs &> /dev/null
+if  command -v helm-docs &> /dev/null
 then
-    if [[ -f "$(which helm-docs)" ]]; then
-      rm $(which helm-docs)
-    fi
-    GO111MODULE=on go get github.com/norwoodj/helm-docs/cmd/helm-docs@latest
+    rm $(which helm-docs)
 fi
+
+GO111MODULE=on go get github.com/norwoodj/helm-docs/cmd/helm-docs@latest
 
 ${GOPATH:-~/go}/bin/helm-docs -c ${DIR}/../charts/
 
