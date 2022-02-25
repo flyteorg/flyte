1c962aec9a865dd9e3b9909df9bf1c750e1c21ec
SmritiSatyanV <94349093+SmritiSatyanV@users.noreply.github.com>
2022-02-25 17:16:02 +0530

    Minor updates (#669)
    * Minor updates
    Grammar, and rendering fix
    Updates based on comments
    Update contribute.rst
    Signed-off-by: SmritiSatyanV <smriti@union.ai>
    
    * Moving panel-and-toc image to static-resources repo and updating the url (#671)
    
    Co-authored-by: Alekhya Sai <alekhyasaip@gmail.com>

--- cookbook/core/control_flow/checkpoint.py
+++ cookbook/core/control_flow/checkpoint.py
@@ -1,29 +1,29 @@
 """
 Intratask Checkpoints
-------------
+----------------------
 
 .. note::
 
-  This feature is available since flytekit version 0.30.0b6+ and needs a Flyte backend version of atleast 0.19.0+
+  This feature is available from Flytekit version 0.30.0b6+ and needs a Flyte backend version of atleast 0.19.0+.
 
-A checkpoint allows a task to recover from a previous failure by recording the state of a task before the failure and
+A checkpoint recovers a task from a previous failure by recording the state of a task before the failure and
 resuming from the latest recorded state.
 
-Why Intra-task checkpoints?
+Why Intra-task Checkpoints?
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-Flyte at its core is a workflow engine, and workflows provide a way to logically break up an operation / program / idea
-into smaller sized tasks. If a task fails, the workflow does not need to run the previously completed tasks, but can
-simply retry the task at hand. Eventually, when the task succeeds, it will never be run again. Thus, task boundaries
+Flyte, at its core, is a workflow engine. Workflows provide a way to break up an operation/program/idea
+logically into smaller tasks. If a task fails, the workflow does not need to run the previously completed tasks. It can
+simply retry the task that failed. Eventually, when the task succeeds, it will not run again. Thus, task boundaries
 naturally serve as checkpoints.
 
-There are cases where it is not easy or desirable to break a task into further smaller tasks, as running a task
-adds overhead. This is especially true when running a large computation in a tight-loop. This these cases, users can
-split each loop iteration into its own task using dynamic workflows, but the overhead of spawning new tasks, recording
-intermediate results, and reconstituting the state can be extremely expensive.
+There are cases where it is not easy or desirable to break a task into smaller tasks, because running a task
+adds to the overhead. This is true when running a large computation in a tight-loop. In such cases, users can
+split each loop iteration into its own task using :ref:`dynamic workflows <Dynamic Workflows>`, but the overhead of spawning new tasks, recording
+intermediate results, and reconstituting the state can be expensive.
 
 Model-training Use Case
-==================
+=======================
 
 An example of this case is model training. Running multiple epochs or different iterations with the same
 dataset can take a long time, but the bootstrap time may be high and creating task boundaries can be expensive.
@@ -31,18 +31,19 @@ dataset can take a long time, but the bootstrap time may be high and creating ta
 To tackle this, Flyte offers a way to checkpoint progress within a task execution as a file or a set of files. These
 checkpoints can be written synchronously or asynchronously. In case of failure, the checkpoint file can be re-read to resume
 most of the state without re-running the entire task. This opens up the opportunity to use alternate compute systems with
-lower guarantees like - `AWS Spot Instances <https://aws.amazon.com/ec2/spot/>`__, `GCP Pre-emptible Instances <https://cloud.google.com/compute/docs/instances/preemptible>`__ etc.
+lower guarantees like `AWS Spot Instances <https://aws.amazon.com/ec2/spot/>`__, `GCP Pre-emptible Instances <https://cloud.google.com/compute/docs/instances/preemptible>`__, etc.
 
 These instances offer great performance at much lower price-points as compared to their on-demand or reserved alternatives.
-This is possible if you construct your tasks in a fault-tolerant way. For most cases, when the task runs for a short duration,
-e.g. less than 10 minutes, the potential of failure is not significant and task-boundary-based recovery offers
+This is possible if you construct the tasks in a fault-tolerant manner. In most cases, when the task runs for a short duration,
+e.g., less than 10 minutes, the potential of failure is insignificant and task-boundary-based recovery offers
 significant fault-tolerance to ensure successful completion.
 
-But, as the time for a task increases, the cost of re-running the task increases and reduces the chance of successful
-completion. This is where Flyte's intra-task checkpointing truly shines. This document provides an example of how to
-develop tasks which utilize intra-task checkpointing. This only provides the low-level API. We intend to integrate
-higher level checkpointing API's available in popular training frameworks like Keras, Pytorch, Scikit learn and
-big-Data frameworks like Spark, Flink to super charge their fault-tolerance.
+But as the time for a task increases, the cost of re-running it increases, and reduces the chances of successful
+completion. This is where Flyte's intra-task checkpointing truly shines. 
+
+Let's look at an example of how to develop tasks which utilize intra-task checkpointing. It only provides the low-level API, though. We intend to integrate
+higher-level checkpointing APIs available in popular training frameworks like Keras, Pytorch, Scikit-learn, and
+big-data frameworks like Spark and Flink to supercharge their fault-tolerance.
 """
 
 from flytekit import task, workflow, current_context
@@ -53,12 +54,11 @@ RETRIES=3
 
 
 # %%
-# This task shows how in case of failure checkpoints can help resume. This is only an example task and shows the api for
-# the checkpointer. The checkpoint system exposes other api's and for a more detailed understanding refer to
-# :ref:py:class:`flytekit.Checkpoint`
+# This task shows how checkpoints can help resume execution in case of a failure. This is an example task and shows the API for
+# the checkpointer. The checkpoint system exposes other APIs. For a detailed understanding, refer to the `checkpointer code <https://github.com/flyteorg/flytekit/blob/master/flytekit/core/checkpointer.py>`__.
 #
-# The goal of this method is to actually return `a+4` It does this in 3 retries of the task, by recovering from previous
-# failures. For each failure it increments the value by 1
+# The goal of this method is to return `a+4`. It performs this operation within 3 retries of the task, by recovering from the previous
+# failures. For each failure, it increments the value by 1.
 @task(retries=RETRIES)
 def use_checkpoint(n_iterations: int) -> int:
     cp = current_context().checkpoint
@@ -67,7 +67,7 @@ def use_checkpoint(n_iterations: int) -> int:
     if prev:
         start = int(prev.decode())
 
-    # We will create a failure interval so that we can create failures ever n iterations and then succeed within
+    # create a failure interval so we can create failures for every 'n' iterations and then succeed within
     # configured retries
     failure_interval = n_iterations * 1.0 / RETRIES
     i = 0
@@ -83,17 +83,19 @@ def use_checkpoint(n_iterations: int) -> int:
 
 
 # %%
-# The workflow in this case simply calls the task. The task itself will be retried for the failure :ref:py:class:`FlyteRecoverableException`
+# The workflow here simply calls the task. The task itself 
+# will be retried for the :ref:`FlyteRecoverableException <flytekit:exception_handling>`.
+#
 @workflow
 def example(n_iterations: int) -> int:
     return use_checkpoint(n_iterations=n_iterations)
 
 
 #%%
-# Locally the checkpoint is stored, but since, retries are not supported, the checkpoint is not really used.
+# The checkpoint is stored locally, but it is not used since retries are not supported.
 if __name__ == "__main__":
     try:
         example(n_iterations=10)
     except RuntimeError as e:
-        # Locally an exception is expected as retries are not performed.
+        # no retries are performed, so an exception is expected when run locally.
         pass
