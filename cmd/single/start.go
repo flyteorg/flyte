f505c9b9fcf8c997f0b6aad4f1a247f31f7b54b2
Ketan Umare <ketan.umare@gmail.com>
2022-03-23 13:54:04 -0700

    working
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- cmd/single/start.go
+++ cmd/single/start.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/flyteorg/flyteadmin/pkg/clusterresource"
+	"github.com/flyteorg/flyteadmin/pkg/common"
 	"github.com/flyteorg/flyteadmin/pkg/runtime"
 	adminServer "github.com/flyteorg/flyteadmin/pkg/server"
 	adminScheduler "github.com/flyteorg/flyteadmin/scheduler"
 	propellerEntrypoint "github.com/flyteorg/flytepropeller/pkg/controller"
 	propellerConfig "github.com/flyteorg/flytepropeller/pkg/controller/config"
+	"github.com/flyteorg/flytestdlib/contextutils"
 	"github.com/flyteorg/flytestdlib/logger"
 	"github.com/flyteorg/flytestdlib/promutils"
-	"net/http"
-	"sync"
-
+	"github.com/flyteorg/flytestdlib/promutils/labeled"
 	_ "github.com/golang/glog"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 	_ "gorm.io/driver/postgres" // Required to import database driver.
+	"net/http"
+	"strings"
 
 	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/array/k8s"
 	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/pod"
@@ -25,10 +28,21 @@ import (
 func GetConsoleHandlers() map[string]func(http.ResponseWriter, *http.Request) {
 	handlers := make(map[string]func(http.ResponseWriter, *http.Request))
 	// Serves console
-	fs := http.StripPrefix("/console/", http.FileServer(http.Dir("./dist")))
+	rawFs := http.FileServer(http.Dir("./dist"))
+	consoleFS := http.StripPrefix("/console/", rawFs)
+	handlers["/console/assets/"] = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Printf("all files under console returning, %s\n", request.URL.Path)
+		consoleFS.ServeHTTP(writer, request)
+	}
+
 	handlers["/console/"] = func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Printf("all files under console returning, %s\n", request.URL.Path)
-		fs.ServeHTTP(writer, request)
+		newPath := strings.TrimLeft(request.URL.Path, "/console")
+		if strings.Contains(newPath, "/") {
+			http.ServeFile(writer, request, "./dist/index.html")
+		} else {
+			consoleFS.ServeHTTP(writer, request)
+		}
 	}
 	handlers["/console"] = func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Println("returning index.html")
@@ -51,19 +65,28 @@ func startClusterResourceController(ctx context.Context) error {
 }
 
 func startAdmin(ctx context.Context) error {
+	logger.Infof(ctx, "Running Database Migrations...")
 	if err := adminServer.Migrate(ctx); err != nil {
 		return err
 	}
+	logger.Infof(ctx, "Seeding default projects...")
 	if err := adminServer.SeedProjects(ctx, []string{"flytesnacks"}); err != nil {
 		return err
 	}
-	if err := adminScheduler.StartScheduler(ctx); err != nil {
-		return err
-	}
-	if err := startClusterResourceController(ctx); err != nil {
-		return err
-	}
-	return adminServer.Serve(ctx, GetConsoleHandlers())
+	g := new(errgroup.Group)
+	g.Go(func() error {
+		logger.Infof(ctx, "Starting Scheduler...")
+		return adminScheduler.StartScheduler(ctx)
+	})
+	logger.Infof(ctx, "Starting cluster resource controller...")
+	g.Go(func() error {
+		return startClusterResourceController(ctx)
+	})
+	g.Go(func() error {
+		logger.Infof(ctx, "Starting Admin server...")
+		return adminServer.Serve(ctx, GetConsoleHandlers())
+	})
+	return g.Wait()
 }
 
 func startPropeller(ctx context.Context) error {
@@ -75,33 +98,33 @@ var startCmd = &cobra.Command{
 	Short: "This command will start the flyte native scheduler and periodically get new schedules from the db for scheduling",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.Background()
-		childCtx, cancel := context.WithCancel(ctx)
-		defer cancel()
-		wg := sync.WaitGroup{}
-		wg.Add(1)
-		go func(ctx context.Context) {
-			defer wg.Done()
-			err := startAdmin(ctx)
+		g, childCtx := errgroup.WithContext(ctx)
+		g.Go(func() error {
+			err := startAdmin(childCtx)
 			if err != nil {
-				logger.Errorf(ctx, "Failed to start Admin, err: %v", err)
-				return
+				logger.Errorf(childCtx, "Failed to start Admin, err: %v", err)
+				return err
 			}
-		}(childCtx)
+			return nil
+		})
 
-		go func(ctx context.Context) {
-			defer wg.Done()
-			err := startPropeller(ctx)
+		g.Go(func() error {
+			err := startPropeller(childCtx)
 			if err != nil {
-				logger.Errorf(ctx, "Failed to start Propeller, err: %v", err)
-				return
+				logger.Errorf(childCtx, "Failed to start Propeller, err: %v", err)
+				return err
 			}
-		}(childCtx)
+			return nil
+		})
 
-		wg.Wait()
-		return nil
+		return g.Wait()
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(startCmd)
+	// Set Keys
+	labeled.SetMetricKeys(contextutils.AppNameKey, contextutils.ProjectKey, contextutils.DomainKey,
+		contextutils.ExecIDKey, contextutils.WorkflowIDKey, contextutils.NodeIDKey, contextutils.TaskIDKey,
+		contextutils.TaskTypeKey, common.RuntimeTypeKey, common.RuntimeVersionKey)
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.17
 
 require (
 	github.com/flyteorg/flyteadmin v0.6.115
-	github.com/flyteorg/flyteplugins v0.10.15
+	github.com/flyteorg/flyteplugins v0.10.16
 	github.com/flyteorg/flytepropeller v0.16.34
 	github.com/flyteorg/flytestdlib v0.4.14
 	github.com/golang/glog v1.0.0
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/pflag v1.0.5
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	gorm.io/driver/postgres v1.2.3
 )
 
@@ -44,7 +45,7 @@ require (
 	github.com/evanphx/json-patch v4.9.0+incompatible // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
-	github.com/flyteorg/flyteidl v0.23.1 // indirect
+	github.com/flyteorg/flyteidl v0.24.0 // indirect
 	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
@@ -134,7 +135,6 @@ require (
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
 	golang.org/x/text v0.3.7 // indirect
@@ -168,5 +168,7 @@ require (
 
 // replace github.com/flyteorg/flyteadmin => github.com/flyteorg/flyteadmin v0.6.116-0.20220318183949-4492d035af95
 replace github.com/flyteorg/flyteadmin => ../flyteadmin
+
+replace github.com/flyteorg/flytepropeller => ../flytepropeller
 
 replace github.com/robfig/cron/v3 => github.com/unionai/cron/v3 v3.0.2-0.20210825070134-bfc34418fe84
