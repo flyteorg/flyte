61eab344db2f507f5d5a706b2ad971ce788d7070
Kevin Su <pingsutw@apache.org>
2022-03-28 10:01:30 +0800

    Updated dockerfile
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- cmd/single/console.go
+++ cmd/single/console.go
@@ -23,6 +23,8 @@ const indexHTML = "/index.html"
 // For every file add a "dist" as the prefix, as every file is assumed to be packaged in "dist" folder fv
 func GetConsoleFile(name string) string {
 	name = strings.TrimPrefix(name, consoleRoot)
+	// TODO: I have to add this to make flyteconsole work in the sandbox
+	name = strings.TrimPrefix(name, "/" + assetsDir)
 	if name == "" || name == "/" {
 		name = indexHTML
 	} else if !strings.Contains(name, assetsDir) {
