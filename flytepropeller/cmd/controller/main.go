3d48153a5381d1db0dbd91ffc15413cd7d55c862
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2022-04-11 22:34:16 -0700

    Moving all plugins to a common package, for easy loading (#423)
    * Moving all plugins to a common package, for easy loading
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * updated
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- flytepropeller/cmd/controller/main.go
+++ flytepropeller/cmd/controller/main.go
@@ -1,17 +1,7 @@
 package main
 
 import (
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/array/awsbatch"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/array/k8s"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/hive"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/kfoperators/mpi"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/kfoperators/pytorch"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/pod"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/sagemaker"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/k8s/spark"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/webapi/athena"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/webapi/bigquery"
-	_ "github.com/flyteorg/flyteplugins/go/tasks/plugins/webapi/snowflake"
+	_ "github.com/flyteorg/flytepropeller/plugins"
 	"github.com/flyteorg/flytestdlib/contextutils"
 	"github.com/flyteorg/flytestdlib/promutils/labeled"
 
