a9108b3564c097a66981502ad24654253a9f58aa
Bruce Arctor <5032356+brucearctor@users.noreply.github.com>
2022-04-12 13:42:34 -0700

    refactor

---
+++ .github/scripts/ci_helper.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -ex
+
+setup_opta () {
+  sudo apt-get update
+  sudo apt-get install -y locales locales-all
+  /bin/bash -c "$(curl -fsSL https://docs.opta.dev/install.sh)"
+  sudo ln -fs ~/.opta/opta /usr/local/bin/opta
+  export PATH=$PATH:/home/runner/.opta
+}
+
+
+commit_changelog () {
+  sed -i "s,changelog:[^*]*# OPTA_CHANGELOG,changelog: $(date +%s) # OPTA_CHANGELOG," dev/opta/flyte.yaml
+}
+
+prepare_flyte_core () {
+ helm repo add flyteorg https://flyteorg.github.io/flyte
+ helm repo update
+ helm fetch --untar --untardir dev/opta flyteorg/flyte-core --version $1
+ echo "changelog: $(date +%s)" >> dev/opta/flyte-core/values-eks.yaml
+}
+
+prepare_helm_chart () {
+    commit_changelog
+    FLYTE_VERSION=$(curl --silent "https://api.github.com/repos/flyteorg/flyte/releases/latest" | jq -r .tag_name)
+    prepare_flyte_core $FLYTE_VERSION
+    sed -i "s,chart_version:[^P]*# FLYTE_VERSION,chart_version: $FLYTE_VERSION # FLYTE_VERSION," dev/opta/flyte.yaml
+}
+
+opta_action () {
+  cd $2
+  if [[ $1 == "force-unlock" ]]
+  then
+    y | OPTA_DEBUG=true HELM_DEBUG=true opta $1 -c flyte.yaml --env $ENV
+    exit 0
+  fi
+  y | OPTA_DEBUG=true HELM_DEBUG=true opta $1 -c flyte.yaml --env $ENV --auto-approve --detailed-plan
+}
+
+
+prepare_flyte_release_build () {
+  REPOSITORY="flyte"
+  [ "${OPTA_ENVIRONMENT}" == "nightly" ] && REPOSITORY="flyteadmin" || REPOSITORY="flyte"
+  if [[ -z "$FLYTEADMIN_TAG" ]]; then
+    FLYTEADMIN_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name)
+  else
+    [ "${FLYTEADMIN_TAG}" == "latest" ] && FLYTEADMIN_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name) || FLYTEADMIN_TAG=$FLYTEADMIN_TAG
+  fi
+
+  [ "${OPTA_ENVIRONMENT}" == "nightly" ] && REPOSITORY="datacatalog" || REPOSITORY="flyte"
+  if [[ -z "$DATACATALOG_TAG" ]]; then
+    DATACATALOG_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name)
+  else
+    [ "${DATACATALOG_TAG}" == "latest" ] && DATACATALOG_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name) || DATACATALOG_TAG=$DATACATALOG_TAG
+  fi
+
+  [ "${OPTA_ENVIRONMENT}" == "nightly" ] && REPOSITORY="flyteconsole" || REPOSITORY="flyte"
+  if [[ -z "$FLYTECONSOLE_TAG" ]]; then
+    FLYTECONSOLE_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name)
+  else
+    [ "${FLYTECONSOLE_TAG}" == "latest" ] && FLYTECONSOLE_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name) || FLYTECONSOLE_TAG=$FLYTECONSOLE_TAG
+  fi
+
+  [ "${OPTA_ENVIRONMENT}" == "nightly" ] && REPOSITORY="flytepropeller" || REPOSITORY="flyte"
+  if [[ -z "$FLYTEPROPELLER_TAG" ]]; then
+    FLYTEPROPELLER_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name)
+  else
+    [ "${FLYTEPROPELLER_TAG}" == "latest" ] && FLYTEPROPELLER_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/$REPOSITORY/releases/latest" | jq -r .tag_name) || FLYTEPROPELLER_TAG=$FLYTEPROPELLER_TAG
+  fi
+  sed -i "s,tag:[^P]*# FLYTEADMIN_TAG,tag: ${FLYTEADMIN_TAG} # FLYTEADMIN_TAG," ./dev/opta/flyte.yaml
+  sed -i "s,tag:[^P]*# FLYTESCHEDULER_TAG,tag: ${FLYTEADMIN_TAG} # FLYTESCHEDULER_TAG," ./dev/opta/flyte.yaml
+  sed -i "s,tag:[^P]*# DATACATALOG_TAG,tag: ${DATACATALOG_TAG} # DATACATALOG_TAG," ./dev/opta/flyte.yaml
+  sed -i "s,tag:[^P]*# FLYTECONSOLE_TAG,tag: ${FLYTECONSOLE_TAG} # FLYTECONSOLE_TAG," ./dev/opta/flyte.yaml
+  sed -i "s,tag:[^P]*# FLYTEPROPELLER_TAG,tag: ${FLYTEPROPELLER_TAG} # FLYTEPROPELLER_TAG," ./dev/opta/flyte.yaml
+}
+
+install_flytekit () {
+  python -m pip install --upgrade pip
+  pip install flytekit
+  pip freeze
+}
+
+setup_flytekit () {
+  install_flytekit
+  echo $CLIENT_SECRET >> /home/runner/secret_location
+  flyte-cli setup-config --host=$DNS
+}
+
+$1 $2 $3
