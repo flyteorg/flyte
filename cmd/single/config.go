1b321e29f772fd5b0717d3200cbf0cfdad54f208
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2022-04-13 07:07:17 -0700

    Fixing single binary (#2348)

--- cmd/single/config.go
+++ cmd/single/config.go
@@ -12,18 +12,18 @@ var (
 type Config struct {
 	Propeller   Propeller   `json:"propeller" pflag:",Configuration to disable propeller or any of its components."`
 	Admin       Admin       `json:"admin" pflag:",Configuration to disable FlyteAdmin or any of its components"`
-	DataCatalog DataCatalog `json:"data_catalog" pflag:",Configuration to disable DataCatalog or any of its components"`
+	DataCatalog DataCatalog `json:"dataCatalog" pflag:",Configuration to disable DataCatalog or any of its components"`
 }
 
 type Propeller struct {
 	Disabled       bool `json:"disabled" pflag:",Disables flytepropeller in the single binary mode"`
-	DisableWebhook bool `json:"disable_webhook" pflag:",Disables webhook only"`
+	DisableWebhook bool `json:"disableWebhook" pflag:",Disables webhook only"`
 }
 
 type Admin struct {
 	Disabled                      bool `json:"disabled" pflag:",Disables flyteadmin in the single binary mode"`
-	DisableScheduler              bool `json:"disable_scheduler" pflag:",Disables Native scheduler in the single binary mode"`
-	DisableClusterResourceManager bool `json:"disable_cluster_resource_manager" pflag:",Disables Cluster resource manager"`
+	DisableScheduler              bool `json:"disableScheduler" pflag:",Disables Native scheduler in the single binary mode"`
+	DisableClusterResourceManager bool `json:"disableClusterResourceManager" pflag:",Disables Cluster resource manager"`
 }
 
 type DataCatalog struct {
