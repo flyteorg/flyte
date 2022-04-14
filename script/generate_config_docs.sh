0f70035809cfe54f6560897e29f4f4965bb776f5
Katrina Rogan <katroganGH@gmail.com>
2022-04-14 10:48:45 -0700

    Don't clone repos in root dir
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- script/generate_config_docs.sh
+++ script/generate_config_docs.sh
@@ -13,6 +13,9 @@ FLYTEADMIN_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flyteadmin
 FLYTEPROPELLER_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flytepropeller/releases/latest" | jq -r .tag_name)
 DATACATALOG_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/datacatalog/releases/latest" | jq -r .tag_name)
 
+rm -rf flytearchives
+mkdir flytearchives
+cd flytearchives
 curl https://github.com/flyteorg/flyteadmin/releases/download/${FLYTEADMIN_TAG}/flyteadmin_${FLYTEADMIN_TAG}_linux_x86_64.tar.gz --output flyteadmin.tar.gz -s -L && tar -xvf flyteadmin.tar.gz
 mv flyteadmin $GOBIN/flyteadmin
 curl https://github.com/flyteorg/flytepropeller/releases/download/${FLYTEPROPELLER_TAG}/flytepropeller_${FLYTEPROPELLER_TAG}_linux_x86_64.tar.gz --output flytepropeller.tar.gz -s -L && tar -xvf flytepropeller.tar.gz
@@ -20,7 +23,9 @@ mv flytepropeller $GOBIN/flytepropeller
 curl https://github.com/flyteorg/datacatalog/releases/download/${DATACATALOG_TAG}/datacatalog_${DATACATALOG_TAG}_linux_x86_64.tar.gz --output datacatalog.tar.gz -s -L && tar -xvf datacatalog.tar.gz
 mv datacatalog $GOBIN/datacatalog
 git clone https://github.com/flyteorg/flyteadmin.git && cd flyteadmin && go build cmd/scheduler/main.go
-mv main $GOBIN/scheduler && rm -rf ../flyteadmin
+mv main $GOBIN/scheduler
+# Clean up tar archives
+cd ../../ && rm -rf flytearchives
 
 output_config () {
 CONFIG_NAME=$1
