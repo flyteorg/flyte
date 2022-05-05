ab6e836dd3c56492c1e44e0674e0aa8eddd5bcd5
Haytham Abuelfutuh <haytham@afutuh.com>
2022-05-05 13:19:36 -0700

    Use flytescheduler binary from released artifacts (#2460)
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- script/generate_config_docs.sh
+++ script/generate_config_docs.sh
@@ -18,12 +18,11 @@ mkdir flytearchives
 cd flytearchives
 curl https://github.com/flyteorg/flyteadmin/releases/download/${FLYTEADMIN_TAG}/flyteadmin_${FLYTEADMIN_TAG}_linux_x86_64.tar.gz --output flyteadmin.tar.gz -s -L && tar -xvf flyteadmin.tar.gz
 mv flyteadmin $GOBIN/flyteadmin
+mv flytescheduler $GOBIN/flytescheduler
 curl https://github.com/flyteorg/flytepropeller/releases/download/${FLYTEPROPELLER_TAG}/flytepropeller_${FLYTEPROPELLER_TAG}_linux_x86_64.tar.gz --output flytepropeller.tar.gz -s -L && tar -xvf flytepropeller.tar.gz
 mv flytepropeller $GOBIN/flytepropeller
 curl https://github.com/flyteorg/datacatalog/releases/download/${DATACATALOG_TAG}/datacatalog_${DATACATALOG_TAG}_linux_x86_64.tar.gz --output datacatalog.tar.gz -s -L && tar -xvf datacatalog.tar.gz
 mv datacatalog $GOBIN/datacatalog
-git clone https://github.com/flyteorg/flyteadmin.git && cd flyteadmin && go build cmd/scheduler/main.go
-mv main $GOBIN/scheduler
 # Clean up tar archives
 cd ../../ && rm -rf flytearchives
 
@@ -56,4 +55,4 @@ $GOBIN/$COMMAND config docs >> "${OUTPUT_PATH}"
 output_config "Admin" flyteadmin flyteadmin
 output_config "Propeller" flytepropeller flytepropeller
 output_config "Datacatalog" flytedatacatalog datacatalog
-output_config "Scheduler" flytescheduler scheduler
+output_config "Scheduler" flytescheduler flytescheduler
