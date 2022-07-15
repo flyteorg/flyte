021e31b5a35d081e8ce09c27e34449a31c339a19
Bruce Arctor <5032356+brucearctor@users.noreply.github.com>
2022-07-15 15:59:35 -0700

    mortests

--- boilerplate/flyte/end2end/run-tests.py
+++ boilerplate/flyte/end2end/run-tests.py
@@ -54,36 +54,35 @@ FLYTESNACKS_WORKFLOW_GROUPS: Mapping[str, List[Tuple[str, dict]]] = {
         ("core.type_system.typed_schema.wf", {}),
         #("my.imperative.workflow.example", {"in1": "hello", "in2": "foo"}),
     ],
-    # UNCOMMENT BELOW!?
-    # "integrations-k8s-spark": [
-    #     ("k8s_spark.pyspark_pi.my_spark", {"triggered_date": datetime.datetime.now()}),
-    # ],
-    # "integrations-kfpytorch": [
-    #     ("kfpytorch.pytorch_mnist.pytorch_training_wf", {}),
-    # ],
-    # "integrations-kftensorflow": [
-    #     ("kftensorflow.tf_mnist.mnist_tensorflow_workflow", {}),
-    # ],
-    # "integrations-pod": [
-    #     ("pod.pod.pod_workflow", {}),
-    # ],
-    # "integrations-pandera_examples": [
-    #     ("pandera_examples.basic_schema_example.process_data", {}),
-    #     # TODO: investigate type mismatch float -> numpy.float64
-    #     # ("pandera_examples.validating_and_testing_ml_pipelines.pipeline", {"data_random_state": 42, "model_random_state": 99}),
-    # ],
-    # "integrations-modin_examples": [
-    #     ("modin_examples.knn_classifier.pipeline", {}),
-    # ],
-    # "integrations-papermilltasks": [
-    #     ("papermilltasks.simple.nb_to_python_wf", {"f": 3.1415926535}),
-    # ],
-    # "integrations-greatexpectations": [
-    #     ("greatexpectations.task_example.simple_wf", {}),
-    #     ("greatexpectations.task_example.file_wf", {}),
-    #     ("greatexpectations.task_example.schema_wf", {}),
-    #     ("greatexpectations.task_example.runtime_wf", {}),
-    # ],
+    "integrations-k8s-spark": [
+        ("k8s_spark.pyspark_pi.my_spark", {"triggered_date": datetime.datetime.now()}),
+    ],
+    "integrations-kfpytorch": [
+        ("kfpytorch.pytorch_mnist.pytorch_training_wf", {}),
+    ],
+    "integrations-kftensorflow": [
+        ("kftensorflow.tf_mnist.mnist_tensorflow_workflow", {}),
+    ],
+    "integrations-pod": [
+        ("pod.pod.pod_workflow", {}),
+    ],
+    "integrations-pandera_examples": [
+        ("pandera_examples.basic_schema_example.process_data", {}),
+        # TODO: investigate type mismatch float -> numpy.float64
+        # ("pandera_examples.validating_and_testing_ml_pipelines.pipeline", {"data_random_state": 42, "model_random_state": 99}),
+    ],
+    "integrations-modin_examples": [
+        ("modin_examples.knn_classifier.pipeline", {}),
+    ],
+    "integrations-papermilltasks": [
+        ("papermilltasks.simple.nb_to_python_wf", {"f": 3.1415926535}),
+    ],
+    "integrations-greatexpectations": [
+        ("greatexpectations.task_example.simple_wf", {}),
+        ("greatexpectations.task_example.file_wf", {}),
+        ("greatexpectations.task_example.schema_wf", {}),
+        ("greatexpectations.task_example.runtime_wf", {}),
+    ],
 }
 
 
