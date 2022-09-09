0454c27b5b8946a1ec6db65512e28fb48bcd2717
Haytham Abuelfutuh <haytham@afutuh.com>
2022-09-09 12:44:47 -0700

    Use grpc client interceptors to properly check for auth requirement (#315)
    * Use grpc client interceptors to properly check for auth requirement
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Some refactor and add unit tests
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * PR Comments
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * lint
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * unit tests
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Attempt a random port
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Listen to localhost only
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * PR Comments
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * use chain unary interceptor instead
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * only log on errors
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    * Attempt to disable error check
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>
    
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- flyteidl/clients/go/admin/token_source.go
+++ flyteidl/clients/go/admin/token_source.go
@@ -6,27 +6,16 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// This class is here because we cannot use the normal "github.com/grpc/grpc-go/credentials/oauth" package to satisfy
+// CustomHeaderTokenSource class is here because we cannot use the normal "github.com/grpc/grpc-go/credentials/oauth" package to satisfy
 // the credentials.PerRPCCredentials interface. This is because we want to be able to support a different 'header'
 // when passing the token in the gRPC call's metadata. The default is filled in in the constructor if none is supplied.
 type CustomHeaderTokenSource struct {
-	oauth2.TokenSource
+	tokenSource  oauth2.TokenSource
 	customHeader string
 	insecure     bool
 }
 
 const DefaultAuthorizationHeader = "authorization"
-
-// GetRequestMetadata gets the authorization metadata as a map using a TokenSource to generate a token
-func (ts CustomHeaderTokenSource) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
-	token, err := ts.Token()
-	if err != nil {
-		return nil, err
-	}
-	return map[string]string{
-		ts.customHeader: token.Type() + " " + token.AccessToken,
-	}, nil
-}
 
 // RequireTransportSecurity returns whether this credentials class requires TLS/SSL. OAuth uses Bearer tokens that are
 // susceptible to MITM (Man-In-The-Middle) attacks that are mitigated by TLS/SSL. We may return false here to make it
@@ -36,6 +25,18 @@ func (ts CustomHeaderTokenSource) RequireTransportSecurity() bool {
 	return !ts.insecure
 }
 
+// GetRequestMetadata gets the authorization metadata as a map using a TokenSource to generate a token
+func (ts CustomHeaderTokenSource) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	token, err := ts.tokenSource.Token()
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]string{
+		ts.customHeader: token.Type() + " " + token.AccessToken,
+	}, nil
+}
+
 func NewCustomHeaderTokenSource(source oauth2.TokenSource, insecure bool, customHeader string) CustomHeaderTokenSource {
 	header := DefaultAuthorizationHeader
 	if customHeader != "" {
@@ -43,7 +44,7 @@ func NewCustomHeaderTokenSource(source oauth2.TokenSource, insecure bool, custom
 	}
 
 	return CustomHeaderTokenSource{
-		TokenSource:  source,
+		tokenSource:  source,
 		customHeader: header,
 		insecure:     insecure,
 	}
