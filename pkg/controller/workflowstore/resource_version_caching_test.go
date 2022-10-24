26ad85757c57cda41bf23a2b054d49eccaa8145d
Daniel Shuy <daniel_shuy@hotmail.com>
2022-10-25 01:12:04 +0800

    Move workflowstore logic to correct abstraction (#496)
    * Move logic to clear workflow CRD managed fields to passthrough workflowstore
    
    Signed-off-by: Daniel Shuy <daniel_shuy@hotmail.com>
    
    * Move logic to track terminated workflows to new TerminatedTrackingStore workflowstore
    
    Signed-off-by: Daniel Shuy <daniel_shuy@hotmail.com>
    
    Signed-off-by: Daniel Shuy <daniel_shuy@hotmail.com>
    Co-authored-by: Dan Rammer <daniel@union.ai>

--- pkg/controller/workflowstore/resource_version_caching_test.go
+++ pkg/controller/workflowstore/resource_version_caching_test.go
@@ -23,6 +23,11 @@ import (
 	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
+const (
+	resourceVersionCachingNamespace = "ns"
+	resourceVersionCachingName      = "name"
+)
+
 func init() {
 	labeled.SetMetricKeys(contextutils.WorkflowIDKey)
 }
@@ -33,14 +38,16 @@ func TestResourceVersionCaching_Get_NotInCache(t *testing.T) {
 
 	scope := promutils.NewTestScope()
 	l := &mockWFNamespaceLister{}
-	wfStore, err := NewResourceVersionCachingStore(ctx, scope, NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l}))
+	passthroughWfStore := NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l})
+	trackTerminatedWfStore, err := NewTerminatedTrackingStore(ctx, scope, passthroughWfStore)
+	wfStore := NewResourceVersionCachingStore(ctx, scope, trackTerminatedWfStore)
 	assert.NoError(t, err)
 
 	t.Run("notFound", func(t *testing.T) {
 		l.GetCb = func(name string) (*v1alpha1.FlyteWorkflow, error) {
-			return nil, kubeerrors.NewNotFound(v1alpha1.Resource(v1alpha1.FlyteWorkflowKind), "name")
+			return nil, kubeerrors.NewNotFound(v1alpha1.Resource(v1alpha1.FlyteWorkflowKind), resourceVersionCachingName)
 		}
-		w, err := wfStore.Get(ctx, "ns", "name")
+		w, err := wfStore.Get(ctx, resourceVersionCachingNamespace, resourceVersionCachingName)
 		assert.Error(t, err)
 		assert.True(t, IsNotFound(err))
 		assert.Nil(t, w)
@@ -48,9 +55,9 @@ func TestResourceVersionCaching_Get_NotInCache(t *testing.T) {
 
 	t.Run("alreadyExists?", func(t *testing.T) {
 		l.GetCb = func(name string) (*v1alpha1.FlyteWorkflow, error) {
-			return nil, kubeerrors.NewAlreadyExists(v1alpha1.Resource(v1alpha1.FlyteWorkflowKind), "name")
+			return nil, kubeerrors.NewAlreadyExists(v1alpha1.Resource(v1alpha1.FlyteWorkflowKind), resourceVersionCachingName)
 		}
-		w, err := wfStore.Get(ctx, "ns", "name")
+		w, err := wfStore.Get(ctx, resourceVersionCachingNamespace, resourceVersionCachingName)
 		assert.Error(t, err)
 		assert.Nil(t, w)
 	})
@@ -59,7 +66,7 @@ func TestResourceVersionCaching_Get_NotInCache(t *testing.T) {
 		l.GetCb = func(name string) (*v1alpha1.FlyteWorkflow, error) {
 			return nil, fmt.Errorf("error")
 		}
-		w, err := wfStore.Get(ctx, "ns", "name")
+		w, err := wfStore.Get(ctx, resourceVersionCachingNamespace, resourceVersionCachingName)
 		assert.Error(t, err)
 		assert.Nil(t, w)
 	})
@@ -69,7 +76,7 @@ func TestResourceVersionCaching_Get_NotInCache(t *testing.T) {
 		l.GetCb = func(name string) (*v1alpha1.FlyteWorkflow, error) {
 			return expW, nil
 		}
-		w, err := wfStore.Get(ctx, "ns", "name")
+		w, err := wfStore.Get(ctx, resourceVersionCachingNamespace, resourceVersionCachingName)
 		assert.NoError(t, err)
 		assert.Equal(t, expW, w)
 	})
@@ -139,15 +146,13 @@ func createFakeClientSet() *fake.Clientset {
 func TestResourceVersionCaching_Get_UpdateAndRead(t *testing.T) {
 	ctx := context.TODO()
 
-	namespace := "ns"
-	name := "name"
 	resourceVersion := "r1"
 
 	mockClient := createFakeClientSet().FlyteworkflowV1alpha1()
 
 	t.Run("Stale", func(t *testing.T) {
-		staleName := name + ".stale"
-		wf := dummyWf(namespace, staleName)
+		staleName := resourceVersionCachingName + ".stale"
+		wf := dummyWf(resourceVersionCachingNamespace, staleName)
 		wf.ResourceVersion = resourceVersion
 
 		scope := promutils.NewTestScope()
@@ -163,13 +168,15 @@ func TestResourceVersionCaching_Get_UpdateAndRead(t *testing.T) {
 		newWf := wf.DeepCopy()
 		newWf.Status.Phase = v1alpha1.WorkflowPhaseSucceeding
 
-		wfStore, err := NewResourceVersionCachingStore(ctx, scope, NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l}))
+		passthroughWfStore := NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l})
+		trackTerminatedWfStore, err := NewTerminatedTrackingStore(ctx, scope, passthroughWfStore)
+		wfStore := NewResourceVersionCachingStore(ctx, scope, trackTerminatedWfStore)
 		assert.NoError(t, err)
 		// Insert a new workflow with R1
 		_, err = wfStore.Update(ctx, newWf, PriorityClassCritical)
 		assert.NoError(t, err)
 
-		w, err := wfStore.Get(ctx, namespace, staleName)
+		w, err := wfStore.Get(ctx, resourceVersionCachingNamespace, staleName)
 		assert.Error(t, err)
 		assert.False(t, IsNotFound(err))
 		assert.True(t, IsWorkflowStale(err))
@@ -177,13 +184,15 @@ func TestResourceVersionCaching_Get_UpdateAndRead(t *testing.T) {
 	})
 
 	t.Run("Updated", func(t *testing.T) {
-		updatedName := name + ".updated"
-		wf := dummyWf(namespace, updatedName)
+		updatedName := resourceVersionCachingName + ".updated"
+		wf := dummyWf(resourceVersionCachingNamespace, updatedName)
 		wf.ResourceVersion = resourceVersion
 
 		scope := promutils.NewTestScope()
 		l := &mockWFNamespaceLister{}
-		wfStore, err := NewResourceVersionCachingStore(ctx, scope, NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l}))
+		passthroughWfStore := NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l})
+		trackTerminatedWfStore, err := NewTerminatedTrackingStore(ctx, scope, passthroughWfStore)
+		wfStore := NewResourceVersionCachingStore(ctx, scope, trackTerminatedWfStore)
 		assert.NoError(t, err)
 		// Insert a new workflow with R1
 		_, err = wfStore.Update(ctx, wf, PriorityClassCritical)
@@ -198,7 +207,7 @@ func TestResourceVersionCaching_Get_UpdateAndRead(t *testing.T) {
 			return wf2, nil
 		}
 
-		w, err := wfStore.Get(ctx, namespace, updatedName)
+		w, err := wfStore.Get(ctx, resourceVersionCachingNamespace, updatedName)
 		assert.NoError(t, err)
 		assert.NotNil(t, w)
 		assert.Equal(t, "r2", w.ResourceVersion)
@@ -207,8 +216,8 @@ func TestResourceVersionCaching_Get_UpdateAndRead(t *testing.T) {
 	// If we -mistakenly- attempted to update the store with the exact workflow object, etcd. will not bump the
 	// resource version. Next read operation should continue to retrieve the same instance of the object.
 	t.Run("NotUpdated", func(t *testing.T) {
-		notUpdatedName := name + ".not-updated"
-		wf := dummyWf(namespace, notUpdatedName)
+		notUpdatedName := resourceVersionCachingName + ".not-updated"
+		wf := dummyWf(resourceVersionCachingNamespace, notUpdatedName)
 		wf.ResourceVersion = resourceVersion
 
 		_, err := mockClient.FlyteWorkflows(wf.GetNamespace()).Create(ctx, wf, v1.CreateOptions{})
@@ -216,7 +225,9 @@ func TestResourceVersionCaching_Get_UpdateAndRead(t *testing.T) {
 
 		scope := promutils.NewTestScope()
 		l := &mockWFNamespaceLister{}
-		wfStore, err := NewResourceVersionCachingStore(ctx, scope, NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l}))
+		passthroughWfStore := NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l})
+		trackTerminatedWfStore, err := NewTerminatedTrackingStore(ctx, scope, passthroughWfStore)
+		wfStore := NewResourceVersionCachingStore(ctx, scope, trackTerminatedWfStore)
 		assert.NoError(t, err)
 		// Insert a new workflow with R1
 		_, err = wfStore.Update(ctx, wf, PriorityClassCritical)
@@ -227,7 +238,7 @@ func TestResourceVersionCaching_Get_UpdateAndRead(t *testing.T) {
 			return wf, nil
 		}
 
-		w, err := wfStore.Get(ctx, namespace, notUpdatedName)
+		w, err := wfStore.Get(ctx, resourceVersionCachingNamespace, notUpdatedName)
 		assert.NoError(t, err)
 		assert.NotNil(t, w)
 		assert.Equal(t, "r1", w.ResourceVersion)
@@ -239,11 +250,9 @@ func TestResourceVersionCaching_UpdateTerminated(t *testing.T) {
 
 	mockClient := createFakeClientSet().FlyteworkflowV1alpha1()
 
-	namespace := "ns"
-	name := "name"
 	resourceVersion := "r1"
 
-	wf := dummyWf(namespace, name)
+	wf := dummyWf(resourceVersionCachingNamespace, resourceVersionCachingName)
 	wf.ResourceVersion = resourceVersion
 
 	_, err := mockClient.FlyteWorkflows(wf.GetNamespace()).Create(ctx, wf, v1.CreateOptions{})
@@ -254,14 +263,16 @@ func TestResourceVersionCaching_UpdateTerminated(t *testing.T) {
 
 	scope := promutils.NewTestScope()
 	l := &mockWFNamespaceLister{}
-	wfStore, err := NewResourceVersionCachingStore(ctx, scope, NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l}))
+	passthroughWfStore := NewPassthroughWorkflowStore(ctx, scope, mockClient, &mockWFLister{V: l})
+	trackTerminatedWfStore, err := NewTerminatedTrackingStore(ctx, scope, passthroughWfStore)
+	wfStore := NewResourceVersionCachingStore(ctx, scope, trackTerminatedWfStore)
 	assert.NoError(t, err)
 	// Insert a new workflow with R1
 	_, err = wfStore.Update(ctx, newWf, PriorityClassCritical)
 	assert.NoError(t, err)
 
 	rvStore := wfStore.(*resourceVersionCaching)
-	v, ok := rvStore.lastUpdatedResourceVersionCache.Load(resourceVersionKey(namespace, name))
+	v, ok := rvStore.lastUpdatedResourceVersionCache.Load(resourceVersionKey(resourceVersionCachingNamespace, resourceVersionCachingName))
 	assert.True(t, ok)
 	assert.Equal(t, resourceVersion, v.(string))
 
@@ -270,15 +281,12 @@ func TestResourceVersionCaching_UpdateTerminated(t *testing.T) {
 	_, err = wfStore.Update(ctx, wf2, PriorityClassCritical)
 	assert.NoError(t, err)
 
-	v, ok = rvStore.lastUpdatedResourceVersionCache.Load(resourceVersionKey(namespace, name))
+	v, ok = rvStore.lastUpdatedResourceVersionCache.Load(resourceVersionKey(resourceVersionCachingNamespace, resourceVersionCachingName))
 	assert.False(t, ok)
 	assert.Nil(t, v)
 
 	// validate that terminated workflows are not retrievable
-	terminated := rvStore.terminatedFilter.Contains(ctx, []byte(resourceVersionKey(namespace, name)))
-	assert.True(t, terminated)
-
-	terminatedWf, err := wfStore.Get(ctx, namespace, name)
+	terminatedWf, err := wfStore.Get(ctx, resourceVersionCachingNamespace, resourceVersionCachingName)
 	assert.Nil(t, terminatedWf)
 	assert.True(t, IsWorkflowTerminated(err))
 }
