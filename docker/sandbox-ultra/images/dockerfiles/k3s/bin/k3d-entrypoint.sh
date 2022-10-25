43d1d4bcc6b39f2827df7986a5285c10435b1249
Yuvraj <evalsocket@users.noreply.github.com>
2022-10-25 09:45:08 +0530

    more changes
    Signed-off-by: Yuvraj <evalsocket@users.noreply.github.com>

--- docker/sandbox-ultra/images/dockerfiles/k3s/bin/k3d-entrypoint.sh
+++ docker/sandbox-ultra/images/dockerfiles/k3s/bin/k3d-entrypoint.sh
@@ -16,15 +16,4 @@ done
 
 echo "[$(date -Iseconds)] Finished k3d entrypoint scripts!" >> "$LOGFILE"
 
-# Flyte will run from /etc/flyte/flyte.yaml as configured in Helm deployment.
-# /etc/flyte/ will be mounted in from host path /srv/flyte/
-# Image should be prebuilt with a default flyte.yaml file stored in /opt/flyte/defaults.flyte.yaml
-# If /srv/flyte/flyte.yaml is not there (as in, not mounted in from the user's state dir) then create it from the default.
-mkdir -p /srv/flyte
-
-if [[ ! -e /srv/flyte/flyte.yaml ]]; then
-  echo "Creating flyte.yaml file from default file"
-  cp /opt/flyte/defaults.flyte.yaml /srv/flyte/flyte.yaml
-fi
-
 exec /bin/k3s "$@"
