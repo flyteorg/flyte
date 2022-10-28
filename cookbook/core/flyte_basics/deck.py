ad5d53ab19c11a5b451dff14c1523573a5c65cff
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2022-10-28 09:26:31 +0530

    Update docs for flytedeck Opt In  (#915)
    * Update deck.py
    
    Signed-off-by: Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
    
    * Update deck.py
    
    Signed-off-by: Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
    
    * Update deck.py
    
    Signed-off-by: Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
    
    * Update deck.py
    
    Signed-off-by: Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
    
    Signed-off-by: Utsav Paul <91927689+Smartmind12@users.noreply.github.com>

--- cookbook/core/flyte_basics/deck.py
+++ cookbook/core/flyte_basics/deck.py
@@ -15,6 +15,10 @@ their data with custom renderers.
 Let's dive into an example.
 
 """
+# %%
+# .. note::
+#
+#   Flyte Decks is an opt-in feature; to enable it, set ``disable_deck`` to ``False`` in the task params.
 
 # %%
 # Import the dependencies.
@@ -33,7 +37,7 @@ iris_df = px.data.iris()
 # %%
 # Create a new deck called ``demo``, and use the box renderer to display the box plot on demo deck.
 # Use MarkdownRenderer to render ``md_text``, and append HTML to the default deck.
-@task
+@task(disable_deck=False)
 def t1() -> str:
     md_text = "#Hello Flyte\n##Hello Flyte\n###Hello Flyte"
     flytekit.Deck("demo", BoxRenderer("sepal_length").to_html(iris_df))
@@ -70,7 +74,7 @@ def t1() -> str:
 
 # %%
 # Use ``Annotated`` to override the default renderer, and display top 10 rows of dataframe.
-@task
+@task(disable_deck=False)
 def t2() -> Annotated[pd.DataFrame, TopFrameRenderer(10)]:
     return iris_df
 
