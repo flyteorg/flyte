ab3ee8a0423b026be7afbdde537d08a763b30483
Kevin Su <pingsutw@apache.org>
2022-12-20 05:12:10 +0800

    Add support Databricks plugin (#299)
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- go/tasks/plugins/webapi/databricks/config.go
+++ go/tasks/plugins/webapi/databricks/config.go
@@ -1,4 +1,4 @@
-package snowflake
+package databricks
 
 import (
 	"time"
@@ -10,6 +10,9 @@ import (
 )
 
 var (
+	defaultCluster = "COMPUTE_CLUSTER"
+	tokenKey       = "FLYTE_DATABRICKS_API_TOKEN" // nolint: gosec
+
 	defaultConfig = Config{
 		WebAPI: webapi.PluginConfig{
 			ResourceQuotas: map[core.ResourceNamespace]int{
@@ -39,14 +42,14 @@ var (
 				Value: 50,
 			},
 		},
-		DefaultWarehouse: "COMPUTE_WH",
-		TokenKey:         "FLYTE_SNOWFLAKE_CLIENT_TOKEN",
+		DefaultCluster: defaultCluster,
+		TokenKey:       tokenKey,
 	}
 
-	configSection = pluginsConfig.MustRegisterSubSection("snowflake", &defaultConfig)
+	configSection = pluginsConfig.MustRegisterSubSection("databricks", &defaultConfig)
 )
 
-// Config is config for 'snowflake' plugin
+// Config is config for 'databricks' plugin
 type Config struct {
 	// WebAPI defines config for the base WebAPI plugin
 	WebAPI webapi.PluginConfig `json:"webApi" pflag:",Defines config for the base WebAPI plugin."`
@@ -54,12 +57,15 @@ type Config struct {
 	// ResourceConstraints defines resource constraints on how many executions to be created per project/overall at any given time
 	ResourceConstraints core.ResourceConstraintsSpec `json:"resourceConstraints" pflag:"-,Defines resource constraints on how many executions to be created per project/overall at any given time."`
 
-	DefaultWarehouse string `json:"defaultWarehouse" pflag:",Defines the default warehouse to use when running on Snowflake unless overwritten by the task."`
+	DefaultCluster string `json:"defaultWarehouse" pflag:",Defines the default warehouse to use when running on Databricks unless overwritten by the task."`
 
-	TokenKey string `json:"snowflakeTokenKey" pflag:",Name of the key where to find Snowflake token in the secret manager."`
+	TokenKey string `json:"databricksTokenKey" pflag:",Name of the key where to find Databricks token in the secret manager."`
 
-	// snowflakeEndpoint overrides Snowflake client endpoint, only for testing
-	snowflakeEndpoint string
+	DatabricksInstance string `json:"databricksInstance" pflag:",Databricks workspace instance name."`
+
+	EntrypointFile string `json:"entrypointFile" pflag:",A URL of the entrypoint file. DBFS and cloud storage (s3://, gcs://, adls://, etc) locations are supported."`
+	// databricksEndpoint overrides databricks instance endpoint, only for testing
+	databricksEndpoint string
 }
 
 func GetConfig() *Config {
