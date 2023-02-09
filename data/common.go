97772fdb3d89f29d428df3ce858ade73d9a2518c
Dan Rammer <daniel@union.ai>
2023-02-09 09:46:50 -0600

    Bumping go version to 1.19 (#45)
    * bumping go version to 1.19
    
    Signed-off-by: Daniel Rammer <hamersaw@protonmail.com>
    
    * updated dockerfile
    
    Signed-off-by: Daniel Rammer <hamersaw@protonmail.com>
    
    ---------
    
    Signed-off-by: Daniel Rammer <hamersaw@protonmail.com>

--- data/common.go
+++ data/common.go
@@ -1,12 +1,15 @@
 // This module contains Flyte CoPilot related code.
 // Currently it only has 2 utilities - downloader and an uploader.
 // Usage Downloader:
-//  downloader := NewDownloader(...)
-//  downloader.DownloadInputs(...) // will recursively download all inputs
+//
+//	downloader := NewDownloader(...)
+//	downloader.DownloadInputs(...) // will recursively download all inputs
 //
 // Usage uploader:
-// uploader := NewUploader(...)
-// uploader.RecursiveUpload(...) // Will recursively upload all the data from the given path depending on the output interface
+//
+//	uploader := NewUploader(...)
+//	uploader.RecursiveUpload(...) // Will recursively upload all the data from the given path depending on the output interface
+//
 // All errors are bubbled up.
 //
 // Both the uploader and downloader accept context.Context variables. These should be used to control timeouts etc.
