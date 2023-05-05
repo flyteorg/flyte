a579e926efc10ddb8cc9d74869ab5082ff03276b
Kevin Su <pingsutw@apache.org>
2023-05-05 13:27:28 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- boilerplate/flyte/docker_build/docker_build.sh
+++ boilerplate/flyte/docker_build/docker_build.sh
@@ -40,7 +40,7 @@ fi
 
 # build the image
 # passing no build phase will build the final image
-docker build -t "$IMAGE_TAG_WITH_SHA" --target=${BUILD_PHASE} ../
+docker build -t "$IMAGE_TAG_WITH_SHA" --target=${BUILD_PHASE} .
 echo "${IMAGE_TAG_WITH_SHA} built locally."
 
 # if REGISTRY specified, push the images to the remote registy
