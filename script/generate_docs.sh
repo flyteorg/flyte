f29663bb83a423b2ba746491ad3da16bef43de77
Niels Bantilan <niels.bantilan@gmail.com>
2023-07-28 10:03:15 -0400

    rename rsts to docs
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- script/generate_docs.sh
+++ script/generate_docs.sh
@@ -15,8 +15,8 @@ mkdir ${BASEDIR}/rsts_tmp || true
 RSTS_DIR=`mktemp -d "${BASEDIR}/rsts_tmp/XXXXXXXXX"`
 
 # Copy all rst files to the same place
-cp -R rsts/* ${RSTS_DIR}
-cp -R _rsts/* ${RSTS_DIR}
+cp -R docs/* ${RSTS_DIR}
+cp -R docs/* ${RSTS_DIR}
 
 # Generate documentation by running script inside the generation container
 docker run --rm -t -e FLYTEKIT_VERSION=${FLYTEKIT_VERSION} -v ${BASEDIR}:/base -v ${BASEDIR}/docs:/docs -v ${RSTS_DIR}:/rsts ghcr.io/nuclyde-io/docbuilder:e70cfafe3397c3b23d11183973c0f44e0024f025 /base/docs_infra/in_container_html_generation.sh
