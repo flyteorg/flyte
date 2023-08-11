79e3a9a5ec1cf3dd86baef515cd8d13793917693
Iaroslav Ciupin <iaroslav@union.ai>
2023-08-11 22:52:30 +0300

    Handle grpc error explicitly (#602)
    Signed-off-by: Iaroslav Ciupin <iaroslav@union.ai>

--- flyteadmin/pkg/rpc/adminservice/util/transformers_test.go
+++ flyteadmin/pkg/rpc/adminservice/util/transformers_test.go
@@ -3,22 +3,25 @@ package util
 import (
 	"context"
 	"errors"
+	"strings"
 	"testing"
 
-	"github.com/flyteorg/flyteadmin/pkg/common"
-
-	adminErrors "github.com/flyteorg/flyteadmin/pkg/errors"
 	mockScope "github.com/flyteorg/flytestdlib/promutils"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/flyteorg/flyteadmin/pkg/common"
+	adminErrors "github.com/flyteorg/flyteadmin/pkg/errors"
 )
 
 var testRequestMetrics = NewRequestMetrics(mockScope.NewTestScope(), "foo")
 
 func TestTransformError_FlyteAdminError(t *testing.T) {
 	invalidArgError := adminErrors.NewFlyteAdminError(codes.InvalidArgument, "invalid arg")
+
 	transformedError := TransformAndRecordError(invalidArgError, &testRequestMetrics)
+
 	transormerStatus, ok := status.FromError(transformedError)
 	assert.True(t, ok)
 	assert.Equal(t, codes.InvalidArgument, transormerStatus.Code())
@@ -26,28 +29,43 @@ func TestTransformError_FlyteAdminError(t *testing.T) {
 
 func TestTransformError_FlyteAdminErrorWithDetails(t *testing.T) {
 	terminalStateError := adminErrors.NewAlreadyInTerminalStateError(context.Background(), "terminal state", "curPhase")
+
 	transformedError := TransformAndRecordError(terminalStateError, &testRequestMetrics)
+
 	transormerStatus, ok := status.FromError(transformedError)
 	assert.True(t, ok)
 	assert.Equal(t, codes.FailedPrecondition, transormerStatus.Code())
 	assert.Equal(t, 1, len(transormerStatus.Details()))
 }
 
+func TestTransformError_GRPCError(t *testing.T) {
+	err := status.Error(codes.InvalidArgument, strings.Repeat("X", common.MaxResponseStatusBytes+1))
+
+	transformedError := TransformAndRecordError(err, &testRequestMetrics)
+
+	transormerStatus, ok := status.FromError(transformedError)
+	assert.True(t, ok)
+	assert.Equal(t, codes.InvalidArgument, transormerStatus.Code())
+	assert.Len(t, transormerStatus.Message(), common.MaxResponseStatusBytes)
+}
+
 func TestTransformError_BasicError(t *testing.T) {
 	err := errors.New("some error")
+
 	transformedError := TransformAndRecordError(err, &testRequestMetrics)
+
 	transormerStatus, ok := status.FromError(transformedError)
 	assert.True(t, ok)
 	assert.Equal(t, codes.Internal, transormerStatus.Code())
 }
 
 func TestTruncateErrorMessage(t *testing.T) {
-	errorMessage := make([]byte, common.MaxResponseStatusBytes+1)
-	for i := 0; i <= common.MaxResponseStatusBytes; i++ {
-		errorMessage[i] = byte('a')
-	}
+	err := adminErrors.NewFlyteAdminError(codes.InvalidArgument, strings.Repeat("X", common.MaxResponseStatusBytes+1))
 
-	err := adminErrors.NewFlyteAdminError(codes.InvalidArgument, string(errorMessage))
 	transformedError := TransformAndRecordError(err, &testRequestMetrics)
-	assert.Len(t, transformedError.Error(), common.MaxResponseStatusBytes)
+
+	transormerStatus, ok := status.FromError(transformedError)
+	assert.True(t, ok)
+	assert.Equal(t, codes.InvalidArgument, transormerStatus.Code())
+	assert.Len(t, transormerStatus.Message(), common.MaxResponseStatusBytes)
 }
