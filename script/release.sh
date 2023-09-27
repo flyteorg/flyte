ee9056952853a8d440cc69747c180d8201b6324d
Kevin Su <pingsutw@apache.org>
2023-09-27 10:13:28 -0700

    Update release script (#4084)
    * Update release script
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * Update tag
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * nit
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * nit
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * nit
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * make helm
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- script/release.sh
+++ script/release.sh
@@ -7,7 +7,7 @@ set -ex
 #                 In other words, if we have two consecutive releases the latest tag in the second release is going to point to an invalid
 #                 tag (because there will not be images tagged with the previous release tag).
 LATEST_TAG=$(git tag | sed 's#[^/]*/##' | sort | tail -n 1)
-
+FLYTEKIT_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flytekit/releases/latest" | jq -r .tag_name | sed 's/^v//')
 FLYTECONSOLE_TAG=$(curl --silent "https://api.github.com/repos/flyteorg/flyteconsole/releases/latest" | jq -r .tag_name)
 
 # bump latest release of flyte component in kustomize
@@ -35,6 +35,8 @@ sed -i "s,tag:[^P]*# FLYTEPROPELLER_TAG,tag: ${LATEST_TAG} # FLYTEPROPELLER_TAG,
 sed -i "s,image:[^P]*# FLYTECOPILOT_IMAGE,image: cr.flyte.org/flyteorg/flytecopilot:${LATEST_TAG} # FLYTECOPILOT_IMAGE," ./charts/flyte/values.yaml
 sed -i "s,image:[^P]*# FLYTECOPILOT_IMAGE,image: cr.flyte.org/flyteorg/flytecopilot:${LATEST_TAG} # FLYTECOPILOT_IMAGE," ./charts/flyte-core/values.yaml
 sed -i "s,tag:[^P]*# FLYTECOPILOT_TAG,tag: ${LATEST_TAG} # FLYTECOPILOT_TAG," ./charts/flyte-binary/values.yaml
+
+sed -i "s,tag:[^P]*# FLYTEAGENT_TAG,tag: ${FLYTEKIT_TAG} # FLYTEAGENT_TAG," ./charts/flyteagent/values.yaml
 
 go get github.com/flyteorg/flyte/flyteadmin@${LATEST_TAG}
 go get github.com/flyteorg/flyte/flytepropeller@${LATEST_TAG}
