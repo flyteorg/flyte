b48be8a2da3806fc90d7639e94c9eb3d4c378986
squiishyy <joe@union.ai>
2023-10-17 16:41:57 -0700

    make RetryOnSpecificErrorCodes, added functionality for calling sendhttprequestwithretry, need to work on config mappingn still, tests in
    Signed-off-by: squiishyy <joe@union.ai>

--- flyteadmin/pkg/async/shared.go
+++ flyteadmin/pkg/async/shared.go
@@ -2,6 +2,7 @@ package async
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/flyteorg/flyte/flytestdlib/logger"
@@ -9,6 +10,24 @@ import (
 
 // RetryDelay indicates how long to wait between restarting a subscriber connection in the case of network failures.
 var RetryDelay = 30 * time.Second
+
+func RetryOnSpecificErrorCodes(attempts int, delay time.Duration, f func() (*http.Response, error), IsErrorCodeRetryable func(*http.Response) bool) error {
+	var err error
+	var resp *http.Response
+	for attempt := 0; attempt <= attempts; attempt++ {
+		resp, err = f()
+		if err != nil {
+			return err
+		}
+		if !IsErrorCodeRetryable(resp) {
+			return err
+		}
+		logger.Warningf(context.Background(),
+			"Failed status code %v on attempt %d of %d", resp.StatusCode, attempt, attempts)
+		time.Sleep(delay)
+	}
+	return err
+}
 
 func RetryOnSpecificErrors(attempts int, delay time.Duration, f func() error, IsErrorRetryable func(error) bool) error {
 	var err error
