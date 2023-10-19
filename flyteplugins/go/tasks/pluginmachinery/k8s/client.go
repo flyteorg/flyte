5f482badd3b0fb5bcaea1d9cd5d3711d1b6e7dd5
Haytham Abuelfutuh <haytham@afutuh.com>
2023-10-19 09:23:34 -0700

    lint for plugins
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- flyteplugins/go/tasks/pluginmachinery/k8s/client.go
+++ flyteplugins/go/tasks/pluginmachinery/k8s/client.go
@@ -3,10 +3,10 @@
 package k8s
 
 import (
-	"context"
+	"net/http"
+
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/rest"
-	"net/http"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
@@ -31,49 +31,6 @@ func newKubeClient(c client.Client, cache cache.Cache) core.KubeClient {
 	return &kubeClient{client: c, cache: cache}
 }
 
-type fallbackClientReader struct {
-	orderedClients []client.Reader
-}
-
-func (c fallbackClientReader) Get(ctx context.Context, key client.ObjectKey, out client.Object) (err error) {
-	for _, k8sClient := range c.orderedClients {
-		if err = k8sClient.Get(ctx, key, out); err == nil {
-			return nil
-		}
-	}
-
-	return
-}
-
-func (c fallbackClientReader) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) (err error) {
-	for _, k8sClient := range c.orderedClients {
-		if err = k8sClient.List(ctx, list, opts...); err == nil {
-			return nil
-		}
-	}
-
-	return
-}
-
-// ClientBuilder builder is the interface for the client builder.
-type ClientBuilder interface {
-	// Build returns a new client.
-	Build(cache cache.Cache, config *rest.Config, options client.Options) (client.Client, error)
-}
-
-type fallbackClientBuilder struct {
-}
-
-func (f *fallbackClientBuilder) Build(_ cache.Cache, config *rest.Config, options client.Options) (client.Client, error) {
-	return client.New(config, options)
-}
-
-// Creates a new k8s client that uses the cached client for reads and falls back to making API
-// calls if it failed. Write calls will always go to raw client directly.
-func NewFallbackClientBuilder() ClientBuilder {
-	return &fallbackClientBuilder{}
-}
-
 type Options struct {
 	MapperProvider func(*rest.Config) (meta.RESTMapper, error)
 	CacheOptions   *cache.Options
@@ -88,13 +45,17 @@ func NewKubeClient(config *rest.Config, options Options) (core.KubeClient, error
 			return apiutil.NewDynamicRESTMapper(config, http.DefaultClient)
 		}
 	}
+
 	mapper, err := options.MapperProvider(config)
 	if err != nil {
 		return nil, err
 	}
 
 	if options.CacheOptions == nil {
-		options.CacheOptions = &cache.Options{Mapper: mapper}
+		options.CacheOptions = &cache.Options{
+			HTTPClient: http.DefaultClient,
+			Mapper:     mapper,
+		}
 	}
 
 	cache, err := cache.New(config, *options.CacheOptions)
@@ -106,12 +67,12 @@ func NewKubeClient(config *rest.Config, options Options) (core.KubeClient, error
 		options.ClientOptions = &client.Options{Mapper: mapper}
 	}
 
-	fallbackClient, err := NewFallbackClientBuilder().Build(cache, config, *options.ClientOptions)
+	client, err := client.New(config, *options.ClientOptions)
 	if err != nil {
 		return nil, err
 	}
 
-	return newKubeClient(fallbackClient, cache), nil
+	return newKubeClient(client, cache), nil
 }
 
 // NewDefaultKubeClient creates a new KubeClient with default options set.
