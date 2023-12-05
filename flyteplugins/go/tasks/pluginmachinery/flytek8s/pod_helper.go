eea43f39e2f628f3d60135c03786295957cd7ba4
Paul Dittamo <pvdittamo@gmail.com>
2023-12-05 11:26:58 -0800

    cleanup
    Signed-off-by: Paul Dittamo <pvdittamo@gmail.com>

--- flyteplugins/go/tasks/pluginmachinery/flytek8s/pod_helper.go
+++ flyteplugins/go/tasks/pluginmachinery/flytek8s/pod_helper.go
@@ -579,7 +579,7 @@ func DemystifyPending(status v1.PodStatus) (pluginsCore.PhaseInfo, error) {
 	}
 
 	podPendingTimeout := config.GetK8sPluginConfig().PodPendingTimeout.Duration
-	if podPendingTimeout > 0 && time.Since(t) >= config.GetK8sPluginConfig().PodPendingTimeout.Duration {
+	if podPendingTimeout > 0 && time.Since(t) >= podPendingTimeout {
 		return pluginsCore.PhaseInfoRetryableFailureWithCleanup("PodPendingTimeout", phaseInfo.Reason(), &pluginsCore.TaskInfo{
 			OccurredAt: &t,
 		}), nil
