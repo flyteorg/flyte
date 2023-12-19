4b45194a726cc3878c64de346f5850dcbf432a1f
Chris Grass <chris.grass@mcg.com>
2023-12-19 11:56:56 -0600

    Add azure_remote_url and corresponding test.
    Signed-off-by: Chris Grass <chris.grass@mcg.com>

---
+++ flyteadmin/pkg/data/implementations/azure_remote_url_test.go
@@ -0,0 +1,38 @@
+package implementations
+
+import (
+	"context"
+	commonMocks "github.com/flyteorg/flyte/flyteadmin/pkg/common/mocks"
+	"github.com/flyteorg/flyte/flytestdlib/storage"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type mockMetadata struct{}
+
+func (m mockMetadata) Exists() bool {
+	return true
+}
+
+func (m mockMetadata) Size() int64 {
+	return 1
+}
+
+func (m mockMetadata) Etag() string {
+	return "etag"
+}
+
+func TestAzureGet(t *testing.T) {
+	inputUri := "abfs//test/data"
+	mockStorage := commonMocks.GetMockStorageClient()
+	mockStorage.ComposedProtobufStore.(*commonMocks.TestDataStore).HeadCb =
+		func(ctx context.Context, reference storage.DataReference) (storage.Metadata, error) {
+			return mockMetadata{}, nil
+		}
+	remoteUrl := AzureRemoteURL{
+		remoteDataStoreClient: *mockStorage, presignDuration: 1,
+	}
+
+	result, _ := remoteUrl.Get(context.TODO(), inputUri)
+	assert.Contains(t, inputUri, result.Url)
+}
