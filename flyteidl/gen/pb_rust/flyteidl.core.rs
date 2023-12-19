b5625363035a4e12f8f498dbf49fd282449ce698
Dan Rammer <daniel@union.ai>
2023-12-19 08:43:10 -0600

    Ignore cache variables (#4618)
    * Provide a way to ignore portions of the signature for cache key calculation purposes
    
    Signed-off-by: troychiu <y.troychiu@gmail.com>
    
    * add stub
    
    Signed-off-by: troychiu <y.troychiu@gmail.com>
    
    * moved cache ignore input vars check to HashLiteralMap function
    
    Signed-off-by: Daniel Rammer <daniel@union.ai>
    
    * working with maptask
    
    Signed-off-by: Daniel Rammer <daniel@union.ai>
    
    ---------
    
    Signed-off-by: troychiu <y.troychiu@gmail.com>
    Signed-off-by: Daniel Rammer <daniel@union.ai>
    Co-authored-by: troychiu <y.troychiu@gmail.com>
    Co-authored-by: Kevin Su <pingsutw@apache.org>

--- flyteidl/gen/pb_rust/flyteidl.core.rs
+++ flyteidl/gen/pb_rust/flyteidl.core.rs
@@ -1134,6 +1134,9 @@ pub struct TaskMetadata {
     /// identically as, the default PodTemplate configured in FlytePropeller.
     #[prost(string, tag="12")]
     pub pod_template_name: ::prost::alloc::string::String,
+    /// cache_ignore_input_vars is the input variables that should not be included when calculating hash for cache.
+    #[prost(string, repeated, tag="13")]
+    pub cache_ignore_input_vars: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
     // For interruptible we will populate it at the node level but require it be part of TaskMetadata
     // for a user to set the value.
     // We are using oneof instead of bool because otherwise we would be unable to distinguish between value being
