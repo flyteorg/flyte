d48d9e87b27bab395089fa96496fb4ea85ec35fa
Paul Dittamo <pvdittamo@gmail.com>
2024-01-05 16:03:04 -0800

    lint
    Signed-off-by: Paul Dittamo <pvdittamo@gmail.com>

--- flyteadmin/boilerplate/flyte/golang_test_targets/download_tooling.sh
+++ flyteadmin/boilerplate/flyte/golang_test_targets/download_tooling.sh
@@ -17,7 +17,7 @@ set -e
 # In the format of "<cli>:<package>" or ":<package>" if no cli
 tools=(
   "github.com/EngHabu/mockery/cmd/mockery"
-  "github.com/flyteorg/flyte/flytestdlib/cli/pflags@latest"
+  "github.com/flyteorg/flytestdlib/cli/pflags@latest"
   "github.com/golangci/golangci-lint/cmd/golangci-lint"
   "github.com/daixiang0/gci"
   "github.com/alvaroloes/enumer"
