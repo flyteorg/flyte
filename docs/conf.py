ac425629047a206532645854c30d14a5a25f4c2b
Niels Bantilan <niels.bantilan@gmail.com>
2024-01-09 17:47:29 -0500

    add algolia searchbar (#4696)
    * add algolia searchbar
    
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>
    
    * update css and search.html filepath
    
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>
    
    ---------
    
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -186,7 +186,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["custom.css", "flyte.css"]
+html_css_files = ["custom.css", "flyte.css", "algolia.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
