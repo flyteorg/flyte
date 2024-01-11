d30de343a83a3e414b0b952a50982c7d0652145c
Niels Bantilan <niels.bantilan@gmail.com>
2024-01-11 12:39:16 -0500

    add monodocs index page in flytectl (#453)
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytectl/docs/source/conf.py
+++ flytectl/docs/source/conf.py
@@ -78,7 +78,13 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = [u"_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    u"_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "docs_index.rst",
+    "overview.rst",
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "tango"
