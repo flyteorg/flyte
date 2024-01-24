0721a7e4066f26df75cdaee4137c4bfe25f3f689
Haytham Abuelfutuh <haytham@afutuh.com>
2024-01-24 12:35:52 -0800

    test
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- flytepropeller/pkg/controller/nodes/output_resolver_test.go
+++ flytepropeller/pkg/controller/nodes/output_resolver_test.go
@@ -1,11 +1,7 @@
 package nodes
 
 import (
-	"os"
 	"testing"
-
-	protoV1 "github.com/golang/protobuf/proto"
-	"google.golang.org/protobuf/proto"
 
 	"github.com/stretchr/testify/assert"
 
@@ -32,31 +28,4 @@ func TestCreateAliasMap(t *testing.T) {
 		m := CreateAliasMap(nil)
 		assert.Equal(t, map[string]string{}, m)
 	}
-}
-
-func TestOutputTemp(t *testing.T) {
-	f, err := os.ReadFile("/Users/haytham/Downloads/outputs.pb")
-	assert.NoError(t, err)
-
-	msg := []protoV1.Message{&core.OutputData{}, &core.LiteralMap{}}
-	var lastErr error
-	var index int
-	for i, m := range msg {
-		err = proto.UnmarshalOptions{DiscardUnknown: false, AllowPartial: false}.Unmarshal(f, protoV1.MessageV2(m))
-		if err != nil {
-			lastErr = err
-			continue
-		}
-
-		if len(msg) == 1 || len(protoV1.MessageV2(m).ProtoReflect().GetUnknown()) == 0 {
-			index = i
-			lastErr = nil
-			break
-		}
-	}
-
-	assert.NoError(t, lastErr)
-	t.Log(index)
-	t.Log(msg[index])
-	t.FailNow()
 }
