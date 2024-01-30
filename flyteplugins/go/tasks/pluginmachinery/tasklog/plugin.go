144ccd91ed8b38326d60540407721ba9faff3bd5
Eduardo Apolinario <653394+eapolinario@users.noreply.github.com>
2024-01-30 11:54:18 -0800

    Bring Scheme back for backwards compatibility (#4789)
    * Bring Scheme back for backwards compatibility
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Rename deprecated field to `DeprecatedScheme`
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flyteplugins/go/tasks/pluginmachinery/tasklog/plugin.go
+++ flyteplugins/go/tasks/pluginmachinery/tasklog/plugin.go
@@ -7,6 +7,15 @@ import (
 	pluginsCore "github.com/flyteorg/flyte/flyteplugins/go/tasks/pluginmachinery/core"
 )
 
+//go:generate enumer --type=TemplateScheme --trimprefix=TemplateScheme -json -yaml
+
+type TemplateScheme int
+
+const (
+	TemplateSchemePod TemplateScheme = iota
+	TemplateSchemeTaskExecution
+)
+
 // TemplateURI is a URI that accepts templates. See: go/tasks/pluginmachinery/tasklog/template.go for available templates.
 type TemplateURI = string
 
@@ -49,6 +58,8 @@ type TemplateLogPlugin struct {
 	Name                string                     `json:"name" pflag:",Name of the plugin."`
 	DisplayName         string                     `json:"displayName" pflag:",Display name for the generated log when displayed in the console."`
 	TemplateURIs        []TemplateURI              `json:"templateUris" pflag:",URI Templates for generating task log links."`
-	DynamicTemplateURIs []TemplateURI              `json:"dynamictemplateUris" pflag:",URI Templates for generating dynamic task log links."`
-	MessageFormat       core.TaskLog_MessageFormat `json:"messageFormat" pflag:",Log Message Format."`
+	DynamicTemplateURIs []TemplateURI              `json:"dynamicTemplateUris" pflag:",URI Templates for generating dynamic task log links."`
+	MessageFormat       core.TaskLog_MessageFormat `json:"messageFormat" pflag:"-,Log Message Format."`
+	// Deprecated: Please, do not use
+	DeprecatedScheme TemplateScheme `json:"scheme" pflag:",Templating scheme to use. Supported values are Pod and TaskExecution."`
 }
