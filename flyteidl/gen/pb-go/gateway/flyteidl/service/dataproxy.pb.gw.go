aaa2519150b3bde7dee6d07a5d92b9925f6a4ca3
Katrina Rogan <katroganGH@gmail.com>
2024-03-18 21:28:51 -0700

    SVR-387: Add org additional binding for CreateUploadLocation (#169)
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- flyteidl/gen/pb-go/gateway/flyteidl/service/dataproxy.pb.gw.go
+++ flyteidl/gen/pb-go/gateway/flyteidl/service/dataproxy.pb.gw.go
@@ -66,6 +66,40 @@ func local_request_DataProxyService_CreateUploadLocation_0(ctx context.Context, 
 
 }
 
+func request_DataProxyService_CreateUploadLocation_1(ctx context.Context, marshaler runtime.Marshaler, client extService.DataProxyServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq extService.CreateUploadLocationRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.CreateUploadLocation(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_DataProxyService_CreateUploadLocation_1(ctx context.Context, marshaler runtime.Marshaler, server extService.DataProxyServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq extService.CreateUploadLocationRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.CreateUploadLocation(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 var (
 	filter_DataProxyService_CreateDownloadLocation_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
@@ -200,6 +234,31 @@ func RegisterDataProxyServiceHandlerServer(ctx context.Context, mux *runtime.Ser
 		}
 
 		forward_DataProxyService_CreateUploadLocation_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_DataProxyService_CreateUploadLocation_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/flyteidl.service.DataProxyService/CreateUploadLocation", runtime.WithHTTPPathPattern("/api/v1/org/dataproxy/artifact_urn"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_DataProxyService_CreateUploadLocation_1(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DataProxyService_CreateUploadLocation_1(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -341,6 +400,28 @@ func RegisterDataProxyServiceHandlerClient(ctx context.Context, mux *runtime.Ser
 
 	})
 
+	mux.Handle("POST", pattern_DataProxyService_CreateUploadLocation_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/flyteidl.service.DataProxyService/CreateUploadLocation", runtime.WithHTTPPathPattern("/api/v1/org/dataproxy/artifact_urn"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_DataProxyService_CreateUploadLocation_1(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DataProxyService_CreateUploadLocation_1(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_DataProxyService_CreateDownloadLocation_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -413,6 +494,8 @@ func RegisterDataProxyServiceHandlerClient(ctx context.Context, mux *runtime.Ser
 var (
 	pattern_DataProxyService_CreateUploadLocation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "dataproxy", "artifact_urn"}, ""))
 
+	pattern_DataProxyService_CreateUploadLocation_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "v1", "org", "dataproxy", "artifact_urn"}, ""))
+
 	pattern_DataProxyService_CreateDownloadLocation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "dataproxy", "artifact_urn"}, ""))
 
 	pattern_DataProxyService_CreateDownloadLink_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "dataproxy", "artifact_link"}, ""))
@@ -422,6 +505,8 @@ var (
 
 var (
 	forward_DataProxyService_CreateUploadLocation_0 = runtime.ForwardResponseMessage
+
+	forward_DataProxyService_CreateUploadLocation_1 = runtime.ForwardResponseMessage
 
 	forward_DataProxyService_CreateDownloadLocation_0 = runtime.ForwardResponseMessage
 
