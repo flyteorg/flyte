0e235e191b941e2b7951ddf695e33b6811eb3a57
Kevin Su <pingsutw@gmail.com>
2024-03-27 13:07:11 -0700

    update mock
    Signed-off-by: Kevin Su <pingsutw@gmail.com>

--- flyteplugins/go/tasks/pluginmachinery/core/secret_manager.go
+++ flyteplugins/go/tasks/pluginmachinery/core/secret_manager.go
@@ -5,6 +5,8 @@ import (
 	coreIdl "github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/core"
 )
 
+//go:generate mockery -all -output=./mocks -case=underscore
+
 type SecretManager interface {
 	Get(ctx context.Context, key string) (string, error)
 	GetForSecret(ctx context.Context, secret *coreIdl.Secret) (string, error)
