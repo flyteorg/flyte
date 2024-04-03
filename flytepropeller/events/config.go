2e099103706d8fa36ca14a6d3ce0dcb0336e9591
Paul Dittamo <pvdittamo@gmail.com>
2024-04-02 19:51:32 -0700

    add retries and backoffs for propeller sending events to admin
    Signed-off-by: Paul Dittamo <pvdittamo@gmail.com>

--- flytepropeller/events/config.go
+++ flytepropeller/events/config.go
@@ -21,17 +21,23 @@ const (
 )
 
 type Config struct {
-	Type     EventReportingType `json:"type" pflag:",Sets the type of EventSink to configure [log/admin/file]."`
-	FilePath string             `json:"file-path" pflag:",For file types, specify where the file should be located."`
-	Rate     int64              `json:"rate" pflag:",Max rate at which events can be recorded per second."`
-	Capacity int                `json:"capacity" pflag:",The max bucket size for event recording tokens."`
+	Type          EventReportingType `json:"type" pflag:",Sets the type of EventSink to configure [log/admin/file]."`
+	FilePath      string             `json:"file-path" pflag:",For file types, specify where the file should be located."`
+	Rate          int64              `json:"rate" pflag:",Max rate at which events can be recorded per second."`
+	Capacity      int                `json:"capacity" pflag:",The max bucket size for event recording tokens."`
+	MaxRetries    uint               `json:"max-retries" pflag:",The max number of retries for event recording."`
+	BackoffScalar int                `json:"base-scalar" pflag:",The base/scalar backoff duration in milliseconds for event recording retries."`
+	BackoffJitter float64            `json:"backoff-jitter" pflag:",The jitter factor for event recording retries."`
 }
 
 var (
 	defaultConfig = Config{
-		Rate:     int64(500),
-		Capacity: 1000,
-		Type:     EventSinkAdmin,
+		Rate:          int64(500),
+		Capacity:      1000,
+		Type:          EventSinkAdmin,
+		MaxRetries:    5,
+		BackoffScalar: 100,
+		BackoffJitter: 0.1,
 	}
 
 	configSection = config.MustRegisterSection(configSectionKey, &defaultConfig)
