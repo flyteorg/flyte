cfdd43df8fb1d1473c60c8525c2ff016d194dd5f
ddl-rliu <richard.liu@dominodatalab.com>
2024-04-05 15:49:14 -0700

    x
    Signed-off-by: ddl-rliu <richard.liu@dominodatalab.com>
    
    x
    
    Signed-off-by: ddl-rliu <richard.liu@dominodatalab.com>
    
    x
    
    Signed-off-by: ddl-rliu <richard.liu@dominodatalab.com>

---
+++ flyteadmin/auth/auth_context_test.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+
+	"github.com/flyteorg/flyte/flyteadmin/auth/config"
+)
+
+func TestOAuth2ClientConfig(t *testing.T) {
+	authCtx := Context{
+		oauth2Client: &oauth2.Config{},
+	}
+
+	type test struct {
+		name                string
+		url                 string
+		expectedRedirectURL string
+	}
+	tests := []test{
+		{
+			name:                "simple publicUrl",
+			url:                 "https://flyte.com",
+			expectedRedirectURL: "https://flyte.com/callback",
+		},
+		{
+			name:                "custom subpath",
+			url:                 "https://flyte.com/custom-subpath/console",
+			expectedRedirectURL: "https://flyte.com/custom-subpath/callback",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := authCtx.OAuth2ClientConfig(config.MustParseURL(tt.url))
+			assert.Equal(t, tt.expectedRedirectURL, cfg.RedirectURL)
+		})
+	}
+}
