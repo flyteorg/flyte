17a66315c58152bd19623ca5c7de127123fc8a99
troychiu <y.troychiu@gmail.com>
2024-04-05 13:31:53 -0700

    Refactor configuration transformer functions
    Signed-off-by: troychiu <y.troychiu@gmail.com>

--- flyteadmin/pkg/repositories/transformers/configuration.go
+++ flyteadmin/pkg/repositories/transformers/configuration.go
@@ -1,6 +1,9 @@
 package transformers
 
-import "github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/admin"
+import (
+	"github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/admin"
+	"github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/core"
+)
 
 // get the real type of matching attributes
 func GetConfigurationFromMatchingAttributes(matchingAttributes *admin.MatchingAttributes) *admin.Configuration {
@@ -26,24 +29,24 @@ func GetConfigurationFromMatchingAttributes(matchingAttributes *admin.MatchingAt
 	}
 }
 
-func GetConfigurationFromMatchableResource(matchableResource *admin.MatchableResource) *admin.Configuration {
+func GetConfigurationFromMatchableResource(matchableResource admin.MatchableResource) *admin.Configuration {
 	switch matchableResource {
 	case admin.MatchableResource_TASK_RESOURCE:
-		return &admin.Configuration{TaskResourceAttributes: x.TaskResourceAttributes}
-	case *admin.MatchingAttributes_ClusterResourceAttributes:
-		return &admin.Configuration{ClusterResourceAttributes: x.ClusterResourceAttributes}
-	case *admin.MatchingAttributes_ExecutionQueueAttributes:
-		return &admin.Configuration{ExecutionQueueAttributes: x.ExecutionQueueAttributes}
-	case *admin.MatchingAttributes_ExecutionClusterLabel:
-		return &admin.Configuration{ExecutionClusterLabel: x.ExecutionClusterLabel}
-	case *admin.MatchingAttributes_QualityOfService:
-		return &admin.Configuration{QualityOfService: x.QualityOfService}
-	case *admin.MatchingAttributes_PluginOverrides:
-		return &admin.Configuration{PluginOverrides: x.PluginOverrides}
-	case *admin.MatchingAttributes_WorkflowExecutionConfig:
-		return &admin.Configuration{WorkflowExecutionConfig: x.WorkflowExecutionConfig}
-	case *admin.MatchingAttributes_ClusterAssignment:
-		return &admin.Configuration{ClusterAssignment: x.ClusterAssignment}
+		return &admin.Configuration{TaskResourceAttributes: &admin.TaskResourceAttributes{}}
+	case admin.MatchableResource_CLUSTER_RESOURCE:
+		return &admin.Configuration{ClusterResourceAttributes: &admin.ClusterResourceAttributes{}}
+	case admin.MatchableResource_EXECUTION_QUEUE:
+		return &admin.Configuration{ExecutionQueueAttributes: &admin.ExecutionQueueAttributes{}}
+	case admin.MatchableResource_EXECUTION_CLUSTER_LABEL:
+		return &admin.Configuration{ExecutionClusterLabel: &admin.ExecutionClusterLabel{}}
+	case admin.MatchableResource_QUALITY_OF_SERVICE_SPECIFICATION:
+		return &admin.Configuration{QualityOfService: &core.QualityOfService{}}
+	case admin.MatchableResource_PLUGIN_OVERRIDE:
+		return &admin.Configuration{PluginOverrides: &admin.PluginOverrides{}}
+	case admin.MatchableResource_WORKFLOW_EXECUTION_CONFIG:
+		return &admin.Configuration{WorkflowExecutionConfig: &admin.WorkflowExecutionConfig{}}
+	case admin.MatchableResource_CLUSTER_ASSIGNMENT:
+		return &admin.Configuration{ClusterAssignment: &admin.ClusterAssignment{}}
 	default:
 		return &admin.Configuration{}
 	}
