9f56c44662c6b1fa0578e48bc8055647ec461629
Austin Liu <austin362667@gmail.com>
2024-04-11 19:13:39 +0800

    fmt
    Signed-off-by: Austin Liu <austin362667@gmail.com>

--- flyteidl/gen/pb_rust/lib.rs
+++ flyteidl/gen/pb_rust/lib.rs
@@ -3,25 +3,25 @@ pub mod datacatalog {
 }
 pub mod flyteidl {
 
-  pub mod admin {
-    include!("flyteidl.admin.rs");
-  }
-  pub mod cache {
-    include!("flyteidl.cacheservice.rs");
-  }
-  pub mod core {
-    include!("flyteidl.core.rs");
-  }
-  pub mod event {
-    include!("flyteidl.event.rs");
-  }
-  pub mod plugins {
-    include!("flyteidl.plugins.rs");
-    pub mod kubeflow{
-      include!("flyteidl.plugins.kubeflow.rs");
+    pub mod admin {
+        include!("flyteidl.admin.rs");
     }
-  }
-  pub mod service {
-    include!("flyteidl.service.rs");
-  }
+    pub mod cache {
+        include!("flyteidl.cacheservice.rs");
+    }
+    pub mod core {
+        include!("flyteidl.core.rs");
+    }
+    pub mod event {
+        include!("flyteidl.event.rs");
+    }
+    pub mod plugins {
+        include!("flyteidl.plugins.rs");
+        pub mod kubeflow {
+            include!("flyteidl.plugins.kubeflow.rs");
+        }
+    }
+    pub mod service {
+        include!("flyteidl.service.rs");
+    }
 }
