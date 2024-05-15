4bd87f992d711b38d0a2e748a87c8a92e690b5bb
davidmirror-ops <david.espejo@union.ai>
2024-05-15 17:40:21 -0500

    Fix mermaid take2
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

--- docs/conf.py
+++ docs/conf.py
@@ -278,7 +278,7 @@ intersphinx_mapping = {
 myst_enable_extensions = ["colon_fence"]
 
 # Sphinx-mermaid config
-mermaid_output_format = "svg"
+mermaid_output_format = "png"
 mermaid_version = "latest"
 mermaid_init_js = "mermaid.initialize({startOnLoad:false});"
 
