2f1f813de1c91c1fb2a82d58534aa2f5ed04f1de
Eduardo Apolinario <653394+eapolinario@users.noreply.github.com>
2024-05-17 10:14:46 -0700

    Skip flytectl upgrade tests (#5381)
    * Skip flytectl upgrade tests
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Fix lint errors for real this time
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Skip all tests using a TestMain
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytectl/cmd/upgrade/upgrade_test.go
+++ flytectl/cmd/upgrade/upgrade_test.go
@@ -1,6 +1,7 @@
 package upgrade
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 
@@ -174,4 +175,8 @@ func TestSelfUpgradeRollback(t *testing.T) {
 		assert.Nil(t, selfUpgrade(s.Ctx, args, s.CmdCtx))
 	})
 
+}
+
+func TestMain(_ *testing.M) {
+	fmt.Println("Skipping due to https://github.com/flyteorg/flyte/issues/5372")
 }
