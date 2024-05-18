b9aec1cf1f4963421590d95a5e5d481a9e1d874b
zychen5186 <brianchen5197@gmail.com>
2024-05-17 17:23:54 -0700

    simplify command_flag_map
    Signed-off-by: zychen5186 <brianchen5197@gmail.com>

--- flytectl/pkg/bubbletea/command_flag_map.go
+++ flytectl/pkg/bubbletea/command_flag_map.go
@@ -8,67 +8,67 @@ var commandFlagMap = make(map[string][]string)
 
 func InitCommandFlagMap() {
 	// compile
-	commandFlagMap[sliceToString([]string{"compile"})] = []string{"--file"}
+	commandFlagMap["compile"] = []string{"--file"}
 	// completion
-	commandFlagMap[sliceToString([]string{"completion"})] = []string{"--shell"}
+	commandFlagMap["completion"] = []string{"--shell"}
 	// config
-	commandFlagMap[sliceToString([]string{"config", "discover"})] = []string{""}
-	commandFlagMap[sliceToString([]string{"config", "docs"})] = []string{""}
-	commandFlagMap[sliceToString([]string{"config", "init"})] = []string{""}
-	commandFlagMap[sliceToString([]string{"config", "validate"})] = []string{""}
+	commandFlagMap["config|discover"] = []string{""}
+	commandFlagMap["config|docs"] = []string{""}
+	commandFlagMap["config|init"] = []string{""}
+	commandFlagMap["config|validate"] = []string{""}
 	// create
-	commandFlagMap[sliceToString([]string{"create", "execution"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"create", "project"})] = []string{"--pid"} //?
+	commandFlagMap["create|execution"] = []string{"-p", "-d"}
+	commandFlagMap["create|project"] = []string{"--pid"} //?
 	// delete
-	commandFlagMap[sliceToString([]string{"delete", "cluster-resource-attribute"})] = []string{""} //?
-	commandFlagMap[sliceToString([]string{"delete", "execution"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"delete", "execution-cluster-label"})] = []string{""}   //?
-	commandFlagMap[sliceToString([]string{"delete", "execution-queue-attribute"})] = []string{""} //?
-	commandFlagMap[sliceToString([]string{"delete", "plugin-override"})] = []string{""}           //?
-	commandFlagMap[sliceToString([]string{"delete", "task-resource-attribute"})] = []string{""}   //?
-	commandFlagMap[sliceToString([]string{"delete", "workflow-execution-config"})] = []string{""} //?
+	commandFlagMap["delete|cluster-resource-attribute"] = []string{""} //?
+	commandFlagMap["delete|execution"] = []string{"-p", "-d"}
+	commandFlagMap["delete|execution-cluster-label"] = []string{""}   //?
+	commandFlagMap["delete|execution-queue-attribute"] = []string{""} //?
+	commandFlagMap["delete|plugin-override"] = []string{""}           //?
+	commandFlagMap["delete|task-resource-attribute"] = []string{""}   //?
+	commandFlagMap["delete|workflow-execution-config"] = []string{""} //?
 	// demo
-	commandFlagMap[sliceToString([]string{"demo", "exec"})] = []string{""} //?
-	commandFlagMap[sliceToString([]string{"demo", "reload"})] = []string{""}
-	commandFlagMap[sliceToString([]string{"demo", "start"})] = []string{""}
-	commandFlagMap[sliceToString([]string{"demo", "status"})] = []string{""}
-	commandFlagMap[sliceToString([]string{"demo", "teardown"})] = []string{""}
+	commandFlagMap["demo|exec"] = []string{""} //?
+	commandFlagMap["demo|reload"] = []string{""}
+	commandFlagMap["demo|start"] = []string{""}
+	commandFlagMap["demo|status"] = []string{""}
+	commandFlagMap["demo|teardown"] = []string{""}
 	// get
-	commandFlagMap[sliceToString([]string{"get", "cluster-resource-attribute"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "execution"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "execution-cluster-label"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "launchplan"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "plugin-override"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "project"})] = []string{}
-	commandFlagMap[sliceToString([]string{"get", "task"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "task-resource-attribute"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "workflow"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"get", "workflow-execution-config"})] = []string{}
+	commandFlagMap["get|cluster-resource-attribute"] = []string{"-p", "-d"}
+	commandFlagMap["get|execution"] = []string{"-p", "-d"}
+	commandFlagMap["get|execution-cluster-label"] = []string{"-p", "-d"}
+	commandFlagMap["get|launchplan"] = []string{"-p", "-d"}
+	commandFlagMap["get|plugin-override"] = []string{"-p", "-d"}
+	commandFlagMap["get|project"] = []string{}
+	commandFlagMap["get|task"] = []string{"-p", "-d"}
+	commandFlagMap["get|task-resource-attribute"] = []string{"-p", "-d"}
+	commandFlagMap["get|workflow"] = []string{"-p", "-d"}
+	commandFlagMap["get|workflow-execution-config"] = []string{}
 	// register
-	commandFlagMap[sliceToString([]string{"register", "examples"})] = []string{}
-	commandFlagMap[sliceToString([]string{"register", "files"})] = []string{}
+	commandFlagMap["register|examples"] = []string{}
+	commandFlagMap["register|files"] = []string{}
 	// sandbox
-	commandFlagMap[sliceToString([]string{"sandbox", "exec"})] = []string{} //?
-	commandFlagMap[sliceToString([]string{"sandbox", "start"})] = []string{}
-	commandFlagMap[sliceToString([]string{"sandbox", "status"})] = []string{}
-	commandFlagMap[sliceToString([]string{"sandbox", "teardown"})] = []string{}
+	commandFlagMap["sandbox|exec"] = []string{} //?
+	commandFlagMap["sandbox|start"] = []string{}
+	commandFlagMap["sandbox|status"] = []string{}
+	commandFlagMap["sandbox|teardown"] = []string{}
 	// update
-	commandFlagMap[sliceToString([]string{"update", "cluster-resource-attribute"})] = []string{"--attrFile"} //?
-	commandFlagMap[sliceToString([]string{"update", "execution"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"update", "execution-cluster-label"})] = []string{"--attrFile"}   //?
-	commandFlagMap[sliceToString([]string{"update", "execution-queue-attribute"})] = []string{"--attrFile"} //?
-	commandFlagMap[sliceToString([]string{"update", "launchplan"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"update", "launchplan-meta"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"update", "plugin-override"})] = []string{"--attrFile"} //?
-	commandFlagMap[sliceToString([]string{"update", "project"})] = []string{"--pid"}
-	commandFlagMap[sliceToString([]string{"update", "task-meta"})] = []string{"-p", "-d"}
-	commandFlagMap[sliceToString([]string{"update", "task-resource-attribute"})] = []string{"--attrFile"}
-	commandFlagMap[sliceToString([]string{"update", "workflow-execution-config"})] = []string{"--attrFile"}
-	commandFlagMap[sliceToString([]string{"update", "workflow-meta"})] = []string{"-p", "-d"}
+	commandFlagMap["update|cluster-resource-attribute"] = []string{"--attrFile"} //?
+	commandFlagMap["update|execution"] = []string{"-p", "-d"}
+	commandFlagMap["update|execution-cluster-label"] = []string{"--attrFile"}   //?
+	commandFlagMap["update|execution-queue-attribute"] = []string{"--attrFile"} //?
+	commandFlagMap["update|launchplan"] = []string{"-p", "-d"}
+	commandFlagMap["update|launchplan-meta"] = []string{"-p", "-d"}
+	commandFlagMap["update|plugin-override"] = []string{"--attrFile"} //?
+	commandFlagMap["update|project"] = []string{"--pid"}
+	commandFlagMap["update|task-meta"] = []string{"-p", "-d"}
+	commandFlagMap["update|task-resource-attribute"] = []string{"--attrFile"}
+	commandFlagMap["update|workflow-execution-config"] = []string{"--attrFile"}
+	commandFlagMap["update|workflow-meta"] = []string{"-p", "-d"}
 	// upgrade
-	commandFlagMap[sliceToString([]string{"upgrade"})] = []string{}
+	commandFlagMap["upgrade"] = []string{}
 	// version
-	commandFlagMap[sliceToString([]string{"version"})] = []string{}
+	commandFlagMap["version"] = []string{}
 }
 
 // sliceToString converts a slice of strings to a string representation
