13551b40a62c07f1e5bea58cf0cb2c302fececc0
zychen5186 <brianchen5197@gmail.com>
2024-06-19 23:06:10 -0700

    regenerate flyteidl.d.ts
    Signed-off-by: zychen5186 <brianchen5197@gmail.com>

--- flyteidl/gen/pb-js/flyteidl.d.ts
+++ flyteidl/gen/pb-js/flyteidl.d.ts
@@ -5743,6 +5743,12 @@ export namespace flyteidl {
 
             /** TaskLog ttl */
             ttl?: (google.protobuf.IDuration|null);
+
+            /** TaskLog ShowWhilePending */
+            ShowWhilePending?: (boolean|null);
+
+            /** TaskLog HideOnceFinished */
+            HideOnceFinished?: (boolean|null);
         }
 
         /** Represents a TaskLog. */
@@ -5765,6 +5771,12 @@ export namespace flyteidl {
 
             /** TaskLog ttl. */
             public ttl?: (google.protobuf.IDuration|null);
+
+            /** TaskLog ShowWhilePending. */
+            public ShowWhilePending: boolean;
+
+            /** TaskLog HideOnceFinished. */
+            public HideOnceFinished: boolean;
 
             /**
              * Creates a new TaskLog instance using the specified properties.
