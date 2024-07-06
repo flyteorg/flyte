8dfe849ef619aec0d5c2ae7cbce1ea79d9463a59
Austin Liu <austin362667@gmail.com>
2024-07-06 10:28:51 +0800

    cleanup unused code in pyor_macro
    Signed-off-by: Austin Liu <austin362667@gmail.com>

--- flyteidl/pyo3_macro/src/lib.rs
+++ flyteidl/pyo3_macro/src/lib.rs
@@ -1,11 +1,7 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{
-    parse_macro_input, token::Brace, AngleBracketedGenericArguments, Data, DeriveInput, Fields,
-    FnArg, GenericArgument, ImplItem, ImplItemMethod, Item, ItemEnum, ItemImpl, ItemMod,
-    ItemStruct, PathArguments, ReturnType, Type, TypePath, Visibility,
-};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Type};
 
 #[proc_macro_derive(WithNew)]
 pub fn with_new(input: TokenStream) -> TokenStream {
@@ -71,9 +67,9 @@ pub fn with_new(input: TokenStream) -> TokenStream {
                 };
 
             if generic_params.is_empty() {
-                // Implement methods template of the `new()` function
+                // Implement methods template of the `new()` constructor function
                 quote! {
-
+                    // Macro main entrypoint, `#name` is the name of the underlying structure.
                     use pyo3::prelude::*;
                     #[pyo3::pymethods]
                     impl #name {
@@ -92,8 +88,7 @@ pub fn with_new(input: TokenStream) -> TokenStream {
                             }
                         }
 
-                        // use prost::Message;
-                        // use pyo3::types::PyBytes;
+                        // For the needs like `load_proto_from_file()`, `write_proto_to_file()` in `flytekit/core/utils.py`
                         pub fn ParseFromString(&mut self, bytes_string: &pyo3::types::PyBytes) -> Result<#name, crate::_flyteidl_rust::MessageDecodeError> {
                             let bt = bytes_string.as_bytes();
                             let de = prost::Message::decode(&bt.to_vec()[..]);
@@ -106,8 +101,6 @@ pub fn with_new(input: TokenStream) -> TokenStream {
                         //     Ok(buf)
                         // }
                     }
-
-                    // https://github.com/hyperium/tonic/blob/c7836521dd417434d625bd653fcf00fb7f7ae25e/tonic/src/request.rs#L28
                 }
             } else {
                 // https://pyo3.rs/v0.21.2/class#no-generic-parameters
@@ -115,54 +108,6 @@ pub fn with_new(input: TokenStream) -> TokenStream {
                 quote! {}
             }
         }
-        // Data::Struct(data) => {
-        //     // Extract the list of field names and types
-        //     let (field_names, field_types): (Vec<_>, Vec<_>) = match &data.fields {
-        //         Fields::Named(ref fields_named) => fields_named
-        //             .named
-        //             .iter()
-        //             .map(|f| (f.ident.clone(), &f.ty))
-        //             .unzip(),
-        //         _ => panic!("AutoPyClass can only be used with structs with named fields"),
-        //     };
-
-        //     let all_values = quote! { #(#field_names),*};
-
-        //     // Make all fields optional
-        //     let optional_field_names: Vec<_> = field_names.iter().collect();
-        //     let optional_field_types: Vec<_> = field_types.iter().map(|ty| quote! { #ty }).collect();
-
-        //     let optional_part = if !optional_field_names.is_empty() {
-        //         quote! { #(#optional_field_names: #optional_field_types),* }
-        //     } else {
-        //         quote! {}
-        //     };
-
-        //     let combined_signatures = if !optional_field_names.is_empty() {
-        //         quote! { #(#optional_field_names = None),* }
-        //     } else {
-        //         quote! {}
-        //     };
-
-        //     if generic_params.is_empty() {
-        //         // Implement methods template of the `new()` function
-        //         quote! {
-        //             #[::pyo3::pymethods]
-        //             impl #name {
-        //                 #[new]
-        //                 #[pyo3(signature = ( #combined_signatures ) )]
-        //                 pub fn new(#optional_part) -> Self {
-        //                     Self {
-        //                         #all_values
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     } else {
-        //         // TODO: Implement methods template of the `new()` function for generic structs
-        //         quote! {}
-        //     }
-        // }
         Data::Enum(_data) => {
             quote! {}
         }
