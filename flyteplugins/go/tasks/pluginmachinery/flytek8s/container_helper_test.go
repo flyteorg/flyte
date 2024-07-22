flyteorg/flyte

commit eb2410780edf5f525429f4d83d8bdd2397f08cc8

Add k8s env froms (#4969)
* Adding allowance for default configMap/secrets to pull env froms

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Updating Helm charts

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Fixing typos

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* More typo

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Ran 'make helm'

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Updating docs from `make helm`

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* `make helm` results

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Fixing issues with env vs envFrom

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Fixing merge issue

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Better naming for variable

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Renaming to be more accurate

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Fixing typo

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Forgot one change

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Fixing linting error

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

* Updating Helm charts

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

---------

Signed-off-by: Neil Stout <neil.j.stout@lmco.com>

flyteplugins/go/tasks/pluginmachinery/flytek8s/container_helper.go
@@ -291,7 +291,7 @@ func AddFlyteCustomizationsToContainer(ctx context.Context, parameters template.
 	}
 	container.Args = modifiedArgs
 
-	container.Env = DecorateEnvVars(ctx, container.Env, parameters.TaskExecMetadata.GetEnvironmentVariables(), parameters.TaskExecMetadata.GetTaskExecutionID())
+	container.Env, container.EnvFrom = DecorateEnvVars(ctx, container.Env, parameters.TaskExecMetadata.GetEnvironmentVariables(), parameters.TaskExecMetadata.GetTaskExecutionID())
 
 	// retrieve platformResources and overrideResources to use when aggregating container resources
 	platformResources := parameters.TaskExecMetadata.GetPlatformResources()

flyteplugins/go/tasks/pluginmachinery/flytek8s/k8s_resource_adds.go
@@ -113,7 +113,7 @@ func GetExecutionEnvVars(id pluginsCore.TaskExecutionID) []v1.EnvVar {
 	return envVars
 }
 
-func DecorateEnvVars(ctx context.Context, envVars []v1.EnvVar, taskEnvironmentVariables map[string]string, id pluginsCore.TaskExecutionID) []v1.EnvVar {
+func DecorateEnvVars(ctx context.Context, envVars []v1.EnvVar, taskEnvironmentVariables map[string]string, id pluginsCore.TaskExecutionID) ([]v1.EnvVar, []v1.EnvFromSource) {
 	envVars = append(envVars, GetContextEnvVars(ctx)...)
 	envVars = append(envVars, GetExecutionEnvVars(id)...)
 
@@ -128,7 +128,21 @@ func DecorateEnvVars(ctx context.Context, envVars []v1.EnvVar, taskEnvironmentVa
 		envVars = append(envVars, v1.EnvVar{Name: k, Value: value})
 	}
 
-	return envVars
+	envFroms := []v1.EnvFromSource{}
+
+	for _, secretName := range config.GetK8sPluginConfig().DefaultEnvFromSecrets {
+		optional := true
+		secretRef := v1.SecretEnvSource{LocalObjectReference: v1.LocalObjectReference{Name: secretName}, Optional: &optional}
+		envFroms = append(envFroms, v1.EnvFromSource{SecretRef: &secretRef})
+	}
+
+	for _, cmName := range config.GetK8sPluginConfig().DefaultEnvFromConfigMaps {
+		optional := true
+		cmRef := v1.ConfigMapEnvSource{LocalObjectReference: v1.LocalObjectReference{Name: cmName}, Optional: &optional}
+		envFroms = append(envFroms, v1.EnvFromSource{ConfigMapRef: &cmRef})
+	}
+
+	return envVars, envFroms
 }
 
 func GetPodTolerations(interruptible bool, resourceRequirements ...v1.ResourceRequirements) []v1.Toleration {

flyteplugins/go/tasks/plugins/array/awsbatch/transformer.go
@@ -136,7 +136,7 @@ func UpdateBatchInputForArray(_ context.Context, batchInput *batch.SubmitJobInpu
 
 func getEnvVarsForTask(ctx context.Context, execID pluginCore.TaskExecutionID, containerEnvVars []*core.KeyValuePair,
 	defaultEnvVars map[string]string) []v1.EnvVar {
-	envVars := flytek8s.DecorateEnvVars(ctx, flytek8s.ToK8sEnvVar(containerEnvVars), nil, execID)
+	envVars, _ := flytek8s.DecorateEnvVars(ctx, flytek8s.ToK8sEnvVar(containerEnvVars), nil, execID)
 	m := make(map[string]string, len(envVars))
 	for _, envVar := range envVars {
 		m[envVar.Name] = envVar.Value

flyteplugins/go/tasks/pluginmachinery/flytek8s/k8s_resource_adds_test.go
@@ -283,7 +283,7 @@ func TestDecorateEnvVars(t *testing.T) {
 				DefaultEnvVars:        tt.additionEnvVar,
 				DefaultEnvVarsFromEnv: tt.additionEnvVarFromEnv,
 			}))
-			if got := DecorateEnvVars(ctx, tt.args.envVars, tt.executionEnvVar, tt.args.id); !reflect.DeepEqual(got, tt.want) {
+			if got, _ := DecorateEnvVars(ctx, tt.args.envVars, tt.executionEnvVar, tt.args.id); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("DecorateEnvVars() = %v, want %v", got, tt.want)
 			}
 		})

charts/flyte/values.yaml
@@ -547,6 +547,8 @@ flyte:
             - FLYTE_AWS_ACCESS_KEY_ID: minio
             - FLYTE_AWS_SECRET_ACCESS_KEY: miniostorage
           default-memory: 200Mi
+          default-env-from-configmaps: []
+          default-env-from-secrets: []
 
     # -- Logger configuration
     logger:

deployment/sandbox/flyte_helm_generated.yaml
@@ -646,6 +646,8 @@ data:
     plugins:
       k8s:
         default-cpus: 100m
+        default-env-from-configmaps: []
+        default-env-from-secrets: []
         default-env-vars:
         - FLYTE_AWS_ENDPOINT: http://minio.flyte:9000
         - FLYTE_AWS_ACCESS_KEY_ID: minio
@@ -7170,7 +7172,7 @@ spec:
   template:
     metadata:
       annotations:
-        configChecksum: "ecefcb1d6df38b32a9ec77d91de63cd40da935a3239b70a5bb0396435878c80"
+        configChecksum: "3500386bee9f0763c6f14460f607cea4e6e24be4224a806c0673db71e13e287"
       labels: 
         app.kubernetes.io/name: flytepropeller
         app.kubernetes.io/instance: flyte
@@ -7244,7 +7246,7 @@ spec:
         app.kubernetes.io/name: flyte-pod-webhook
         app.kubernetes.io/version: v1.11.0
       annotations:
-        configChecksum: "ecefcb1d6df38b32a9ec77d91de63cd40da935a3239b70a5bb0396435878c80"
+        configChecksum: "3500386bee9f0763c6f14460f607cea4e6e24be4224a806c0673db71e13e287"
     spec:
       securityContext: 
         fsGroup: 65534

docker/sandbox-bundled/manifests/complete-agent.yaml
@@ -816,7 +816,7 @@ type: Opaque
 ---
 apiVersion: v1
 data:
-  haSharedSecret: VTg5RFBvY205cXRtcEVFbw==
+  haSharedSecret: U0tNSVV0aUwzZzYxNVZnVQ==
   proxyPassword: ""
   proxyUsername: ""
 kind: Secret
@@ -1412,7 +1412,7 @@ spec:
     metadata:
       annotations:
         checksum/config: 8f50e768255a87f078ba8b9879a0c174c3e045ffb46ac8723d2eedbe293c8d81
-        checksum/secret: b8339a7b42c1e04b994b517d4ddd749074787df4a0eaef9e1aa2181988aea2d7
+        checksum/secret: e0d4db2f1f52d2e4d16959b463e9470d657899ab22db5d8eb2fbc605cdcda0a2
       labels:
         app: docker-registry
         release: flyte-sandbox

docker/sandbox-bundled/manifests/complete.yaml
@@ -796,7 +796,7 @@ type: Opaque
 ---
 apiVersion: v1
 data:
-  haSharedSecret: ZTNEUmhqY1VsWDBXNFowTg==
+  haSharedSecret: bmNtdzJkQ0l2Rm5BS3VZSg==
   proxyPassword: ""
   proxyUsername: ""
 kind: Secret
@@ -1360,7 +1360,7 @@ spec:
     metadata:
       annotations:
         checksum/config: 8f50e768255a87f078ba8b9879a0c174c3e045ffb46ac8723d2eedbe293c8d81
-        checksum/secret: cedf489b22e27428631c5c365e58bfd51cf8465bf04d07a74462677278017b80
+        checksum/secret: b1ca10c95becb2e7214c9ece8101acc322ecde650f1e70bdb1bbb37323c948f6
       labels:
         app: docker-registry
         release: flyte-sandbox

commit acb730c3a172d78e49e63f996658ba368ba3e431

Cherry Pick keep EnvFrom from pod template (#5423) (#384)
* keep EnvFrom from pod template

not complete nor tested, just as hint for potential fix for regression in https://github.com/flyteorg/flyte/pull/4969



## Overview
*TODO: Provide a brief description of the feature or issue, and then summarize the changes in this PR. For larger changes make sure to callout high level design decisions. Reference relevant [design docs](https://unionai.atlassian.net/wiki/spaces/ENG/pages/78512174/Design+Review) or tracking issues.*

## Test Plan
1. flyte sandbox
2. dogfood gcp
3. build kite (running)

## Code and Screenshot
### Code
`example.py`
```python
import typing  # noqa: E402

from flytekit import Resources, task, workflow  # noqa: E402
from .other_task import count_unique_numbers
@task
def square_1(x: int) -> int:
    return x * x

@workflow
def wf(x: typing.List[int]) -> int:
    x=count_unique_numbers(x=x).with_overrides(requests=Resources(cpu="1111m", mem="1111Mi", ephemeral_storage="543Mi"),
                                                limits=Resources(cpu="1111m", mem="1111Mi", ephemeral_storage="543Mi"),
                                               )
    return square_1(x=x).with_overrides(requests=Resources(cpu="1111m", mem="1111Mi", ephemeral_storage="543Mi"),
                                                limits=Resources(cpu="1111m", mem="1111Mi", ephemeral_storage="543Mi"),
                                               )

if __name__ == "__main__":
    print(wf(x=[1, 1, 2, 3]))
```
`other_task.py`
```python
import typing  # noqa: E402

from flytekit import Resources, task, workflow  # noqa: E402

@task(
    requests=Resources(cpu="1000m", mem="1000Mi", ephemeral_storage="200Mi"),
)
def count_unique_numbers(x: typing.List[int]) -> int:
    s = set()
    for i in x:
        s.add(i)
    return len(s)
```
### Screenshot

flyteplugins/go/tasks/pluginmachinery/flytek8s/container_helper.go
@@ -297,7 +297,7 @@ func AddFlyteCustomizationsToContainer(ctx context.Context, parameters template.
 	if parameters.IncludeConsoleURL {
 		consoleURL = parameters.TaskExecMetadata.GetConsoleURL()
 	}
-	container.Env, container.EnvFrom = DecorateEnvVars(ctx, container.Env, parameters.TaskExecMetadata.GetEnvironmentVariables(), parameters.TaskExecMetadata.GetTaskExecutionID(), consoleURL)
+	container.Env, container.EnvFrom = DecorateEnvVars(ctx, container.Env, container.EnvFrom, parameters.TaskExecMetadata.GetEnvironmentVariables(), parameters.TaskExecMetadata.GetTaskExecutionID(), consoleURL)
 
 	// retrieve platformResources and overrideResources to use when aggregating container resources
 	platformResources := parameters.TaskExecMetadata.GetPlatformResources()

flyteplugins/go/tasks/pluginmachinery/flytek8s/k8s_resource_adds.go
@@ -127,7 +127,7 @@ func GetExecutionEnvVars(id pluginsCore.TaskExecutionID, consoleURL string) []v1
 	return envVars
 }
 
-func DecorateEnvVars(ctx context.Context, envVars []v1.EnvVar, taskEnvironmentVariables map[string]string, id pluginsCore.TaskExecutionID, consoleURL string) ([]v1.EnvVar, []v1.EnvFromSource) {
+func DecorateEnvVars(ctx context.Context, envVars []v1.EnvVar, envFroms []v1.EnvFromSource, taskEnvironmentVariables map[string]string, id pluginsCore.TaskExecutionID, consoleURL string) ([]v1.EnvVar, []v1.EnvFromSource) {
 	envVars = append(envVars, GetContextEnvVars(ctx)...)
 	envVars = append(envVars, GetExecutionEnvVars(id, consoleURL)...)
 
@@ -141,8 +141,6 @@ func DecorateEnvVars(ctx context.Context, envVars []v1.EnvVar, taskEnvironmentVa
 		value := os.Getenv(envVarName)
 		envVars = append(envVars, v1.EnvVar{Name: k, Value: value})
 	}
-
-	envFroms := []v1.EnvFromSource{}
 
 	for _, secretName := range config.GetK8sPluginConfig().DefaultEnvFromSecrets {
 		optional := true

flyteplugins/go/tasks/plugins/array/awsbatch/transformer.go
@@ -138,7 +138,7 @@ func UpdateBatchInputForArray(_ context.Context, batchInput *batch.SubmitJobInpu
 
 func getEnvVarsForTask(ctx context.Context, execID pluginCore.TaskExecutionID, containerEnvVars []*core.KeyValuePair,
 	defaultEnvVars map[string]string) []v1.EnvVar {
-	envVars, _ := flytek8s.DecorateEnvVars(ctx, flytek8s.ToK8sEnvVar(containerEnvVars), nil, execID, "")
+	envVars, _ := flytek8s.DecorateEnvVars(ctx, flytek8s.ToK8sEnvVar(containerEnvVars), nil, nil, execID, "")
 	m := make(map[string]string, len(envVars))
 	for _, envVar := range envVars {
 		m[envVar.Name] = envVar.Value

flyteplugins/go/tasks/pluginmachinery/flytek8s/container_helper_test.go
@@ -695,3 +695,40 @@ func TestAddFlyteCustomizationsToContainer_ValidateExistingResources(t *testing.
 	assert.True(t, container.Resources.Requests.Cpu().Equal(resource.MustParse("10")))
 	assert.True(t, container.Resources.Limits.Cpu().Equal(resource.MustParse("10")))
 }
+
+func TestAddFlyteCustomizationsToContainer_ValidateEnvFrom(t *testing.T) {
+	configMapSource := v1.EnvFromSource{
+		ConfigMapRef: &v1.ConfigMapEnvSource{
+			LocalObjectReference: v1.LocalObjectReference{
+				Name: "my-configmap",
+			},
+		},
+	}
+	secretSource := v1.EnvFromSource{
+		SecretRef: &v1.SecretEnvSource{
+			LocalObjectReference: v1.LocalObjectReference{
+				Name: "my-secret",
+			},
+		},
+	}
+
+	container := &v1.Container{
+		Command: []string{
+			"{{ .Input }}",
+		},
+		Args: []string{
+			"{{ .OutputPrefix }}",
+		},
+		EnvFrom: []v1.EnvFromSource{
+			configMapSource,
+			secretSource,
+		},
+	}
+
+	err := AddFlyteCustomizationsToContainer(context.TODO(), getTemplateParametersForTest(nil, nil, false, ""), ResourceCustomizationModeEnsureExistingResourcesInRange, container)
+	assert.NoError(t, err)
+
+	assert.Len(t, container.EnvFrom, 2)
+	assert.Equal(t, container.EnvFrom[0], configMapSource)
+	assert.Equal(t, container.EnvFrom[1], secretSource)
+}

flyteplugins/go/tasks/pluginmachinery/flytek8s/k8s_resource_adds_test.go
@@ -330,7 +330,7 @@ func TestDecorateEnvVars(t *testing.T) {
 				DefaultEnvVars:        tt.additionEnvVar,
 				DefaultEnvVarsFromEnv: tt.additionEnvVarFromEnv,
 			}))
-			if got, _ := DecorateEnvVars(ctx, tt.args.envVars, tt.executionEnvVar, tt.args.id, tt.consoleURL); !reflect.DeepEqual(got, tt.want) {
+			if got, _ := DecorateEnvVars(ctx, tt.args.envVars, nil, tt.executionEnvVar, tt.args.id, tt.consoleURL); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("DecorateEnvVars() = %v, want %v", got, tt.want)
 			}
 		})