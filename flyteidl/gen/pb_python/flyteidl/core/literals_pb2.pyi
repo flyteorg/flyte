3d387295a950791d5cca2f36e2e0d9a45e81c1c6
Katrina Rogan <katroganGH@gmail.com>
2024-08-14 20:27:30 +0200

    Add offloaded path to literal (#5660)
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- flyteidl/gen/pb_python/flyteidl/core/literals_pb2.pyi
+++ flyteidl/gen/pb_python/flyteidl/core/literals_pb2.pyi
@@ -104,7 +104,7 @@ class Scalar(_message.Message):
     def __init__(self, primitive: _Optional[_Union[Primitive, _Mapping]] = ..., blob: _Optional[_Union[Blob, _Mapping]] = ..., binary: _Optional[_Union[Binary, _Mapping]] = ..., schema: _Optional[_Union[Schema, _Mapping]] = ..., none_type: _Optional[_Union[Void, _Mapping]] = ..., error: _Optional[_Union[_types_pb2.Error, _Mapping]] = ..., generic: _Optional[_Union[_struct_pb2.Struct, _Mapping]] = ..., structured_dataset: _Optional[_Union[StructuredDataset, _Mapping]] = ..., union: _Optional[_Union[Union, _Mapping]] = ...) -> None: ...
 
 class Literal(_message.Message):
-    __slots__ = ["scalar", "collection", "map", "hash", "metadata"]
+    __slots__ = ["scalar", "collection", "map", "hash", "metadata", "uri", "size_bytes"]
     class MetadataEntry(_message.Message):
         __slots__ = ["key", "value"]
         KEY_FIELD_NUMBER: _ClassVar[int]
@@ -117,12 +117,16 @@ class Literal(_message.Message):
     MAP_FIELD_NUMBER: _ClassVar[int]
     HASH_FIELD_NUMBER: _ClassVar[int]
     METADATA_FIELD_NUMBER: _ClassVar[int]
+    URI_FIELD_NUMBER: _ClassVar[int]
+    SIZE_BYTES_FIELD_NUMBER: _ClassVar[int]
     scalar: Scalar
     collection: LiteralCollection
     map: LiteralMap
     hash: str
     metadata: _containers.ScalarMap[str, str]
-    def __init__(self, scalar: _Optional[_Union[Scalar, _Mapping]] = ..., collection: _Optional[_Union[LiteralCollection, _Mapping]] = ..., map: _Optional[_Union[LiteralMap, _Mapping]] = ..., hash: _Optional[str] = ..., metadata: _Optional[_Mapping[str, str]] = ...) -> None: ...
+    uri: str
+    size_bytes: int
+    def __init__(self, scalar: _Optional[_Union[Scalar, _Mapping]] = ..., collection: _Optional[_Union[LiteralCollection, _Mapping]] = ..., map: _Optional[_Union[LiteralMap, _Mapping]] = ..., hash: _Optional[str] = ..., metadata: _Optional[_Mapping[str, str]] = ..., uri: _Optional[str] = ..., size_bytes: _Optional[int] = ...) -> None: ...
 
 class LiteralCollection(_message.Message):
     __slots__ = ["literals"]
