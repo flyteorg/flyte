d352a2ed82c7be2afc6f63c3c4b5e6ada2e54549
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-08-23 14:42:53 -0700

    reset client builder test
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flyteidl/clients/go/admin/client_builder_test.go
+++ flyteidl/clients/go/admin/client_builder_test.go
@@ -2,15 +2,9 @@ package admin
 
 import (
 	"context"
-	"fmt"
 	"net/url"
 	"reflect"
 	"testing"
-
-	adminMocks "github.com/flyteorg/flyte/flyteidl/clients/go/admin/mocks"
-	"github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/service"
-	"github.com/stretchr/testify/mock"
-	"k8s.io/apimachinery/pkg/util/rand"
 
 	"github.com/stretchr/testify/assert"
 
@@ -19,30 +13,13 @@ import (
 )
 
 func TestClientsetBuilder_Build(t *testing.T) {
-	httpPort := rand.IntnRange(10000, 60000)
-	grpcPort := rand.IntnRange(10000, 60000)
-	m := &adminMocks.AuthMetadataServiceServer{}
-	m.OnGetOAuth2MetadataMatch(mock.Anything, mock.Anything).Return(&service.OAuth2MetadataResponse{
-		AuthorizationEndpoint: fmt.Sprintf("http://localhost:%d/oauth2/authorize", httpPort),
-		TokenEndpoint:         fmt.Sprintf("http://localhost:%d/oauth2/token", httpPort),
-		JwksUri:               fmt.Sprintf("http://localhost:%d/oauth2/jwks", httpPort),
-	}, nil)
-	m.OnGetPublicClientConfigMatch(mock.Anything, mock.Anything).Return(&service.PublicClientAuthConfigResponse{
-		Scopes: []string{"all"},
-	}, nil)
-	u, err := url.Parse(fmt.Sprintf("dns:///localhost:%d", grpcPort))
-	assert.NoError(t, err)
-
-	s := newAuthMetadataServer(t, grpcPort, httpPort, m)
-	ctx := context.Background()
-	assert.NoError(t, s.Start(ctx))
-	defer s.Close()
-
+	u, _ := url.Parse("localhost:8089")
 	cb := NewClientsetBuilder().WithConfig(&Config{
 		UseInsecureConnection: true,
 		Endpoint:              config.URL{URL: *u},
 	}).WithTokenCache(cache.NewTokenCacheInMemoryProvider())
-	_, err = cb.Build(ctx)
+	ctx := context.Background()
+	_, err := cb.Build(ctx)
 	assert.NoError(t, err)
 	assert.True(t, reflect.TypeOf(cb.tokenCache) == reflect.TypeOf(cache.NewTokenCacheInMemoryProvider()))
 }
