9646272a9c35288953e8b03e8ce8bdda6378e156
wayner0628 <a901639@gmail.com>
2024-08-26 13:39:22 +0800

    change configuration name
    Signed-off-by: wayner0628 <a901639@gmail.com>

--- flyteadmin/pkg/runtime/interfaces/application_configuration.go
+++ flyteadmin/pkg/runtime/interfaces/application_configuration.go
@@ -50,7 +50,7 @@ type PostgresConfig struct {
 
 type FeatureGates struct {
 	EnableArtifacts bool `json:"enableArtifacts" pflag:",Enable artifacts feature."`
-	EnableHumanHash bool `json:"enableHumanHash" pflag:",Enable humanhash feature."`
+	EnableFriendlyNames bool `json:"enableFriendlyNames" pflag:",Enable generation of friendly execution names feature."`
 }
 
 // ApplicationConfig is the base configuration to start admin
