92f7b9cebabdb898bc94c0df04207c05e738402c
Kevin Su <pingsutw@apache.org>
2024-08-28 13:34:03 -0700

    nit
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flyteadmin/pkg/common/naming/execution_name.go
+++ flyteadmin/pkg/common/naming/execution_name.go
@@ -22,7 +22,8 @@ func GetExecutionName(seed int64) string {
 	config := configProvider.GetTopLevelConfig()
 	if config.FeatureGates.EnableFriendlyNames {
 		hashKey := []byte(rand.String(ExecutionIDLength))
-		result, _ := humanhash.Humanize(hashKey, 4) // Ignoring the error as it's guaranteed hash key longer than result in this context
+		// Ignoring the error as it's guaranteed hash key longer than result in this context.
+		result, _ := humanhash.Humanize(hashKey, 4)
 		return result
 	}
 	return fmt.Sprintf(ExecutionStringFormat, rand.String(ExecutionIDLength-1))
