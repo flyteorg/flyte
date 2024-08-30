e397586c3097a8b96211c3a11a3be116e34ca1fb
Thomas J. Fan <thomasjpfan@gmail.com>
2024-08-30 15:21:30 -0400

    Adds functional tests for actors and CI (#440)
    ## Overview
    Adds functional tests for actors and runs it on Github Actions. Overall this:
    
    1. Builds a flyte-sandbox-bundled
    2. Builds a runtime image with union instead & any rust updates in this repo
    3. Starts flyte-sandbox-bundled 
    4. Runs an actor execution on the sandbox with pytest.
    
    Here is a CI run: https://github.com/unionai/flyte/actions/runs/10512448826?pr=440
    
    ## Upstream Changes
    Should this change be upstreamed to OSS (flyteorg/flyte)? If not, please uncheck this box, which is used for auditing. Note, it is the responsibility of each developer to actually upstream their changes. See [this guide](https://unionai.atlassian.net/wiki/spaces/ENG/pages/447610883/Flyte+-+Union+Cloud+Development+Runbook/#When-are-versions-updated%3F).
    - [ ] To be upstreamed to OSS
    
    ## Issue
    Start of https://linear.app/unionai/issue/COR-1457/fasttask-functional-tests

---
+++ fasttask/worker/functional-tests/conftest.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+from flytekit.remote import FlyteRemote
+from flytekit.configuration import Config
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def remote() -> FlyteRemote:
+    return FlyteRemote(
+        Config.for_sandbox(),
+        default_project="flytesnacks",
+        default_domain="development",
+    )
+
+
+@pytest.fixture(scope="session")
+def workflows_dir() -> Path:
+    return Path(__file__).parent / "workflows"
+
+
+@pytest.fixture(scope="session")
+def config_path() -> str:
+    sandbox_path = Path(__file__).parent / "sandbox-yaml.yaml"
+    return os.fspath(sandbox_path.resolve())
+
+
+@pytest.fixture(scope="session")
+def union_image() -> str:
+    image = os.getenv("UNION_RUNTIME_TEST_IMAGE")
+    if image is None:
+        pytest.fail("Please set UNION_RUNTIME_TEST_IMAGE")
+    return image
---
+++ fasttask/worker/functional-tests/workflows/simple_actor.py
@@ -0,0 +1,22 @@
+from flytekit import workflow
+
+from union.actor import ActorEnvironment
+import os
+
+actor = ActorEnvironment(
+    name="simple-actor",
+    parallelism=1,
+    replica_count=1,
+    ttl_seconds=100,
+    container_image=os.getenv("UNION_RUNTIME_TEST_IMAGE"),
+)
+
+
+@actor.task
+def add_one(num: int) -> int:
+    return num + 1
+
+
+@workflow
+def add_five(num: int) -> int:
+    return add_one(num=add_one(num=add_one(num=add_one(num=add_one(num=num)))))
