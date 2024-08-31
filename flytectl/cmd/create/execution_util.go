466df3fcd38bafd9c13ccd883b3ac468912b985a
wayner0628 <a901639@gmail.com>
2024-09-01 00:41:58 +0800

    revert flytectl
    Signed-off-by: wayner0628 <a901639@gmail.com>

--- flytectl/cmd/create/execution_util.go
+++ flytectl/cmd/create/execution_util.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	cmdCore "github.com/flyteorg/flyte/flytectl/cmd/core"
 	cmdGet "github.com/flyteorg/flyte/flytectl/cmd/get"
 	"github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/admin"
 	"github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/core"
 	"github.com/flyteorg/flyte/flytestdlib/logger"
+	"github.com/google/uuid"
 	"sigs.k8s.io/yaml"
 )
 
@@ -147,6 +149,9 @@ func recoverExecution(ctx context.Context, executionName string, project string,
 
 func createExecutionRequest(ID *core.Identifier, inputs *core.LiteralMap, envs *admin.Envs, securityContext *core.SecurityContext, authRole *admin.AuthRole, targetExecName string, targetExecutionCluster string) *admin.ExecutionCreateRequest {
 
+	if len(targetExecName) == 0 {
+		targetExecName = "f" + strings.ReplaceAll(uuid.New().String(), "-", "")[:19]
+	}
 	var clusterAssignment *admin.ClusterAssignment
 	if executionConfig.ClusterPool != "" {
 		clusterAssignment = &admin.ClusterAssignment{ClusterPoolName: executionConfig.ClusterPool}
