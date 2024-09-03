4eb88ac4eb925b1f2f312b70711cec07d7d8d112
Future-Outlier <eric901201@gmail.com>
2024-09-03 11:25:12 +0800

    Add Logging for Agent-Supported Task Types
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flyteplugins/go/tasks/plugins/webapi/agent/client.go
+++ flyteplugins/go/tasks/plugins/webapi/agent/client.go
@@ -3,6 +3,7 @@ package agent
 import (
 	"context"
 	"crypto/x509"
+	"strings"
 
 	"golang.org/x/exp/maps"
 	"google.golang.org/grpc"
@@ -127,8 +128,10 @@ func getAgentRegistry(ctx context.Context, cs *ClientSet) Registry {
 			continue
 		}
 
+		var agentSupportedTaskCategories []string
 		for _, agent := range res.GetAgents() {
 			deprecatedSupportedTaskTypes := agent.SupportedTaskTypes
+			agentSupportedTaskCategories = append(agentSupportedTaskCategories, deprecatedSupportedTaskTypes...)
 			for _, supportedTaskType := range deprecatedSupportedTaskTypes {
 				agent := &Agent{AgentDeployment: agentDeployment, IsSync: agent.IsSync}
 				newAgentRegistry[supportedTaskType] = map[int32]*Agent{defaultTaskTypeVersion: agent}
@@ -137,9 +140,13 @@ func getAgentRegistry(ctx context.Context, cs *ClientSet) Registry {
 			supportedTaskCategories := agent.SupportedTaskCategories
 			for _, supportedCategory := range supportedTaskCategories {
 				agent := &Agent{AgentDeployment: agentDeployment, IsSync: agent.IsSync}
-				newAgentRegistry[supportedCategory.GetName()] = map[int32]*Agent{supportedCategory.GetVersion(): agent}
+				supportedCategoryName := supportedCategory.GetName()
+				newAgentRegistry[supportedCategoryName] = map[int32]*Agent{supportedCategory.GetVersion(): agent}
+				agentSupportedTaskCategories = append(agentSupportedTaskCategories, supportedCategoryName)
 			}
+
 		}
+		logger.Infof(ctx, "AgentDeployment [%v] supports the following task types: [%v]", agentDeployment.Endpoint, strings.Join(agentSupportedTaskCategories, ", "))
 	}
 
 	// If the agent doesn't implement the metadata service, we construct the registry based on the configuration
@@ -160,6 +167,7 @@ func getAgentRegistry(ctx context.Context, cs *ClientSet) Registry {
 		}
 	}
 
+	logger.Infof(ctx, "AgentDeployments support the following task types: [%v]", strings.Join(maps.Keys(newAgentRegistry), ", "))
 	return newAgentRegistry
 }
 
