f1770d16d0e526456f1acb06c995be92cd6efa88
nikki everett <nikki@union.ai>
2024-09-10 12:46:28 -0500

    remove custom CSS and JS, since this mostly duplicates or interferes with theme CSS/JS and docsearch CSS/JS
    Signed-off-by: nikki everett <nikki@union.ai>

--- docs/conf.py
+++ docs/conf.py
@@ -196,8 +196,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["custom.css", "flyte.css"]
-html_js_files = ["custom.js"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
