5dfacf1a54c71aad4fbffdcbcf8a0ea4fd273c10
pmahindrakar-oss <prafulla.mahindrakar@gmail.com>
2024-09-09 21:58:10 -0700

    feedback
    Signed-off-by: pmahindrakar-oss <prafulla.mahindrakar@gmail.com>

--- flytepropeller/pkg/controller/nodes/common/utils.go
+++ flytepropeller/pkg/controller/nodes/common/utils.go
@@ -148,7 +148,7 @@ func CheckOffloadingCompat(ctx context.Context, nCtx interfaces.NodeExecutionCon
 		return &phaseInfo
 	}
 	runtimeData := taskNode.CoreTask().GetMetadata().GetRuntime()
-	if consumesOffloadLiteral && !literalOffloadingConfig.IsSupportedSDKVersion(runtimeData.GetType().String(), runtimeData.GetVersion()) {
+	if !literalOffloadingConfig.IsSupportedSDKVersion(runtimeData.GetType().String(), runtimeData.GetVersion()) {
 		if !literalOffloadingConfig.Enabled {
 			errMsg := fmt.Sprintf("task [%s] is trying to consume offloaded literals but feature is not enabled", taskID)
 			logger.Errorf(ctx, errMsg)
