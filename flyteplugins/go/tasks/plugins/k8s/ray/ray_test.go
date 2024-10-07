e41e24534f337230e4864ec4df78b255d17eee62
Amin Maghsodi <amin.m@aut.ac.ir>
2024-10-07 09:56:27 +0330

    Update ray_test.go
    Signed-off-by: Amin Maghsodi <amin.m@aut.ac.ir>

--- flyteplugins/go/tasks/plugins/k8s/ray/ray_test.go
+++ flyteplugins/go/tasks/plugins/k8s/ray/ray_test.go
@@ -755,7 +755,7 @@ func TestGetTaskPhase(t *testing.T) {
 		{rayv1.JobDeploymentStatusRunning, pluginsCore.PhaseRunning, false},
 		{rayv1.JobDeploymentStatusComplete, pluginsCore.PhaseSuccess, false},
 		{rayv1.JobDeploymentStatusFailed, pluginsCore.PhasePermanentFailure, false},
-		{rayv1.JobDeploymentStatusSuspended, pluginsCore.PhaseUndefined, true},
+		{rayv1.JobDeploymentStatusSuspended, pluginsCore.PhaseQueued, true},
 	}
 
 	for _, tc := range testCases {
