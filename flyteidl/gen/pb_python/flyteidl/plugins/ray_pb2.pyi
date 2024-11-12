3c3ae05571411b38f58384904d82b6f50c9c8549
Jason Parraga <Sovietaced@gmail.com>
2024-11-12 09:03:54 -0800

    Decouple ray submitter, worker, and head resources (#5933)
    Signed-off-by: Jason Parraga <sovietaced@gmail.com>

--- flyteidl/gen/pb_python/flyteidl/plugins/ray_pb2.pyi
+++ flyteidl/gen/pb_python/flyteidl/plugins/ray_pb2.pyi
@@ -1,3 +1,4 @@
+from flyteidl.core import tasks_pb2 as _tasks_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
@@ -30,7 +31,7 @@ class RayCluster(_message.Message):
     def __init__(self, head_group_spec: _Optional[_Union[HeadGroupSpec, _Mapping]] = ..., worker_group_spec: _Optional[_Iterable[_Union[WorkerGroupSpec, _Mapping]]] = ..., enable_autoscaling: bool = ...) -> None: ...
 
 class HeadGroupSpec(_message.Message):
-    __slots__ = ["ray_start_params"]
+    __slots__ = ["ray_start_params", "k8s_pod"]
     class RayStartParamsEntry(_message.Message):
         __slots__ = ["key", "value"]
         KEY_FIELD_NUMBER: _ClassVar[int]
@@ -39,11 +40,13 @@ class HeadGroupSpec(_message.Message):
         value: str
         def __init__(self, key: _Optional[str] = ..., value: _Optional[str] = ...) -> None: ...
     RAY_START_PARAMS_FIELD_NUMBER: _ClassVar[int]
+    K8S_POD_FIELD_NUMBER: _ClassVar[int]
     ray_start_params: _containers.ScalarMap[str, str]
-    def __init__(self, ray_start_params: _Optional[_Mapping[str, str]] = ...) -> None: ...
+    k8s_pod: _tasks_pb2.K8sPod
+    def __init__(self, ray_start_params: _Optional[_Mapping[str, str]] = ..., k8s_pod: _Optional[_Union[_tasks_pb2.K8sPod, _Mapping]] = ...) -> None: ...
 
 class WorkerGroupSpec(_message.Message):
-    __slots__ = ["group_name", "replicas", "min_replicas", "max_replicas", "ray_start_params"]
+    __slots__ = ["group_name", "replicas", "min_replicas", "max_replicas", "ray_start_params", "k8s_pod"]
     class RayStartParamsEntry(_message.Message):
         __slots__ = ["key", "value"]
         KEY_FIELD_NUMBER: _ClassVar[int]
@@ -56,9 +59,11 @@ class WorkerGroupSpec(_message.Message):
     MIN_REPLICAS_FIELD_NUMBER: _ClassVar[int]
     MAX_REPLICAS_FIELD_NUMBER: _ClassVar[int]
     RAY_START_PARAMS_FIELD_NUMBER: _ClassVar[int]
+    K8S_POD_FIELD_NUMBER: _ClassVar[int]
     group_name: str
     replicas: int
     min_replicas: int
     max_replicas: int
     ray_start_params: _containers.ScalarMap[str, str]
-    def __init__(self, group_name: _Optional[str] = ..., replicas: _Optional[int] = ..., min_replicas: _Optional[int] = ..., max_replicas: _Optional[int] = ..., ray_start_params: _Optional[_Mapping[str, str]] = ...) -> None: ...
+    k8s_pod: _tasks_pb2.K8sPod
+    def __init__(self, group_name: _Optional[str] = ..., replicas: _Optional[int] = ..., min_replicas: _Optional[int] = ..., max_replicas: _Optional[int] = ..., ray_start_params: _Optional[_Mapping[str, str]] = ..., k8s_pod: _Optional[_Union[_tasks_pb2.K8sPod, _Mapping]] = ...) -> None: ...
