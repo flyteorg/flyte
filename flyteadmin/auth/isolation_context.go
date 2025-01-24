4c470d7ff4a787fc49ffe11ff03ace4dda0686c1
Jason Parraga <sovietaced@gmail.com>
2025-01-24 00:20:23 -0800

    more documentation

--- flyteadmin/auth/isolation_context.go
+++ flyteadmin/auth/isolation_context.go
@@ -10,6 +10,7 @@ const (
 	ContextKeyIsolationContext = contextutils.Key("isolation_context")
 )
 
+// An IsolationContext provides context around how to isolate or filter resources for a particular API request.
 type IsolationContext struct {
 	resourceScopes []ResourceScope
 }
@@ -18,6 +19,8 @@ func NewIsolationContext(resourceScopes []ResourceScope) IsolationContext {
 	return IsolationContext{resourceScopes: resourceScopes}
 }
 
+// GetResourceScopes gets the scope of resources that the client can access. These are typically Flyte projects as well
+// as domains within projects.
 func (c IsolationContext) GetResourceScopes() []ResourceScope {
 	return c.resourceScopes
 }
@@ -29,20 +32,22 @@ type ResourceScope struct {
 	Domain  string
 }
 
-// TargetResourceScopeDepth represents the depth/scope of an individual resource. Sometimes only project level or domain level
-// scope is applicable to a resource. In such cases, a user's resource scope may need to be truncated to match the depth
-// of the target resource scope.
+// TargetResourceScopeDepth represents the depth/scope of an individual resource. Sometimes only project level scope is
+// applicable to a resource while the user has domain level scope within a project. In such cases, a user's resource
+// scope may need to be truncated to match the depth of the target resource scope during filtering operations.
 type TargetResourceScopeDepth = int
 
 const (
-	ProjectTargetResourceScopeDepth = 0
-	DomainTargetResourceScopeDepth  = 1
+	ProjectTargetResourceScopeDepth = 0 // The resource can only be filtered at a project level
+	DomainTargetResourceScopeDepth  = 1 // THe resource can be filtered at both a project and domain level
 )
 
+// WithContext adds the isolation context to the go context
 func (c IsolationContext) WithContext(ctx context.Context) context.Context {
 	return context.WithValue(ctx, ContextKeyIsolationContext, c)
 }
 
+// IsolationContextFromContext extracts the isolation context from a go context
 func IsolationContextFromContext(ctx context.Context) IsolationContext {
 	existing := ctx.Value(ContextKeyIsolationContext)
 	if existing != nil {
