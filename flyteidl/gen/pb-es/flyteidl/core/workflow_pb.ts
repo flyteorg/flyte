5ec1a6051af86bee15e7c38b727bfda3a4c6a198
arbaobao <asd3431090@gmail.com>
2025-02-04 02:29:10 +0800

    Support overriding task pod_template via with_overrides (#6118)
    Signed-off-by: Nelson Chen <asd3431090@gmail.com>
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    Co-authored-by: Kevin Su <pingsutw@apache.org>

--- flyteidl/gen/pb-es/flyteidl/core/workflow_pb.ts
+++ flyteidl/gen/pb-es/flyteidl/core/workflow_pb.ts
@@ -11,7 +11,7 @@ import { Identifier } from "./identifier_pb.js";
 import { Binding, LiteralMap, RetryStrategy } from "./literals_pb.js";
 import { QualityOfService } from "./execution_pb.js";
 import { TypedInterface } from "./interface_pb.js";
-import { ExtendedResources, Resources } from "./tasks_pb.js";
+import { ExtendedResources, K8sPod, Resources } from "./tasks_pb.js";
 
 /**
  * Defines a condition and the execution unit that should be executed if the condition is satisfied.
@@ -1222,6 +1222,14 @@ export class TaskNodeOverrides extends Message<TaskNodeOverrides> {
    */
   containerImage = "";
 
+  /**
+   * Override for the pod template used by task pods
+   * +optional
+   *
+   * @generated from field: flyteidl.core.K8sPod pod_template = 4;
+   */
+  podTemplate?: K8sPod;
+
   constructor(data?: PartialMessage<TaskNodeOverrides>) {
     super();
     proto3.util.initPartial(data, this);
@@ -1233,6 +1241,7 @@ export class TaskNodeOverrides extends Message<TaskNodeOverrides> {
     { no: 1, name: "resources", kind: "message", T: Resources },
     { no: 2, name: "extended_resources", kind: "message", T: ExtendedResources },
     { no: 3, name: "container_image", kind: "scalar", T: 9 /* ScalarType.STRING */ },
+    { no: 4, name: "pod_template", kind: "message", T: K8sPod },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): TaskNodeOverrides {
