f23ed538fe2f6530718d84f3950e572d4b4246b4
machichima <nary12321@gmail.com>
2025-03-01 16:09:00 +0800

    feat: enable passing multiple attrFile flag
    Signed-off-by: machichima <nary12321@gmail.com>

--- flytectl/cmd/config/subcommand/workflowexecutionconfig/update_config.go
+++ flytectl/cmd/config/subcommand/workflowexecutionconfig/update_config.go
@@ -4,9 +4,9 @@ package workflowexecutionconfig
 
 // AttrUpdateConfig Matchable resource attributes configuration passed from command line
 type AttrUpdateConfig struct {
-	AttrFile string `json:"attrFile" pflag:",attribute file name to be used for updating attribute for the resource type."`
-	DryRun   bool   `json:"dryRun" pflag:",execute command without making any modifications."`
-	Force    bool   `json:"force" pflag:",do not ask for an acknowledgement during updates."`
+	AttrFile []string `json:"attrFile" pflag:",attribute file names to be used for updating attribute for the resource type."`
+	DryRun   bool     `json:"dryRun" pflag:",execute command without making any modifications."`
+	Force    bool     `json:"force" pflag:",do not ask for an acknowledgement during updates."`
 }
 
 var DefaultUpdateConfig = &AttrUpdateConfig{}
