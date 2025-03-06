c8cfa213212dc6b8a31891bfcd6f73b02fd327d9
Kevin Su <pingsutw@apache.org>
2025-03-06 15:25:39 -0800

    lint
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flyteplugins/go/tasks/plugins/webapi/bigquery/config_flags_test.go
+++ flyteplugins/go/tasks/plugins/webapi/bigquery/config_flags_test.go
@@ -133,7 +133,7 @@ func TestConfig_SetFlags(t *testing.T) {
 			testValue := "1"
 
 			cmdFlags.Set("webApi.writeRateLimiter.qps", testValue)
-			if vInt, err := cmdFlags.GetInt("webApi.writeRateLimiter.qps"); err == nil {
+			if vInt, err := cmdFlags.GetFloat64("webApi.writeRateLimiter.qps"); err == nil {
 				testDecodeJson_Config(t, fmt.Sprintf("%v", vInt), &actual.WebAPI.WriteRateLimiter.QPS)
 
 			} else {
