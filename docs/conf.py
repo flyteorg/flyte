ac21e672e521b772c6da7221022611cb35e004db
Kevin Su <pingsutw@apache.org>
2025-06-04 15:11:43 -0700

    update versioon
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ author = "Flyte"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "1.15.3"
+release = "1.16.0b0"
 
 # -- General configuration ---------------------------------------------------
 
