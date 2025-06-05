c710fb60db62793510a60d03ffa15a671267ae89
Kevin Su <pingsutw@apache.org>
2025-06-05 15:06:52 -0700

    Update github workflow description (#6494)
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ author = "Flyte"
 
 # The short X.Y version
 version = ""
-# The full version, including alpha/beta/rc tags
+# The full version, including alpha/beta/rc tags. For example, 0.1.1, 1.16.0-b0
 release = "1.16.0-b2"
 
 # -- General configuration ---------------------------------------------------
