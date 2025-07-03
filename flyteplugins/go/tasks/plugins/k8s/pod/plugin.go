7850b884dda6a4d8f0a0180ddeef6645eb158742
Kevin Su <pingsutw@apache.org>
2025-07-03 16:51:45 -0700

    refactor: Adjust CoPilot init and Pod status logic
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flyteplugins/go/tasks/plugins/k8s/pod/plugin.go
+++ flyteplugins/go/tasks/plugins/k8s/pod/plugin.go
@@ -122,7 +122,7 @@ func (p plugin) BuildResource(ctx context.Context, taskCtx pluginsCore.TaskExecu
 	// set primaryContainerKey annotation if this is a Sidecar task or, as an optimization, if there is only a single
 	// container. this plugin marks the task complete if the primary Container is complete, so if there is only one
 	// container we can mark the task as complete before the Pod has been marked complete.
-	if taskTemplate.GetType() == SidecarTaskType || len(podSpec.Containers) == 1 {
+	if taskTemplate.GetType() == SidecarTaskType || (len(podSpec.Containers) == 1 && taskTemplate.GetType() != rawContainerTaskType) {
 		objectMeta.Annotations[flytek8s.PrimaryContainerKey] = primaryContainerName
 	}
 
@@ -208,10 +208,18 @@ func DemystifyPodStatus(ctx context.Context, pod *v1.Pod, info pluginsCore.TaskI
 		// DO NOTHING
 	default:
 		if !primaryContainerExists {
-			// if all of the containers in the Pod are complete, as an optimization, we can declare the task as
+			// if all of the containers and sidecars in the Pod are complete, as an optimization, we can declare the task as
 			// succeeded rather than waiting for the Pod to be marked completed.
 			allSuccessfullyTerminated := len(pod.Status.ContainerStatuses) > 0
 			for _, s := range pod.Status.ContainerStatuses {
+				if s.State.Waiting != nil || s.State.Running != nil || (s.State.Terminated != nil && s.State.Terminated.ExitCode != 0) {
+					allSuccessfullyTerminated = false
+				}
+			}
+
+			// Init container will become sidecar if the restart policy is set to Always
+			// https://kubernetes.io/docs/concepts/workloads/pods/sidecar-containers/#sidecar-containers-and-pod-lifecycle
+			for _, s := range pod.Status.InitContainerStatuses {
 				if s.State.Waiting != nil || s.State.Running != nil || (s.State.Terminated != nil && s.State.Terminated.ExitCode != 0) {
 					allSuccessfullyTerminated = false
 				}
