40e620e7ed61f098d9c6b84eb9eddcee417af869
machichima <nary12321@gmail.com>
2025-09-03 19:25:20 -0700

    refactor: lint
    Signed-off-by: machichima <nary12321@gmail.com>

--- flyteplugins/go/tasks/plugins/k8s/ray/ray_test.go
+++ flyteplugins/go/tasks/plugins/k8s/ray/ray_test.go
@@ -13,9 +13,9 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/structpb"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/utils/ptr"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 
 	"github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/core"
 	"github.com/flyteorg/flyte/flyteidl/gen/pb-go/flyteidl/plugins"
