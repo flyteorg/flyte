e4000bb9f57c9fe4e74636340644e26cb0889c67
Haytham Abuelfutuh <haytham@afutuh.com>
2025-09-15 08:38:47 -0700

    Fix oneof buf lint
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- gen/python/flyteidl2/core/artifact_id_pb2.pyi
+++ gen/python/flyteidl2/core/artifact_id_pb2.pyi
@@ -1,5 +1,4 @@
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
-from flyteidl2.core import identifier_pb2 as _identifier_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
