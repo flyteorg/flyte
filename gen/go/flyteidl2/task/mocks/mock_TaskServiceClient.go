4f1e4d4c5a5e437b874ecfeae2b73cc608e7bebe
Nary Yeh <60069744+machichima@users.noreply.github.com>
2025-10-24 22:51:20 -0700

    [FEAT] add List versions request and response (#6687)
    * feat: add List versions request and response
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    * feat: add ListVersions RPC
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    * fix: make gen
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    * fix: make gen
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    * feat: add common list request to list versions request
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    ---------
    
    Signed-off-by: machichima <nary12321@gmail.com>

--- gen/go/flyteidl2/task/mocks/mock_TaskServiceClient.go
+++ gen/go/flyteidl2/task/mocks/mock_TaskServiceClient.go
@@ -247,6 +247,80 @@ func (_c *TaskServiceClient_ListTasks_Call) RunAndReturn(run func(context.Contex
 	return _c
 }
 
+// ListVersions provides a mock function with given fields: ctx, in, opts
+func (_m *TaskServiceClient) ListVersions(ctx context.Context, in *task.ListVersionsRequest, opts ...grpc.CallOption) (*task.ListVersionsResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for ListVersions")
+	}
+
+	var r0 *task.ListVersionsResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *task.ListVersionsRequest, ...grpc.CallOption) (*task.ListVersionsResponse, error)); ok {
+		return rf(ctx, in, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *task.ListVersionsRequest, ...grpc.CallOption) *task.ListVersionsResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*task.ListVersionsResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *task.ListVersionsRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// TaskServiceClient_ListVersions_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ListVersions'
+type TaskServiceClient_ListVersions_Call struct {
+	*mock.Call
+}
+
+// ListVersions is a helper method to define mock.On call
+//   - ctx context.Context
+//   - in *task.ListVersionsRequest
+//   - opts ...grpc.CallOption
+func (_e *TaskServiceClient_Expecter) ListVersions(ctx interface{}, in interface{}, opts ...interface{}) *TaskServiceClient_ListVersions_Call {
+	return &TaskServiceClient_ListVersions_Call{Call: _e.mock.On("ListVersions",
+		append([]interface{}{ctx, in}, opts...)...)}
+}
+
+func (_c *TaskServiceClient_ListVersions_Call) Run(run func(ctx context.Context, in *task.ListVersionsRequest, opts ...grpc.CallOption)) *TaskServiceClient_ListVersions_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		variadicArgs := make([]grpc.CallOption, len(args)-2)
+		for i, a := range args[2:] {
+			if a != nil {
+				variadicArgs[i] = a.(grpc.CallOption)
+			}
+		}
+		run(args[0].(context.Context), args[1].(*task.ListVersionsRequest), variadicArgs...)
+	})
+	return _c
+}
+
+func (_c *TaskServiceClient_ListVersions_Call) Return(_a0 *task.ListVersionsResponse, _a1 error) *TaskServiceClient_ListVersions_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *TaskServiceClient_ListVersions_Call) RunAndReturn(run func(context.Context, *task.ListVersionsRequest, ...grpc.CallOption) (*task.ListVersionsResponse, error)) *TaskServiceClient_ListVersions_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewTaskServiceClient creates a new instance of TaskServiceClient. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewTaskServiceClient(t interface {
