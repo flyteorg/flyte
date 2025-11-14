741768cc372fab443e4cc27993ee684e45660629
machichima <nary12321@gmail.com>
2025-11-14 15:06:32 +0800

    feat: created by to repeat EnrichedIdentity
    Signed-off-by: machichima <nary12321@gmail.com>

--- gen/go/flyteidl2/workflow/run_definition.pb.validate.go
+++ gen/go/flyteidl2/workflow/run_definition.pb.validate.go
@@ -4225,6 +4225,40 @@ func (m *TaskGroup) validate(all bool) error {
 		}
 	}
 
+	for idx, item := range m.GetCreatedBy() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, TaskGroupValidationError{
+						field:  fmt.Sprintf("CreatedBy[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, TaskGroupValidationError{
+						field:  fmt.Sprintf("CreatedBy[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return TaskGroupValidationError{
+					field:  fmt.Sprintf("CreatedBy[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	// no validation rules for ShouldDelete
 
 	if len(errors) > 0 {
