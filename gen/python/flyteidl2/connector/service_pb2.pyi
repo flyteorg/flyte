f511bb69336a4f0f7b17968967bb42fa5531e424
Kevin Su <pingsutw@apache.org>
2025-12-10 14:53:34 -0800

    rename
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- gen/python/flyteidl2/connector/service_pb2.pyi
+++ gen/python/flyteidl2/connector/service_pb2.pyi
@@ -1,4 +1,4 @@
-from flyteidl2.connector import task_pb2 as _task_pb2
+from flyteidl2.connector import connector_pb2 as _connector_pb2
 from google.api import annotations_pb2 as _annotations_pb2
 from google.protobuf import descriptor as _descriptor
 from typing import ClassVar as _ClassVar
