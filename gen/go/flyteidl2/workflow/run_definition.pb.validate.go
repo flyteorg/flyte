50f027b3cb771aba9d086ee1e8e925fb78122956
machichima <nary12321@gmail.com>
2025-12-18 16:40:28 +0800

    feat: add error counts for task group proto
    Signed-off-by: machichima <nary12321@gmail.com>

--- gen/go/flyteidl2/workflow/run_definition.pb.validate.go
+++ gen/go/flyteidl2/workflow/run_definition.pb.validate.go
@@ -4269,6 +4269,35 @@ func (m *TaskGroup) validate(all bool) error {
 
 	// no validation rules for ShortName
 
+	if all {
+		switch v := interface{}(m.GetErrorCounts()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, TaskGroupValidationError{
+					field:  "ErrorCounts",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, TaskGroupValidationError{
+					field:  "ErrorCounts",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetErrorCounts()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return TaskGroupValidationError{
+				field:  "ErrorCounts",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return TaskGroupMultiError(errors)
 	}
@@ -4345,3 +4374,111 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = TaskGroupValidationError{}
+
+// Validate checks the field values on TaskGroup_ErrorCounts with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *TaskGroup_ErrorCounts) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on TaskGroup_ErrorCounts with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// TaskGroup_ErrorCountsMultiError, or nil if none found.
+func (m *TaskGroup_ErrorCounts) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *TaskGroup_ErrorCounts) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for UserError
+
+	// no validation rules for SystemError
+
+	// no validation rules for UnspecifiedError
+
+	if len(errors) > 0 {
+		return TaskGroup_ErrorCountsMultiError(errors)
+	}
+
+	return nil
+}
+
+// TaskGroup_ErrorCountsMultiError is an error wrapping multiple validation
+// errors returned by TaskGroup_ErrorCounts.ValidateAll() if the designated
+// constraints aren't met.
+type TaskGroup_ErrorCountsMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m TaskGroup_ErrorCountsMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m TaskGroup_ErrorCountsMultiError) AllErrors() []error { return m }
+
+// TaskGroup_ErrorCountsValidationError is the validation error returned by
+// TaskGroup_ErrorCounts.Validate if the designated constraints aren't met.
+type TaskGroup_ErrorCountsValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e TaskGroup_ErrorCountsValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e TaskGroup_ErrorCountsValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e TaskGroup_ErrorCountsValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e TaskGroup_ErrorCountsValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e TaskGroup_ErrorCountsValidationError) ErrorName() string {
+	return "TaskGroup_ErrorCountsValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e TaskGroup_ErrorCountsValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sTaskGroup_ErrorCounts.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = TaskGroup_ErrorCountsValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = TaskGroup_ErrorCountsValidationError{}
