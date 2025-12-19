002321ee2f43cc9e32330cf98726e1090cd2e696
findfluctuate <findfluctuate@outlook.com>
2025-12-19 15:09:02 +0800

    chore: execute goimports to format the code (#6801)
    Signed-off-by: findfluctuate <findfluctuate@outlook.com>
    Co-authored-by: Nary Yeh <60069744+machichima@users.noreply.github.com>

--- boilerplate/flyte/golang_support_tools/tools.go
+++ boilerplate/flyte/golang_support_tools/tools.go
@@ -4,10 +4,10 @@
 package tools
 
 import (
-	_ "github.com/vektra/mockery/v2/cmd"
 	_ "github.com/alvaroloes/enumer"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
+	_ "github.com/vektra/mockery/v2/cmd"
 
 	_ "github.com/flyteorg/flyte/flytestdlib/cli/pflags"
 )
