19dad820f8ffae7b2dc93b233dd2ee6402eb36aa
Michael Hotan <mike@union.ai>
2026-02-08 16:53:11 -0800

    Add ExecutedBy EnrichedIdentity to RunSpec
    Supports the ability to associate user identity when entities with
    RunSpecs are registered
    
    Signed-off-by: Michael Hotan <mike@union.ai>

--- gen/go/flyteidl2/task/run.pb.validate.go
+++ gen/go/flyteidl2/task/run.pb.validate.go
@@ -801,6 +801,35 @@ func (m *RunSpec) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetExecutedBy()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, RunSpecValidationError{
+					field:  "ExecutedBy",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, RunSpecValidationError{
+					field:  "ExecutedBy",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetExecutedBy()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return RunSpecValidationError{
+				field:  "ExecutedBy",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return RunSpecMultiError(errors)
 	}
