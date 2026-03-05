574249632e45fc65bfb68b0d4e33f79085d1784e
machichima <nary12321@gmail.com>
2026-03-05 17:09:10 +0800

    feat: add to main.go
    Signed-off-by: machichima <nary12321@gmail.com>

---
+++ events/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/flyteorg/flyte/v2/app"
+	"github.com/flyteorg/flyte/v2/events"
+	eventsconfig "github.com/flyteorg/flyte/v2/events/config"
+)
+
+func main() {
+	a := &app.App{
+		Name:  "events-service",
+		Short: "Events Service for Flyte",
+		Setup: func(ctx context.Context, sc *app.SetupContext) error {
+			cfg := eventsconfig.GetConfig()
+			sc.Host = cfg.Server.Host
+			sc.Port = cfg.Server.Port
+
+			return events.Setup(ctx, sc)
+		},
+	}
+	if err := a.Run(); err != nil {
+		os.Exit(1)
+	}
+}
