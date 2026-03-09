fbc6cf9efbe558592465378193956bb251b71ad4
Alex Wu <c.alexwu@gmail.com>
2026-03-09 15:50:00 +0800

    fix sandbox cluster name
    Signed-off-by: Alex Wu <c.alexwu@gmail.com>

--- docker/sandbox-bundled/bin/k3d-entrypoint-flyte-sandbox-bootstrap.sh
+++ docker/sandbox-bundled/bin/k3d-entrypoint-flyte-sandbox-bootstrap.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 flyte-sandbox-bootstrap
+
+KUBECONFIG_PATH="${K3S_KUBECONFIG_OUTPUT:-/etc/rancher/k3s/k3s.yaml}"
+(
+  while ! [ -s "$KUBECONFIG_PATH" ]; do sleep 1; done
+  sed -i 's/: default/: flytev2-sandbox/g' "$KUBECONFIG_PATH"
+) &
