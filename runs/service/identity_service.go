c81a96d8dd33f5036407dbf7c21bb01092f200b5
Kevin Su <pingsutw@apache.org>
2026-03-09 13:19:54 -0700

    add-todo
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- runs/service/identity_service.go
+++ runs/service/identity_service.go
@@ -20,6 +20,7 @@ func NewIdentityService() *IdentityService {
 var _ authconnect.IdentityServiceHandler = (*IdentityService)(nil)
 
 // UserInfo returns information about the currently logged in user.
+// TODO: Wire with real auth to populate user info from the authenticated context.
 func (s *IdentityService) UserInfo(
 	ctx context.Context,
 	req *connect.Request[auth.UserInfoRequest],
