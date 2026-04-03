1d75de62021c74e26b556ed7bfe2a976dcd55c45
Sergey Vilgelm <523825+SVilgelm@users.noreply.github.com>
2026-04-02 18:56:36 -0700

    Migrate mockery from v2 to v3.7.0 (#7148)

--- flyteplugins/go/tasks/pluginmachinery/secret/secrets_test.go
+++ flyteplugins/go/tasks/pluginmachinery/secret/secrets_test.go
@@ -34,8 +34,8 @@ func TestSecretsWebhook_Mutate(t *testing.T) {
 
 	t.Run("First fail", func(t *testing.T) {
 		mutator := &mocks.SecretsInjector{}
-		mutator.OnInjectMatch(mock.Anything, mock.Anything, mock.Anything).Return(nil, false, fmt.Errorf("failed"))
-		mutator.OnType().Return(config.SecretManagerTypeGlobal)
+		mutator.EXPECT().Inject(mock.Anything, mock.Anything, mock.Anything).Return(nil, false, fmt.Errorf("failed"))
+		mutator.EXPECT().Type().Return(config.SecretManagerTypeGlobal)
 
 		m := SecretsPodMutator{
 			enabledSecretManagerTypes: []config.SecretManagerType{config.SecretManagerTypeGlobal},
@@ -51,8 +51,8 @@ func TestSecretsWebhook_Mutate(t *testing.T) {
 
 	t.Run("added", func(t *testing.T) {
 		mutator := &mocks.SecretsInjector{}
-		mutator.OnInjectMatch(mock.Anything, mock.Anything, mock.Anything).Return(&corev1.Pod{}, true, nil)
-		mutator.OnType().Return(config.SecretManagerTypeGlobal)
+		mutator.EXPECT().Inject(mock.Anything, mock.Anything, mock.Anything).Return(&corev1.Pod{}, true, nil)
+		mutator.EXPECT().Type().Return(config.SecretManagerTypeGlobal)
 		ctx := context.Background()
 
 		m := SecretsPodMutator{
