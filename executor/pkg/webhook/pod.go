b4b152f2ba1b000e688205e2c9d257fc79f58f83
Jason Parraga <sovietaced@gmail.com>
2026-04-30 16:25:28 -0700

    fix new refs
    Signed-off-by: Jason Parraga <sovietaced@gmail.com>

--- executor/pkg/webhook/pod.go
+++ executor/pkg/webhook/pod.go
@@ -77,7 +77,7 @@ func getPodMutatePath() string {
 }
 
 func generateMutatePath(gvk schema.GroupVersionKind) string {
-	return "/mutate-" + strings.Replace(gvk.Group, ".", "-", -1) + "-" +
+	return "/mutate-" + strings.ReplaceAll(gvk.Group, ".", "-") + "-" +
 		gvk.Version + "-" + strings.ToLower(gvk.Kind)
 }
 
@@ -102,7 +102,7 @@ func (pm PodMutator) CreateMutationWebhookConfiguration(namespace string) (*admi
 		},
 		Webhooks: []admissionregistrationv1.MutatingWebhook{
 			{
-				Name:         webhookName,
+				Name: webhookName,
 				ClientConfig: admissionregistrationv1.WebhookClientConfig{
 					CABundle: caBytes,
 					Service: &admissionregistrationv1.ServiceReference{
