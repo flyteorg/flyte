d8b21700d6360c175cacb36b11642db89a978178
Katrina Rogan <katroganGH@gmail.com>
2026-05-11 12:47:56 +0200

    Allow routing secrets endpoints via SelectCluster
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- gen/go/flyteidl2/secret/payload.pb.validate.go
+++ gen/go/flyteidl2/secret/payload.pb.validate.go
@@ -115,6 +115,35 @@ func (m *CreateSecretRequest) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetClusterPoolId()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, CreateSecretRequestValidationError{
+					field:  "ClusterPoolId",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, CreateSecretRequestValidationError{
+					field:  "ClusterPoolId",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetClusterPoolId()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return CreateSecretRequestValidationError{
+				field:  "ClusterPoolId",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return CreateSecretRequestMultiError(errors)
 	}
