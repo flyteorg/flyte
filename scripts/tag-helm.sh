466f743258d64e582ee8a2abd31fc6e4e52080eb
Jason Parraga <sovietaced@gmail.com>
2026-05-28 16:54:02 -0700

    Add initial workflow to publish helm charts on release tags
    Signed-off-by: Jason Parraga <sovietaced@gmail.com>

---
+++ scripts/tag-helm.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+grep -rlZ "version:[^P]*# VERSION" ./charts/flyte-binary/Chart.yaml | xargs -0 sed -i "s/version:[^P]*# VERSION/version: ${VERSION} # VERSION/g"
+sed "s/v0.2.0/${VERSION}/g" ./charts/flyte-binary/README.md > temp.txt && mv temp.txt ./charts/flyte-binary/README.md
+
+helm dep update ./charts/flyte-binary
+
+# bump latest release of flyte component in helm
+sed -i "s,tag:[^P]*# FLYTE_TAG,tag: ${VERSION} # FLYTE_TAG," ./charts/flyte-binary/values.yaml
