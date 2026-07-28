2e44706359f4c23d4606d02e1bc90fbae606a336
Iaroslav Ciupin <iaroslav@union.ai>
2026-07-28 10:32:48 +0300

    fix
    Signed-off-by: Iaroslav Ciupin <iaroslav@union.ai>

--- runs/setup.go
+++ runs/setup.go
@@ -11,7 +11,12 @@ import (
 
 	"connectrpc.com/connect"
 	"connectrpc.com/otelconnect"
+	"github.com/getsentry/sentry-go"
+	"github.com/getsentry/sentry-go/attribute"
+
 	"github.com/flyteorg/flyte/v2/flytestdlib/app"
+	"github.com/flyteorg/flyte/v2/flytestdlib/logger"
+	"github.com/flyteorg/flyte/v2/flytestdlib/otelutils"
 	"github.com/flyteorg/flyte/v2/gen/go/flyteidl2/actions/actionsconnect"
 	"github.com/flyteorg/flyte/v2/gen/go/flyteidl2/auth/authconnect"
 	projectpb "github.com/flyteorg/flyte/v2/gen/go/flyteidl2/project"
@@ -27,11 +32,6 @@ import (
 	"github.com/flyteorg/flyte/v2/runs/repository/models"
 	"github.com/flyteorg/flyte/v2/runs/scheduler"
 	"github.com/flyteorg/flyte/v2/runs/service"
-	"github.com/getsentry/sentry-go"
-	"github.com/gets
-
-	"github.com/flyteorg/flyte/v2/flytestdlib/logger"
-	"github.com/flyteorg/flyte/v2/flytestdlib/otelutils"
 )
 
 const otelServiceName = "runs-service"
