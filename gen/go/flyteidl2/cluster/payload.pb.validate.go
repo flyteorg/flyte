47a95ae06b93d03642cf1ca022fc93920b24eb72
Drew Rochon <11633183+notSoWiseOldMan@users.noreply.github.com>
2026-07-30 15:45:22 -0700

    Adding cluster_id to SelectClusterRequest
    Signed-off-by: Drew Rochon <11633183+notSoWiseOldMan@users.noreply.github.com>

--- gen/go/flyteidl2/cluster/payload.pb.validate.go
+++ gen/go/flyteidl2/cluster/payload.pb.validate.go
@@ -388,6 +388,47 @@ func (m *SelectClusterRequest) validate(all bool) error {
 			}
 		}
 
+	case *SelectClusterRequest_ClusterId:
+		if v == nil {
+			err := SelectClusterRequestValidationError{
+				field:  "Resource",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetClusterId()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, SelectClusterRequestValidationError{
+						field:  "ClusterId",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, SelectClusterRequestValidationError{
+						field:  "ClusterId",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetClusterId()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return SelectClusterRequestValidationError{
+					field:  "ClusterId",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		_ = v // ensures v is used
 	}
