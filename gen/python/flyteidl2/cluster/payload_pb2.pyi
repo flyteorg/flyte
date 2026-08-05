c36f64e967b72631287b8ab14ef5669cbd81b404
Drew <11633183+notSoWiseOldMan@users.noreply.github.com>
2026-08-05 15:00:32 -0700

    feat: Adding cluster_id to SelectClusterRequest (#7739)
    Signed-off-by: Drew Rochon <11633183+notSoWiseOldMan@users.noreply.github.com>

--- gen/python/flyteidl2/cluster/payload_pb2.pyi
+++ gen/python/flyteidl2/cluster/payload_pb2.pyi
@@ -10,7 +10,7 @@ from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Opti
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class SelectClusterRequest(_message.Message):
-    __slots__ = ["org_id", "project_id", "task_id", "action_id", "action_attempt_id", "app_id", "cluster_pool_id", "domain_id", "operation"]
+    __slots__ = ["org_id", "project_id", "task_id", "action_id", "action_attempt_id", "app_id", "cluster_pool_id", "domain_id", "cluster_id", "operation"]
     class Operation(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
         __slots__ = []
         OPERATION_UNSPECIFIED: _ClassVar[SelectClusterRequest.Operation]
@@ -45,6 +45,7 @@ class SelectClusterRequest(_message.Message):
     APP_ID_FIELD_NUMBER: _ClassVar[int]
     CLUSTER_POOL_ID_FIELD_NUMBER: _ClassVar[int]
     DOMAIN_ID_FIELD_NUMBER: _ClassVar[int]
+    CLUSTER_ID_FIELD_NUMBER: _ClassVar[int]
     OPERATION_FIELD_NUMBER: _ClassVar[int]
     org_id: _identifier_pb2.OrgIdentifier
     project_id: _identifier_pb2.ProjectIdentifier
@@ -54,8 +55,9 @@ class SelectClusterRequest(_message.Message):
     app_id: _app_definition_pb2.Identifier
     cluster_pool_id: _identifier_pb2.ClusterPoolIdentifier
     domain_id: _identifier_pb2.DomainIdentifier
+    cluster_id: _identifier_pb2.ClusterIdentifier
     operation: SelectClusterRequest.Operation
-    def __init__(self, org_id: _Optional[_Union[_identifier_pb2.OrgIdentifier, _Mapping]] = ..., project_id: _Optional[_Union[_identifier_pb2.ProjectIdentifier, _Mapping]] = ..., task_id: _Optional[_Union[_task_definition_pb2.TaskIdentifier, _Mapping]] = ..., action_id: _Optional[_Union[_identifier_pb2.ActionIdentifier, _Mapping]] = ..., action_attempt_id: _Optional[_Union[_identifier_pb2.ActionAttemptIdentifier, _Mapping]] = ..., app_id: _Optional[_Union[_app_definition_pb2.Identifier, _Mapping]] = ..., cluster_pool_id: _Optional[_Union[_identifier_pb2.ClusterPoolIdentifier, _Mapping]] = ..., domain_id: _Optional[_Union[_identifier_pb2.DomainIdentifier, _Mapping]] = ..., operation: _Optional[_Union[SelectClusterRequest.Operation, str]] = ...) -> None: ...
+    def __init__(self, org_id: _Optional[_Union[_identifier_pb2.OrgIdentifier, _Mapping]] = ..., project_id: _Optional[_Union[_identifier_pb2.ProjectIdentifier, _Mapping]] = ..., task_id: _Optional[_Union[_task_definition_pb2.TaskIdentifier, _Mapping]] = ..., action_id: _Optional[_Union[_identifier_pb2.ActionIdentifier, _Mapping]] = ..., action_attempt_id: _Optional[_Union[_identifier_pb2.ActionAttemptIdentifier, _Mapping]] = ..., app_id: _Optional[_Union[_app_definition_pb2.Identifier, _Mapping]] = ..., cluster_pool_id: _Optional[_Union[_identifier_pb2.ClusterPoolIdentifier, _Mapping]] = ..., domain_id: _Optional[_Union[_identifier_pb2.DomainIdentifier, _Mapping]] = ..., cluster_id: _Optional[_Union[_identifier_pb2.ClusterIdentifier, _Mapping]] = ..., operation: _Optional[_Union[SelectClusterRequest.Operation, str]] = ...) -> None: ...
 
 class SelectClusterResponse(_message.Message):
     __slots__ = ["cluster_endpoint", "cluster"]
