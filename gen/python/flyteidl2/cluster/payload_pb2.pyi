34e03235fb938cd2572d921ef11feb835715f8a0
Drew <11633183+notSoWiseOldMan@users.noreply.github.com>
2026-08-07 14:25:32 -0700

    Adding OPERATION_GET_CLUSTER_CONFIG (#7800)
    Signed-off-by: Drew Rochon <11633183+notSoWiseOldMan@users.noreply.github.com>

--- gen/python/flyteidl2/cluster/payload_pb2.pyi
+++ gen/python/flyteidl2/cluster/payload_pb2.pyi
@@ -25,6 +25,7 @@ class SelectClusterRequest(_message.Message):
         OPERATION_UPLOAD_TRIGGER: _ClassVar[SelectClusterRequest.Operation]
         OPERATION_GET_IMAGE: _ClassVar[SelectClusterRequest.Operation]
         OPERATION_TRACKED_RUN_DATA: _ClassVar[SelectClusterRequest.Operation]
+        OPERATION_GET_CLUSTER_CONFIG: _ClassVar[SelectClusterRequest.Operation]
     OPERATION_UNSPECIFIED: SelectClusterRequest.Operation
     OPERATION_CREATE_UPLOAD_LOCATION: SelectClusterRequest.Operation
     OPERATION_UPLOAD_INPUTS: SelectClusterRequest.Operation
@@ -37,6 +38,7 @@ class SelectClusterRequest(_message.Message):
     OPERATION_UPLOAD_TRIGGER: SelectClusterRequest.Operation
     OPERATION_GET_IMAGE: SelectClusterRequest.Operation
     OPERATION_TRACKED_RUN_DATA: SelectClusterRequest.Operation
+    OPERATION_GET_CLUSTER_CONFIG: SelectClusterRequest.Operation
     ORG_ID_FIELD_NUMBER: _ClassVar[int]
     PROJECT_ID_FIELD_NUMBER: _ClassVar[int]
     TASK_ID_FIELD_NUMBER: _ClassVar[int]
