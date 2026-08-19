26d42d50c3f01348e0a64d713dfc2372fe70faa2
davidlin20dev <davidlin20.dev@gmail.com>
2026-08-19 10:45:14 -0700

    feat(settings): add pod_template_name to settings proto
    Signed-off-by: davidlin20dev <davidlin20.dev@gmail.com>

--- gen/go/flyteidl2/settings/settings_definition.pb.validate.go
+++ gen/go/flyteidl2/settings/settings_definition.pb.validate.go
@@ -2128,6 +2128,35 @@ func (m *Settings) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetPodTemplateName()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, SettingsValidationError{
+					field:  "PodTemplateName",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, SettingsValidationError{
+					field:  "PodTemplateName",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetPodTemplateName()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return SettingsValidationError{
+				field:  "PodTemplateName",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return SettingsMultiError(errors)
 	}
