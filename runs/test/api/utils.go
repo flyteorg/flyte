97b2bc2c91bb8eec50a2bc5629a4f6c705439e5a
davidlin20dev <davidlin20.dev@gmail.com>
2026-08-31 12:08:22 -0700

    test(runs): mount the settings service in the api test harness
    Signed-off-by: davidlin20dev <davidlin20.dev@gmail.com>

--- runs/test/api/utils.go
+++ runs/test/api/utils.go
@@ -10,7 +10,7 @@ func uniqueString() string { //nolint: unused
 	return fmt.Sprintf("%d", time.Now().UnixNano())
 }
 
-func newClient() *http.Client { //nolint: unused
+func newClient() *http.Client {
 	return &http.Client{
 		Timeout: 30 * time.Second,
 	}
