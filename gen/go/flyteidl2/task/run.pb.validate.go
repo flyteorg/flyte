d218932c1cd696587d2e8c6f59a8a9582829db4a
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2026-09-23 20:44:26 -0700

    add to runspec
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- gen/go/flyteidl2/task/run.pb.validate.go
+++ gen/go/flyteidl2/task/run.pb.validate.go
@@ -1412,6 +1412,35 @@ func (m *TaskResourceDefaults) validate(all bool) error {
 
 	}
 
+	if all {
+		switch v := interface{}(m.GetDefaultAccelerator()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, TaskResourceDefaultsValidationError{
+					field:  "DefaultAccelerator",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, TaskResourceDefaultsValidationError{
+					field:  "DefaultAccelerator",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetDefaultAccelerator()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return TaskResourceDefaultsValidationError{
+				field:  "DefaultAccelerator",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return TaskResourceDefaultsMultiError(errors)
 	}
